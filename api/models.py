13753d85df17cb2a0cfb6e7172cb3ab4196fd301
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-09 18:47:39 -0500

    initial models and model tests

--- api/models.py
+++ api/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class Disaster(models.Model):
+    """ A disaster, which could cover multiple countries """
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    """ A country """
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Document(models.Model):
+    """ A document, located somwehere """
+
+    name = models.CharField(max_length=100)
+    uri = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Appeal(models.Model):
+    """ An appeal for a disaster and country, containing documents """
+
+    # appeal ID, assinged by creator
+    aid = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    disaster = models.ForeignKey(Disaster)
+    country = models.ForeignKey(Country)
+    documents = models.ManyToManyField(Document)
+
+    def __str__(self):
+        return self.aid
+
+
+class FieldReport(models.Model):
+    """ A field report for a disaster and country, containing documents """
+
+    fid = models.CharField(max_length=100)
+    address = models.TextField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    disaster = models.ForeignKey(Disaster)
+    country = models.ForeignKey(Country)
+    documents = models.ManyToManyField(Document)
+
+    def __str__(self):
+        return self.aid
+
+
+class Service(models.Model):
+    """ A resource that may or may not be deployed """
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    deployed = models.BooleanField(default=False)
+    location = models.TextField()
+
+    def __str__(self):
+        return self.name
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
 
-# Create your tests here.
+import api.models as models
+
+
+class DiasterTest(TestCase):
+    def setUp(self):
+        models.Disaster.create(name='disaster1', description='test disaster')
+        models.Disaster.create(name='disaster2', description='another test disaster')
+
+    def test_disaster_create(self):
+        obj1 = models.Disaster.objects.get(name='disaster1')
+        obj2 = models.Disaster.objects.get(name='disaster2')
+        self.assertEqual(obj1.description, 'test disaster')
+        self.assertEqual(obj2.description, 'another test disaster')
+
+
+class CountryTest(TestCase):
+    def setUp(self):
+        models.Country.create(name='country1')
+
+    def test_country_create(self):
+        obj = models.Country.objects.get(name='country1')
+        self.assertEqual(obj.name, 'country1')
+
+
+class DocumentTest(TestCase):
+    def setUp(self):
+        models.Document.create(name='document1', uri='/path/to/file')
+
+    def test_document_create(self):
+        obj = models.Document.objects.get(name='document1')
+        self.assertEqual(obj.uri, '/path/to/file')
+
+
+class AppealTest(TestCase):
+    def setUp(self):
+        disaster = models.Disaster.create(name='disaster1', description='test disaster')
+        models.Appeal.create(name='test1', disaster=disaster)
+
+    def test_appeal_create(self):
+        disaster = models.Disaster.objects.get(name='disaster1')
+        country = models.Country.objects.get(name='country')
+        obj = models.Appeal.objects.get(name='test1')
+        self.assertRqual(obj.description, 'test disaster')
+        self.assertEqual(obj.country, country)
+        self.assertEqual(obj.disaster, disaster)
+
+
+class FieldReportTest(TestCase):
+    def setUp(self):
+        disaster = models.Disaster.create(name='disaster1', description='test disaster')
+        country = models.Country.create(name='country')
+        models.FieldReport.create(name='test1', disaster=disaster, country=country)
+
+    def test_field_report_create(self):
+        disaster = models.Disaster.objects.get(name='disaster1')
+        country = models.Country.objects.get(name='country')
+        obj = models.FieldReport.objects.get(name='test1')
+        self.assertRqual(obj.description, 'test disaster')
+        self.assertEqual(obj.country, country)
+        self.assertEqual(obj.disaster, disaster)
+
+
+class ServiceTest(TestCase):
+    def setUp(self):
+        models.Service.create(name='test1', location='earth')
+        models.Service.create(name='test2', deployed=True, location='iceland')
+
+    def test_service_create(self):
+        obj1 = models.Service.objects.get(name='test1')
+        self.assertFalse(obj1.deployed)
+        self.assertEqual(obj1.location, 'earth')
+        obj1 = models.Service.objects.get(name='test2')
+        self.assertTrue(obj1.deployed)
+        self.assertEqual(obj1.location, 'iceland')
+
