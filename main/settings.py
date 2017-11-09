7153b1ad0725a2ac64d030f4b9db35e6d63486bf
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-09 18:47:32 -0500

    do not import local settings if it does not exist

--- main/settings.py
+++ main/settings.py
@@ -74,4 +74,5 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 
-from .secret_settings import *
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'secret_settings.py')):
+    from .secret_settings import *
