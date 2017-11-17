024b157124990bea5d40c1e45e69a2ac12b3affa
Derek Lieu <dl@dereklieu.com>
2017-11-17 17:06:46 -0500

    Create token authentication endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+from tastypie.models import ApiKey
+from .utils import pretty_request
+from .authentication import token_duration
 
-# Create your views here.
+@csrf_exempt
+def get_auth_token(request):
+    print(pretty_request(request))
+    if request.META.get('CONTENT_TYPE') != 'application/json':
+        return HttpResponseBadRequest('Content-type must be `application/json`')
+    elif request.method != 'POST':
+        return HttpResponseBadRequest('HTTP method must be `POST`')
+    else:
+        body = json.loads(request.body.decode('utf-8'))
+        username = body['username']
+        password = body['password']
+        if not username or not password:
+            return HttpResponseBadRequest('Body must contain `username` and `password`')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            api_key, created = ApiKey.objects.get_or_create(user=user)
+            return JsonResponse({
+                'token': api_key.key,
+                'username': username,
+                'expires': api_key.created + token_duration,
+            })
+        else:
+            return HttpResponseBadRequest('Could not authenticate')
--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from tastypie.api import Api
 from api.resources import DisasterTypeResource, EventResource, CountryResource, FieldReportResource
+from api.views import get_auth_token
 
 v1_api = Api(api_name='v1')
 v1_api.register(DisasterTypeResource())
@@ -27,4 +28,5 @@ v1_api.register(FieldReportResource())
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(v1_api.urls)),
+    url(r'^get_auth_token', get_auth_token),
 ]
