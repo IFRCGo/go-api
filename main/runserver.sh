8b1eb08d6eb7934d17197f555345a20f1a5325fd
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-20 15:44:19 -0500

    put envvars in quotes

--- main/runserver.sh
+++ main/runserver.sh
@@ -25,13 +25,13 @@ exec gunicorn main.wsgi:application \
 # set up cron
 echo export GO_FTPHOST=$GO_FTPHOST >> $HOME/.env
 echo export GO_FTPUSER=$GO_FTPUSER >> $HOME/.env
-echo export GO_FTPPASS=$GO_FTPPASS >> $HOME/.env
-echo export GO_DBPASS=$GO_DBPASS >> $HOME/.env
+echo export GO_FTPPASS="$GO_FTPPASS" >> $HOME/.env
+echo export GO_DBPASS="$GO_DBPASS" >> $HOME/.env
 echo export PATH=$PATH:/usr/local/bin >> $HOME/.env
-echo export DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY >> $HOME/.env
+echo export DJANGO_SECRET_KEY="$DJANGO_SECRET_KEY" >> $HOME/.env
 echo export DJANGO_DB_NAME=$DJANGO_DB_NAME >> $HOME/.env
-echo export DJANGO_DB_USER=$DJANGO_DB_USER >> $HOME/.env
-echo export DJANGO_DB_PASS=$DJANGO_DB_PASS >> $HOME/.env
+echo export DJANGO_DB_USER="$DJANGO_DB_USER" >> $HOME/.env
+echo export DJANGO_DB_PASS="$DJANGO_DB_PASS" >> $HOME/.env
 echo export DJANGO_DB_HOST=$DJANGO_DB_HOST >> $HOME/.env
 echo export DJANGO_DB_PORT=$DJANGO_DB_PORT >> $HOME/.env
 (echo '0 * * * * . /home/ifrc/.env; python /home/ifrc/go-api/manage.py ingest_mdb >> /home/ifrc/logs/ingest_mdb.log 2>&1') | crontab -
