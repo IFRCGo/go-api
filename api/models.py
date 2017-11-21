40260651da7e5d699bd6de9f2a952538703269da
Derek Lieu <dl@dereklieu.com>
2017-11-21 14:24:40 -0800

    Fix the related name

--- api/models.py
+++ api/models.py
@@ -124,7 +124,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='user',
+        related_name='profile',
         primary_key=True,
     )
 
@@ -155,6 +155,6 @@ class Profile(models.Model):
 # Save a user profile whenever we create a user
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile = Profile(user=instance)
-        profile.save()
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
