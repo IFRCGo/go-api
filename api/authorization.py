c2bb73e454b1fd56dd8f0d29479ffd47376af7ae
Derek Lieu <dl@dereklieu.com>
2017-11-22 22:09:14 -0800

    Also allow any authenticated reads of field reports

--- api/authorization.py
+++ api/authorization.py
@@ -1,7 +1,13 @@
 from tastypie.authorization import DjangoAuthorization
 
 # Overwrite Django authorization to allow
-# anyone to create a field report
+# anyone to read or create a field report
 class FieldReportAuthorization(DjangoAuthorization):
+    def read_list(self, object_list, bundle):
+        return True
+
+    def read_detail(self, object_list, bundle):
+        return True
+
     def create_detail(self, object_list, bundle):
         return True
