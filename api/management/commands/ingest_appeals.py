4531d1a388775626ba11f81edc26401198a53cd9
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-23 00:28:24 -0500

    finish up appeals ingestion

--- api/management/commands/ingest_appeals.py
+++ api/management/commands/ingest_appeals.py
@@ -1,7 +1,11 @@
 import os
+import logging
 import requests
+from datetime import datetime, timedelta, timezone
 from django.core.management.base import BaseCommand
 from api.models import Appeal, Country, DisasterType, Event
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -21,14 +25,18 @@ class Command(BaseCommand):
         # with open('appeals.json') as f:
         #     results = json.loads(f.read())
 
-        ids = [r.aid for r in Appeal.objects.all()]
+        eids = [e.eid for e in Event.objects.all()]
+        aids = [a.aid for a in Appeal.objects.all()]
 
-        print('%s events' % Event.objects.all().count())
-        print('%s appeals' % Appeal.objects.all().count())
-        print('%s events in Appeals API database' % len(results))
+        print('%s current events' % Event.objects.all().count())
+        print('%s current appeals' % Appeal.objects.all().count())
+
+        results = [r for r in results if r['APP_Id'] not in eids]
+        print('Ingesting %s events' % len(results))
+
+        timeformat = '%Y-%m-%dT%H:%M:%S'
+
         for i, r in enumerate(results):
-            if r['APP_Id'] in ids:
-                continue
             print(i) if (i % 100) == 0 else None
             # create an Event for this
             dtype = DisasterType.objects.filter(name=r['ADT_name']).first()
@@ -43,29 +51,29 @@ class Command(BaseCommand):
                 'code': r['APP_code'],
             }
             event, created = Event.objects.get_or_create(eid=r['APP_Id'], defaults=fields)
+            if created:
+                eids.append(r['APP_Id'])
 
             country = Country.objects.filter(name=r['OSC_name'])
             if country.count() == 0:
                 country = None
 
-            aids = [a.aid for a in Appeal.objects.all()]
-            for appeal in r['Details']:
-                if appeal['APD_code'] in aids:
-                    continue
+            appeals = [a for a in r['Details'] if a['APD_code'] not in aids]
+            for appeal in appeals:
                 amount_funded = 0 if appeal['ContributionAmount'] is None else appeal['ContributionAmount']
                 fields = {
                     'event': event,
                     'country': country,
                     'sector': r['OSS_name'],
-                    'start_date': appeal['APD_startDate'],
-                    'end_date': appeal['APD_endDate'],
+                    'start_date': datetime.strptime(appeal['APD_startDate'], timeformat).replace(tzinfo=timezone.utc),
+                    'end_date': datetime.strptime(appeal['APD_endDate'], timeformat).replace(tzinfo=timezone.utc),
                     'num_beneficiaries': appeal['APD_noBeneficiaries'],
                     'amount_requested': appeal['APD_amountCHF'],
                     'amount_funded': amount_funded
                 }
-                # this will always create since we check ids above, but aids are not unique so get
-                # would return more than one item if we didn't bail at the top of this loop
                 item, created = Appeal.objects.get_or_create(aid=appeal['APD_code'], defaults=fields)
+                if created:
+                    aids.append(appeal['APD_code'])
 
         print('%s events' % Event.objects.all().count())
         print('%s appeals' % Appeal.objects.all().count())
