3c4465c994f1ea81465d0d373f4ba7e93b8b2f9e
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-28 00:18:37 -0500

    update FieldReport model and add Contact table

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
+from enumfields import EnumIntegerField
+from enumfields import Enum
 
 
 class DisasterType(models.Model):
@@ -64,6 +66,12 @@ class Document(models.Model):
         return self.name
 
 
+class AppealType(Enum):
+    """ Types of appeals """
+    DREF = 0
+    APPEAL = 1
+
+
 class Appeal(models.Model):
     """ An appeal for a disaster and country, containing documents """
 
@@ -71,6 +79,7 @@ class Appeal(models.Model):
     aid = models.CharField(max_length=20)
     start_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
+    atype = EnumIntegerField(AppealType, default=0)
 
     event = models.ForeignKey(Event, null=True)
     country = models.ForeignKey(Country, null=True)
@@ -86,6 +95,17 @@ class Appeal(models.Model):
 
     def __str__(self):
         return self.aid
+
+
+class Contact(models.Model):
+    """ Contact """
+
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
+    email = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
 
 
 class FieldReport(models.Model):
@@ -105,16 +125,25 @@ class FieldReport(models.Model):
     num_missing = models.IntegerField(null=True)
     num_affected = models.IntegerField(null=True)
     num_displaced = models.IntegerField(null=True)
-    num_assisted_gov = models.IntegerField(null=True)
-    num_assisted_rc = models.IntegerField(null=True)
+    num_assisted = models.IntegerField(null=True)
     num_localstaff = models.IntegerField(null=True)
     num_volunteers = models.IntegerField(null=True)
     num_expats_delegates = models.IntegerField(null=True)
 
+    gov_num_injured = models.IntegerField(null=True)
+    gov_num_dead = models.IntegerField(null=True)
+    gov_num_missing = models.IntegerField(null=True)
+    gov_num_affected = models.IntegerField(null=True)
+    gov_num_displaced = models.IntegerField(null=True)
+    gov_num_assisted = models.IntegerField(null=True)
+
     # action IDs - other tables?
-    action = models.TextField(blank=True, default='')
+    action_national = models.TextField(blank=True, default='')
+    action_foreign = models.TextField(blank=True, default='')
+    action_federation = models.TextField(blank=True, default='')
 
     # contacts
+    contacts = models.ManyToManyField(Contact)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -151,7 +180,7 @@ class Profile(models.Model):
     # https://drive.google.com/drive/u/1/folders/1auXpAPhOh4YROnKxOfFy5-T7Ki96aIb6k
     org = models.CharField(blank=True, max_length=100)
     org_type = models.CharField(
-        choices = (
+        choices=(
             ('NTLS', 'National Society'),
             ('DLGN', 'Delegation'),
             ('SCRT', 'Secretariat'),
