827812d85e36fa4773b5ebe5316253b635d00d36
Derek Lieu <dl@dereklieu.com>
2017-11-28 12:32:02 -0500

    Add function to create a pk map

--- api/fixtures/update-country-names.py
+++ api/fixtures/update-country-names.py
@@ -17,18 +17,27 @@ def main(input, output):
 
     # replace old keys with new, and dedupe
     new_records = []
+    # also keep track of what old primary keys map with new
+    pk_map = {}
     for fixture in records:
         key = fixture['fields']['name']
 
         if key in DISASTER_TYPE_MAPPING:
             fixture['fields']['name'] = DISASTER_TYPE_MAPPING[key]
 
-        exists = len([f for f in new_records if f['fields']['name'] == fixture['fields']['name']])
-        if not exists:
+        exists = [f for f in new_records if f['fields']['name'] == fixture['fields']['name']]
+        old_pk = str(fixture['pk'])
+        if len(exists):
+            pk_map[old_pk] = exists[0]['pk']
+        else:
             new_records.append(fixture)
+            pk_map[old_pk] = old_pk
 
-    with open(output, 'w') as o:
+    with open('%s.json' % output, 'w') as o:
         o.write(json.dumps(new_records, indent=2))
+
+    with open('%s-pks.json' % output, 'w') as o:
+        o.write(json.dumps(pk_map, indent=2))
 
 
 def parse_args(args):
