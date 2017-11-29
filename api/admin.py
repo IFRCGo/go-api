3f98002fc6c35dcf8aa35fbb46f5df4cd1b6bc5e
Matthew Hanson <matt.a.hanson@gmail.com>
2017-11-28 23:10:33 -0500

    add ActionsTaken to admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DisasterType, Event, Country, FieldReport, Appeal
+from .models import DisasterType, Event, Country, FieldReport, Appeal, ActionsTaken
 
 # Register your models here.
 admin.site.register(DisasterType)
@@ -7,4 +7,4 @@ admin.site.register(Event)
 admin.site.register(Country)
 admin.site.register(Appeal)
 admin.site.register(FieldReport)
-
+admin.site.register(ActionsTaken)
