14263a3ddc0b5d7f23326e2ce336d20014372e91
Derek Lieu <dl@dereklieu.com>
2017-12-03 22:58:15 -0600

    Create aggregate view for time-based queries

--- main/urls.py
+++ main/urls.py
@@ -29,7 +29,7 @@ from api.resources import (
     UserResource,
     ProfileResource,
 )
-from api.views import get_auth_token, es_keyword_search
+from api.views import get_auth_token, es_keyword_search, aggregate_by_time
 
 v1_api = Api(api_name='v1')
 v1_api.register(DisasterTypeResource())
@@ -48,5 +48,6 @@ urlpatterns = [
     url(r'^api/', include(v1_api.urls)),
     url(r'^api/v1/es_search/', es_keyword_search.as_view()),
     url(r'^api/v1/graphql/', GraphQLView.as_view(graphiql=True)),
+    url(r'^api/v1/aggregate/', aggregate_by_time.as_view()),
     url(r'^get_auth_token', get_auth_token),
 ]
