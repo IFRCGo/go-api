79b313d0f6d3cb65f5745b1766d43cba780f8efb
Matthew Hanson <matt.a.hanson@gmail.com>
2017-12-08 13:12:45 -0500

    remove duplicate models

--- api/models.py
+++ api/models.py
@@ -119,32 +119,6 @@ class GDACSEvent(models.Model):
     country = models.TextField()
 
 
-class RegionName(IntEnum):
-    AFRICA = 0
-    AMERICAS = 1
-    ASIA_PACIFIC = 2
-    EUROPE = 3
-    MENA = 4
-
-
-class Region(models.Model):
-    """ A region """
-    name = EnumIntegerField(RegionName)
-
-
-class Country(models.Model):
-    """ A country """
-
-    name = models.CharField(max_length=100)
-    iso = models.CharField(max_length=2, null=True)
-    society_name = models.TextField(blank=True)
-    society_url = models.URLField(blank=True)
-    region = models.ForeignKey(Region, null=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Action(models.Model):
     """ Action taken """
     name = models.CharField(max_length=100)
