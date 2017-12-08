a96a62fe6dd8132acba468140c417628903e8ebc
Derek Lieu <dl@dereklieu.com>
2017-12-08 15:35:42 -0500

    Use reverse relation query to get emails

--- api/triggers.py
+++ api/triggers.py
@@ -1,8 +1,8 @@
 import os
 from django.db.models.signals import post_save
-from django.conf import settings
 from django.template import loader
 from django.forms.models import model_to_dict
+from django.contrib.auth.models import User
 
 from .esconnection import ES_CLIENT
 from .models import Profile, Event, Appeal, FieldReport
@@ -15,7 +15,7 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     instance.profile.save()
 
-post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
+post_save.connect(create_profile, sender=User)
 
 
 def index_es(sender, instance, created, **kwargs):
@@ -35,28 +35,28 @@ if os.environ.get('BULK_IMPORT') != '1' and ES_CLIENT is not None:
 
 
 template_paths = {
-    '%s%s' % (RecordType.EVENT, SubscriptionType.NEW) : 'email/new_appeal.html',
-    '%s%s' % (RecordType.EVENT, SubscriptionType.EDIT) : 'email/new_appeal.html',
+    '%s%s' % (RecordType.EVENT, SubscriptionType.NEW) : 'email/new_event.html',
+    '%s%s' % (RecordType.EVENT, SubscriptionType.EDIT) : 'email/new_event.html',
+    '%s%s' % (RecordType.APPEAL, SubscriptionType.NEW) : 'email/new_appeal.html',
+    '%s%s' % (RecordType.APPEAL, SubscriptionType.EDIT) : 'email/new_appeal.html',
+    '%s%s' % (RecordType.REPORT, SubscriptionType.NEW) : 'email/new_report.html',
+    '%s%s' % (RecordType.REPORT, SubscriptionType.EDIT) : 'email/new_report.html',
 }
 
 
-def send_email(sender, instance, created, **kwargs):
-    print('in event post save')
+def send_notification(sender, instance, created, **kwargs):
     rtype = getattr(RecordType, instance.record_type())
     stype = SubscriptionType.NEW if created else SubscriptionType.EDIT
-    print(rtype, stype)
 
-    subscribers = Subscription.objects.filter(rtype=rtype, stype=stype).values('user')
-    print(subscribers)
+    subscribers = User.objects.filter(subscription__rtype=rtype, subscription__stype=stype).values('email')
 
     context = model_to_dict(instance)
     print(context)
 
     template_path = template_paths['%s%s' % (rtype, stype)]
-    print(template_path)
     template = loader.get_template(template_path)
     content = template.render(context=context)
     print(content)
 
 if os.environ.get('BULK_IMPORT') != '1':
-    post_save.connect(send_email, sender=Event)
+    post_save.connect(send_notification, sender=Event)
