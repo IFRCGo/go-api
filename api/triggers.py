44495983203b1a753b71641881edd13e271ddc47
Derek Lieu <dl@dereklieu.com>
2017-12-08 17:48:21 -0500

    Send email on commit, and use the lookup id for lookups

--- api/triggers.py
+++ api/triggers.py
@@ -1,7 +1,7 @@
 import os
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.template import loader
-from django.forms.models import model_to_dict
 from django.contrib.auth.models import User
 
 from .esconnection import ES_CLIENT
@@ -19,13 +19,15 @@ post_save.connect(create_profile, sender=User)
 
 
 def index_es(sender, instance, created, **kwargs):
-    if ES_CLIENT is not None:
-        ES_CLIENT.index(
-            index=instance.es_index(),
-            doc_type='page',
-            id=instance.es_id(),
-            body=instance.indexing(),
-        )
+    def on_commit():
+        if ES_CLIENT is not None:
+            ES_CLIENT.index(
+                index=instance.es_index(),
+                doc_type='page',
+                id=instance.es_id(),
+                body=instance.indexing(),
+            )
+    transaction.on_commit(on_commit)
 
 # Avoid automatic indexing during bulk imports
 if os.environ.get('BULK_IMPORT') != '1' and ES_CLIENT is not None:
@@ -45,18 +47,42 @@ template_paths = {
 
 
 def send_notification(sender, instance, created, **kwargs):
-    rtype = getattr(RecordType, instance.record_type())
-    stype = SubscriptionType.NEW if created else SubscriptionType.EDIT
+    def on_commit():
+        rtype = getattr(RecordType, instance.record_type())
+        stype = SubscriptionType.NEW if created else SubscriptionType.EDIT
 
-    subscribers = User.objects.filter(subscription__rtype=rtype, subscription__stype=stype).values('email')
+        subscribers = User.objects.filter(subscription__rtype=rtype, subscription__stype=stype).values('email')
 
-    context = model_to_dict(instance)
-    print(context)
+        lookups = []
+        # add subscribers who have subscribed to this specific disaster type
+        if instance.dtype is not None:
+            lookups.append('d%s' % instance.dtype.id)
 
-    template_path = template_paths['%s%s' % (rtype, stype)]
-    template = loader.get_template(template_path)
-    content = template.render(context=context)
-    print(content)
+        # appeals have one country, events and reports have multiple
+        # also include attached regions
+        if instance.record_type() == 'APPEAL' and instance.country is not None:
+            lookups.append('c%s' % instance.country.id)
+            if instance.country.region is not None:
+                lookups.append('%rs' % instance.country.region.id)
+        elif instance.countries is not None:
+            countries = instance.countries.prefetch_related('region').all()
+            lookups += ['c%s' % country.id for country in countries]
+            lookups += ['r%s' % getattr(country, 'region.id', None) for country in countries]
+
+        if len(lookups):
+            subscribers = (subscribers | User.objects.filter(subscription__lookup_id__in=lookups).values('email')).distinct()
+
+        print(subscribers)
+        if len(subscribers):
+            context = instance.email_context()
+            print(context)
+
+            template_path = template_paths['%s%s' % (rtype, stype)]
+            template = loader.get_template(template_path)
+            content = template.render(context=context)
+            print(content)
+
+    transaction.on_commit(on_commit)
 
 if os.environ.get('BULK_IMPORT') != '1':
     post_save.connect(send_notification, sender=Event)
