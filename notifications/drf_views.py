1c8c894c28da13088ad42a37d1c1f75cb0a9c037
Derek Lieu <dl@dereklieu.com>
2018-06-05 22:40:57 -0400

    Surge alert serializer sends no message when un-authenticated

--- notifications/drf_views.py
+++ notifications/drf_views.py
@@ -1,10 +1,19 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
 from .models import SurgeAlert, Subscription
-from .serializers import SurgeAlertSerializer, SubscriptionSerializer
+from .serializers import (
+    SurgeAlertSerializer,
+    UnauthenticatedSurgeAlertSerializer,
+    SubscriptionSerializer,
+)
 
 class SurgeAlertViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
     queryset = SurgeAlert.objects.all()
-    serializer_class = SurgeAlertSerializer
+    def get_serializer_class(self):
+        if self.request.user.is_authenticated:
+            return SurgeAlertSerializer
+        return UnauthenticatedSurgeAlertSerializer
 
 class SubscriptionViewset(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
