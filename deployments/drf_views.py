876e241d846f91b95972ae78fce70556e5351e47
Derek Lieu <dl@dereklieu.com>
2018-06-06 14:36:16 -0400

    Allow list-style filters

--- deployments/drf_views.py
+++ deployments/drf_views.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django_filters import Filter, rest_framework as filters
 from rest_framework import viewsets
 from .models import (
     ERUOwner,
@@ -10,6 +12,7 @@ from .models import (
     Rdrt,
     RdrtPerson,
 )
+from api.models import Country
 from .serializers import (
     ERUOwnerSerializer,
     ERUSerializer,
@@ -20,17 +23,45 @@ from .serializers import (
     RdrtPersonSerializer,
 )
 
+# https://github.com/carltongibson/django-filter/issues/137#issuecomment-127073878
+class ListFilter(Filter):
+    def __init__(self, filter_value=lambda x: x, **kwargs):
+        super(ListFilter, self).__init__(**kwargs)
+        self.filter_value_fn = filter_value
+
+    def sanitize(self, value_list):
+        return [v for v in value_list if v != u'']
+
+    def filter(self, qs, value):
+        values = value.split(u",")
+        values = self.sanitize(values)
+        values = map(self.filter_value_fn, values)
+        f = Q()
+        for v in values:
+            kwargs = {self.name: v}
+            f = f|Q(**kwargs)
+        return qs.filter(f)
+
 class ERUOwnerViewset(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = ERUOwner.objects.all()
     serializer_class = ERUOwnerSerializer
+    ordering_fields = ('created_at', 'updated_at',)
+
+class ERUFilter(filters.FilterSet):
+    deployed_to__isnull = filters.BooleanFilter(name='deployed_to', lookup_expr='isnull')
+    deployed_to__in = ListFilter(name='deployed_to__id')
+    class Meta:
+        model = ERU
+        fields = ('available',)
 
 class ERUViewset(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = ERU.objects.all()
     serializer_class = ERUSerializer
+    filter_class = ERUFilter
 
 class HeopViewset(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (TokenAuthentication,)
