e53a960b9d7136acf1eb2cf7b10b1300d6febaea
Derek Lieu <dl@dereklieu.com>
2018-06-06 11:00:35 -0400

    Deployments should be private

--- deployments/drf_views.py
+++ deployments/drf_views.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import (
     ERUOwner,
@@ -19,29 +21,43 @@ from .serializers import (
 )
 
 class ERUOwnerViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = ERUOwner.objects.all()
     serializer_class = ERUOwnerSerializer
 
 class ERUViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = ERU.objects.all()
     serializer_class = ERUSerializer
 
 class HeopViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Heop.objects.all()
     serializer_class = HeopSerializer
 
 class FactViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Fact.objects.all()
     serializer_class = FactSerializer
 
 class RdrtViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Rdrt.objects.all()
     serializer_class = RdrtSerializer
 
 class FactPersonViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = FactPerson.objects.all()
     serializer_class = FactPersonSerializer
 
 class RdrtPersonViewset(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = RdrtPerson.objects.all()
     serializer_class = RdrtPersonSerializer
