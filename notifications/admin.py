97420db6ff160b9d66f7b791b6b666a32d8d63be
Derek Lieu <dl@dereklieu.com>
2018-06-20 11:40:09 +0200

    Use autocomplete fields for events in admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 import notifications.models as models
 
+class SurgeAlertAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('event',)
 
-admin.site.register(models.SurgeAlert)
+admin.site.register(models.SurgeAlert, SurgeAlertAdmin)
 admin.site.register(models.Subscription)
