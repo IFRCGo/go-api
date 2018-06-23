3e13cf92c84840520536a50375ee43bc0c0ca808
Derek Lieu <dl@dereklieu.com>
2018-06-23 17:21:33 +0200

    Use one generic mapping

--- api/indexes.py
+++ api/indexes.py
@@ -1,31 +1,8 @@
-EventPageMapping = {
+GenericMapping = {
     'properties': {
         'id': {'type': 'keyword'},
         'name': {'type': 'text'},
-        'dtype': {'type': 'text'},
         'location': {'type': 'text'},
-        'summary': {'type': 'text'},
-        'date': {'type': 'date'},
-    }
-}
-
-AppealPageMapping = {
-    'properties': {
-        'id': {'type': 'keyword'},
-        'name': {'type': 'text'},
-        'dtype': {'type': 'text'},
-        'location': {'type': 'text'},
-        'date': {'type': 'date'},
-    }
-}
-
-ReportPageMapping = {
-    'properties': {
-        'id': {'type': 'text'},
-        'name': {'type': 'text'},
-        'dtype': {'type': 'text'},
-        'location': {'type': 'text'},
-        'summary': {'type': 'text'},
         'date': {'type': 'date'},
     }
 }
