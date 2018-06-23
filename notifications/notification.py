c959c462a6d0365d569869479c5a40b7ad0e4ef6
Derek Lieu <dl@dereklieu.com>
2018-06-23 18:20:07 +0200

    Use logger for email log output

--- notifications/notification.py
+++ notifications/notification.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from django.utils.html import strip_tags
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from api.logger import logger
 
 
 username = os.environ.get('EMAIL_USER')
@@ -26,16 +27,16 @@ class SendMail(threading.Thread):
             server.login(username, password)
             server.sendmail(username, self.recipients, self.msg.as_string())
             server.quit()
-            print('Notifications sent!')
+            logger.info('Notifications sent!')
         except SMTPAuthenticationError:
-            print('SMTPAuthenticationError')
-            print('Cannot send notification')
+            logger.error('SMTPAuthenticationError')
+            logger.error('Cannot send notification')
 
 
 def send_notification (subject, recipients, html):
     if not username or not password:
-        print('No EMAIL_USER and/or EMAIL_PASS set as environment variables')
-        print('Cannot send notification')
+        logger.warn('No EMAIL_USER and/or EMAIL_PASS set as environment variables')
+        logger.warn('Cannot send notification')
         return
 
     msg = MIMEMultipart('alternative')
