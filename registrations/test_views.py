7896fe9b383ea2dfe81dcf946382e11791a8746d
Szabo Zoltan <szabozoltan69@gmail.com>
2018-06-25 17:27:10 +0200

    Test of Two Gatekeepers, response status code (200) tests

--- registrations/test_views.py
+++ registrations/test_views.py
@@ -55,17 +55,20 @@ class TwoGatekeepersTest(APITestCase):
             'contact': [ {'email':'jo@arcs.org.af'},{'email':'ke@arcs.org.af'},]
         }
         headers = {'CONTENT_TYPE': 'application/json'}
-        response = self.client.post('/register', body, format='json', headers=headers).content
-        response = json.loads(response)
+        r = self.client.post('/register', body, format='json', headers=headers)
+        response = json.loads(r.content)
         verboseprint(response)
         self.assertIn('\'status\': \'ok\'',str(response))
+        self.assertEqual(r.status_code,200)
 
         verboseprint ('3a. Accessing the Pending users table to obtain the user\'s token')
         pending_user = Pending.objects.get(user__username=newusr)
         verboseprint(" Pending user username: ",pending_user.user)
         verboseprint(" Pending user key: ",pending_user.pk)
         verboseprint(" Pending user token: ",pending_user.token)
-        verboseprint(" Pending user admin_token: ",pending_user.admin_token)
+        verboseprint(" Pending user admin_token_1: ",pending_user.admin_token_1)
+        verboseprint(" Pending user admin_token_2: ",pending_user.admin_token_2)
+        verboseprint(" Pending user admin_validat_status: ",pending_user.admin_validat_status)
         verboseprint(" Pending user email: ",pending_user)
         if verbose:
             pp = pprint.PrettyPrinter(indent=4)
@@ -75,24 +78,45 @@ class TwoGatekeepersTest(APITestCase):
             'user': newusr,
             'token': pending_user.token,
         }
-        response = self.client.get('/verify_email', body1, format='json', headers=headers).content
+        r = self.client.get('/verify_email', body1, format='json', headers=headers)
+        response = r.content
         verboseprint(response[:999])
         self.assertIn('We are verifying your IFRC references and will notify you',str(response))
+        self.assertEqual(r.status_code,200)
 
         verboseprint ('5a. Confirming that a user without an official email is not activated')
         self.assertFalse(pending_user.user.is_active)
 
-        verboseprint ('6a. Using the admin token and new user username to query views.ValidateUser')
+        verboseprint ('6a_1. Using the first admin token and new user username to query views.ValidateUser')
         body2 = {
             'user': newusr,
-            'token': pending_user.admin_token
+            'token': pending_user.admin_token_1
         }
         verboseprint(body2)
-        response = self.client.get('/validate_user', body2, format='json', headers=headers).content
+        r = self.client.get('/validate_user', body2, format='json', headers=headers)
+        response=r.content
+        verboseprint(response[:999])
+        self.assertIn('The IFRC GO user account is still not active because an other administrator has to approve it also',str(response))
+        self.assertEqual(r.status_code,200)
+
+        verboseprint ('7a_1. Confirming that a user without an official email is STILL NOT activated')
+        boarded_user = User.objects.get(username=newusr)
+        verboseprint(boarded_user)
+        self.assertFalse(boarded_user.is_active)
+
+        verboseprint ('6a_2. Using the second admin token and new user username to query views.ValidateUser')
+        body3 = {
+            'user': newusr,
+            'token': pending_user.admin_token_2
+        }
+        verboseprint(body3)
+        r = self.client.get('/validate_user', body3, format='json', headers=headers)
+        response=r.content
         verboseprint(response[:999])
         self.assertIn('The IFRC GO user account is now active and a confirmation email has been sent to the new user',str(response))
-
-        verboseprint ('7a. Confirming that a user without an official email is activated')
+        self.assertEqual(r.status_code,200)
+        
+        verboseprint ('7a_2. Confirming that a user without an official email is finally ACTIVATED')
         boarded_user = User.objects.get(username=newusr)
         verboseprint(boarded_user)
         self.assertTrue(boarded_user.is_active)
@@ -114,17 +138,21 @@ class TwoGatekeepersTest(APITestCase):
             'lastname': 'Falk',
         }
         headers = {'CONTENT_TYPE': 'application/json'}
-        response = self.client.post('/register', body, format='json', headers=headers).content
+        r = self.client.post('/register', body, format='json', headers=headers)
+        response=r.content
         response = json.loads(response)
         verboseprint(response)
         self.assertIn('\'status\': \'ok\'',str(response))
+        self.assertEqual(r.status_code,200)
         
         verboseprint ('3b. Accessing the Pending users table to obtain the user\'s token')
         pending_user = Pending.objects.get(user__username=newusr)
         verboseprint(" Pending user username: ",pending_user.user)
         verboseprint(" Pending user key: ",pending_user.pk)
         verboseprint(" Pending user token: ",pending_user.token)
-        verboseprint(" Pending user admin_token: ",pending_user.admin_token)
+        verboseprint(" Pending user admin_token_1: ",pending_user.admin_token_1)
+        verboseprint(" Pending user admin_token_2: ",pending_user.admin_token_2)
+        verboseprint(" Pending user admin_validat_status: ",pending_user.admin_validat_status)
         verboseprint(" Pending user email: ",pending_user)
         if verbose:
             pp = pprint.PrettyPrinter(indent=4)
@@ -136,9 +164,11 @@ class TwoGatekeepersTest(APITestCase):
             'token': pending_user.token,
         }
         verboseprint(body1)
-        response = self.client.get('/verify_email', body1, format='json', headers=headers).content
+        r = self.client.get('/verify_email', body1, format='json', headers=headers)
+        response=r.content
         verboseprint(response[:111])
         self.assertIn('validated your email address and your IFRC Go account is now approved.',str(response))
+        self.assertEqual(r.status_code,200)
 
         verboseprint ('5b. Confirming that a user with an official email is activated')
         boarded_user = User.objects.get(username=newusr)
