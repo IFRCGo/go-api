4d375b6777580fb1ecb3f59e13c36599d60ece19
Derek Lieu <dl@dereklieu.com>
2018-06-27 00:56:15 +0200

    Apply a autocomplete analyzer to ES search

--- api/indexes.py
+++ api/indexes.py
@@ -3,8 +3,36 @@ GenericMapping = {
         'id': {'type': 'keyword'},
         'type': {'type': 'keyword'},
         'name': {'type': 'text'},
-        'body': { 'type': 'text'},
+        'body': {
+            'type': 'text',
+            'analyzer': 'autocomplete'
+        },
         'date': {'type': 'date'},
+    }
+}
+
+GenericSetting = {
+    'settings': {
+        'number_of_shards': 1,
+        'analysis': {
+            'filter': {
+                'autocomplete_filter': {
+                    'type': 'edge_ngram',
+                    'min_gram': 1,
+                    'max_gram': 20
+                }
+            },
+            'analyzer': {
+                'autocomplete': {
+                    'type': 'custom',
+                    'tokenizer': 'standard',
+                    'filter': [
+                        'lowercase',
+                        'autocomplete_filter'
+                    ]
+                }
+            }
+        }
     }
 }
 
