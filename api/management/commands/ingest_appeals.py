8d0054a196e8c9b26bb4a3bcc3776bf30401c569
Szabo Zoltan <szabozoltan69@gmail.com>
2018-10-12 10:09:18 +0200

    Using 3 years limit instead of forever

--- api/management/commands/ingest_appeals.py
+++ api/management/commands/ingest_appeals.py
@@ -42,12 +42,14 @@ class Command(BaseCommand):
             with open('appeals.json', 'w') as outfile:
                 json.dump(records, outfile)
 
+            in_3_years = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(days=1095)
             codes = [a.code for a in Appeal.objects.all()]
             for r in records:
                 if not r['APP_code'] in codes:
                     new.append(r)
-                # We use all records, do NOT check if last_modified > since_last_checked
-                modified.append(r)
+                last_modified = self.parse_date(r['APP_modifyTime'])
+                if last_modified > in_3_years:
+                    modified.append(r)
 
         return new, modified
 
