6cc94062d73a55dfd56ce1d0ac5828041fc4b4e0
Szabo Zoltan <szabozoltan69@gmail.com>
2019-04-16 15:04:29 +0200

    PER form data insertion via JSON POST

--- per/test_views.py
+++ per/test_views.py
@@ -11,6 +11,10 @@ class SmokeTest(APITestCase):
             'code': 'A1',
             'name': 'Nemo',
             'language': 1,
+            'unique_id': '1aad9295-ceb9-4ad5-9b10-84cc423e93f4',
+            'started_at': '2019-04-11 11:42:22.278796+00',
+            'submitted_at': '2019-04-11 09:42:52.278796+00',
+            'data': [{'id': '1.1', 'op': 3, 'nt': 'notes here'}, {'id': '1.2', 'op': 0, 'nt': 'notes here also'}]
         }
         headers = {'CONTENT_TYPE': 'application/json'}
         resp = self.client.post('/sendperform', body, format='json', headers=headers)
--- per/views.py
+++ per/views.py
@@ -1,4 +1,4 @@
-import json
+import json, datetime, pytz
 from django.http import JsonResponse, HttpResponse
 from api.views import (
     bad_request,
@@ -6,17 +6,34 @@ from api.views import (
     PublicJsonPostView,
     PublicJsonRequestView,
 )
-from .models import Form
+from .models import Form, FormData
 
 def create_form(raw):
 
-    form = Form.objects.create(code=raw['code'],
-                               name=raw['name'],
-                               language=raw['language'],
-                               ip_address=raw['ip_address'],
-                               finalized=False,
+    form = Form.objects.create(code         = raw['code'],
+                               name         = raw['name'],
+                               language     = raw['language'],
+                               ip_address   = raw['ip_address'],
+                               unique_id    = raw['unique_id'],
+                               started_at   = raw['started_at'],
+                               ended_at     = raw['ended_at'],
+                               submitted_at = raw['submitted_at'],
+                               comment      = raw['comment'],
+                               validated    = raw['validated'],
+                               finalized    = raw['finalized'],
                                )
+    form.save()
     return form
+
+def create_form_data(raw, form):
+
+    form_data = FormData.objects.create(form= form,
+                            question_id     = raw['id'],
+                            selected_option = raw['op'],
+                            notes           = raw['nt'],
+                            )
+    form_data.save()
+    return form_data
 
 class FormSent(PublicJsonPostView):
     def handle_post(self, request, *args, **kwargs):
@@ -34,6 +51,21 @@ class FormSent(PublicJsonPostView):
             'language',
         ]
 
+        currentDT = datetime.datetime.now(pytz.timezone('UTC'))
+        if 'started_at' not in body:
+            body['started_at']   = str(currentDT)
+        if 'ended_at' not in body:
+            body['ended_at']     = str(currentDT)
+        if 'submitted_at' not in body:
+            body['submitted_at'] = str(currentDT)
+        if 'comment' not in body:
+            body['comment'] = None
+        if 'validated' not in body:
+            body['validated'] = False
+        if 'finalized' not in body:
+            body['finalized'] = False
+
+
         missing_fields = [field for field in required_fields if field not in body]
         if len(missing_fields):
             return bad_request('Could not complete request. Please submit %s' % ', '.join(missing_fields))
@@ -46,6 +78,27 @@ class FormSent(PublicJsonPostView):
         except:
             return bad_request('Could not insert PER form record.')
 
-        form.save()
+        if 'data' in body:
+            for rubr in body['data']:
+                try:
+                    create_form_data(rubr, form)
+                except:
+                    return bad_request('Could not insert PER formdata record.')
 
         return JsonResponse({'status': 'ok'})
+
+# *** Test script from bash (similar to test_views.py, but remains in db):
+# curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{
+#            "code": "A1",
+#            "name": "Nemo",
+#            "language": 1,
+#            "unique_id": "1aad9295-ceb9-4ad5-9b10-84cc423e93f4",
+#            "started_at": "2019-04-11 11:42:22.278796+00",
+#            "submitted_at": "2019-04-11 09:42:52.278796+00",
+#            "data": [{"id": "1.1", "op": 3, "nt": "notes here"}, {"id": "1.2", "op": 0, "nt": "notes here also"}]
+#             }' \
+#  http://localhost:8000/sendperform
+#  *** Afterpurger:
+#  delete from per_formdata where notes like 'notes here%'; delete from per_form where name='Nemo';
