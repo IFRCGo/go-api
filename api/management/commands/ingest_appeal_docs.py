447c7bafaba10dee03f3071e9532d5c762ab0f27
Sanjay Bhangar <sanjaybhangar@gmail.com>
2019-05-23 14:45:47 +0530

    change logic of ingest_appeal_docs to only ingest appeals with 0 docs of the last 3 months by default, add a --fullscan flag to scan for all appeals

--- api/management/commands/ingest_appeal_docs.py
+++ api/management/commands/ingest_appeal_docs.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timezone
+from dateutil.relativedelta import relativedelta
 from urllib.request import urlopen
 import json
 from bs4 import BeautifulSoup
@@ -11,6 +12,14 @@ from api.logger import logger
 
 class Command(BaseCommand):
     help = 'Ingest existing appeal documents'
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--fullscan',
+            action='store_true',
+            help='Run a full scan on all appeals',
+        )
 
     def makelist(self, table):
         result = []
@@ -35,20 +44,34 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info('Starting appeal document ingest')
 
+        if options['fullscan']:
+            # If the `--fullscan` option is passed, check ALL appeals
+            print('Doing a full scan of all Appeals')
+            qset = Appeal.objects.all()
+        else:
+            # By default, only check appeals for the past 3 months where Appeal Documents is 0
+            now = datetime.now()
+            three_months_ago = now - relativedelta(months=3)
+            qset = Appeal.objects.filter(appealdocument__isnull=True).filter(end_date__gt=three_months_ago)
+
         # First get all Appeal Codes
-        appeal_codes = [a.code for a in Appeal.objects.all()]
+        appeal_codes = [a.code for a in qset]
 
         # Modify code taken from https://pastebin.com/ieMe9yPc to scrape `publications-and-reports` and find
         # Documents for each appeal code
         output = []
+        page_not_found = []
         for code in appeal_codes:
-            print(code)
             docs_url = 'http://www.ifrc.org/en/publications-and-reports/appeals/?ac='+code+'&at=0&c=&co=&dt=1&f=&re=&t=&ti=&zo='
-            response = urlopen(docs_url)
+            try:
+                response = urlopen(docs_url)
+            except: # if we get an error fetching page for an appeal, we ignore it
+                page_not_found.append(code)
+                continue
+
             soup = BeautifulSoup(response.read(), "lxml")
             div = soup.find('div', id='cw_content')
             for t in div.findAll('tbody'):
-                print(self.makelist(t))
                 output = output + self.makelist(t)
 
         # Once we have all Documents in output, we add all missing Documents to the associated Appeal
@@ -58,6 +81,7 @@ class Command(BaseCommand):
 
         acodes = list(set([a[2] for a in output]))
         for code in acodes:
+            code = code.replace(' ', '') # some codes have spaces, remove them
             try:
                 appeal = Appeal.objects.get(code=code)
             except ObjectDoesNotExist:
@@ -85,4 +109,5 @@ class Command(BaseCommand):
                     created.append(doc[0])
         logger.info('%s appeal documents created' % len(created))
         logger.info('%s existing appeal documents' % len(existing))
+        logger.info('%s pages not found for appeal' % len(page_not_found))
         logger.warn('%s documents without appeals in system' % len(not_found))
