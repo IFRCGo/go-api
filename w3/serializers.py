dc223f73f5de64205ab418832b901a3afd33a67e
Szabo Zoltan <szabozoltan69@gmail.com>
2019-06-26 19:05:31 +0200

    First end-points and tests for Project API part

--- w3/serializers.py
+++ w3/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Currencies
 
 class ProjectSerializer(serializers.ModelSerializer):
+    budget_currency = Currencies.toJSON(Project.budget_currency)
     class Meta:
         model = Project
+        fields = '__all__'
--- w3/views.py
+++ w3/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import json, datetime, pytz
+from django.http import JsonResponse
+from api.views import (
+    bad_request,
+    bad_http_request,
+    PublicJsonPostView,
+    PublicJsonRequestView,
+)
+from .models import (
+    Project
+)
 
 # Create your views here.
+
+class ProjectSent(PublicJsonPostView):
+    def handle_post(self, request, *args, **kwargs):
+
+        body = json.loads(request.body.decode('utf-8'))
+
+        return JsonResponse({'status': 'ok'})
