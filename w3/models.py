508bf45e047639111010c8692e265375c78c90c1
Sanjay Bhangar <sanjaybhangar@gmail.com>
2019-06-26 16:50:53 +0530

    add w3 to installed_apps, add on_delete to FK relations in model

--- w3/models.py
+++ w3/models.py
@@ -22,8 +22,8 @@ class Currencies(Enum):
 
 class Project(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL) # user who created this project
-    reporting_ns = models.ForeignKey(Country) # this is the national society that is reporting the project
-    project_district = models.ForeignKey(District) # this is the district where the project is actually taking place
+    reporting_ns = models.ForeignKey(Country, on_delete=models.CASCADE) # this is the national society that is reporting the project
+    project_district = models.ForeignKey(District, on_delete=models.CASCADE) # this is the district where the project is actually taking place
     name = models.TextField()
     programme_type = EnumIntegerField(ProgrammeTypes)
     sector = EnumIntegerField(Sectors)
