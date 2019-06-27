bef41e1de631e7e32ed2a27b6abf76211c0b4f0b
Sanjay Bhangar <sanjaybhangar@gmail.com>
2019-06-27 16:53:58 +0530

    oops, forgot to commit changes to deployments code

--- deployments/test_views.py
+++ deployments/test_views.py
@@ -15,14 +15,14 @@ class ProjectGetTest(APITestCase):
         user.set_password(password)
         user.save()
 
-        country1 = Country.objects.create(name='country1')
+        country1 = Country.objects.create(name='country1', iso='XX')
         country1.save()
-        country2 = Country.objects.create(name='country2')
+        country2 = Country.objects.create(name='country2', iso='YY')
         country2.save()
 
-        district1 = District.objects.create(name='district1')
+        district1 = District.objects.create(name='district1', country=country1)
         district1.save
-        district2 = District.objects.create(name='district2')
+        district2 = District.objects.create(name='district2', country=country2)
         district2.save
 
         first = Project.objects.create(
@@ -37,6 +37,20 @@ class ProjectGetTest(APITestCase):
                     ,budget_amount    = 6000
                     ,status           = 0)
         first.save()
+
+        second = Project.objects.create(
+                     user             = user
+                    ,reporting_ns     = country1
+                    ,project_district = district2
+                    ,name             = 'bbb'
+                    ,programme_type   = 1
+                    ,sector           = 1
+                    ,start_date       = '2012-12-12'
+                    ,end_date         = '2013-01-01'
+                    ,budget_amount    = 3000
+                    ,status           = 1)
+
+        second.save()
 
     def test_1(self):
         user = User.objects.get(username=username)
@@ -82,4 +96,12 @@ class ProjectGetTest(APITestCase):
         self.client.force_authenticate(user=user, token=token)
         resp = self.client.post('/api/v2/create_project/', body, format='json')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(len(Project.objects.all()), 2) # we created 2 projects until now here
+        self.assertEqual(len(Project.objects.all()), 3) # we created 2 projects until now here
+
+    # def test_get_projects(self):
+    #     resp = self.client.get('/api/v2/project/', format='json')
+    #     self.assertEqual(resp.status_code, 200)
+    #     data = json.loads(resp.content)
+    #     print(data)
+
+
