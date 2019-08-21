9bb8ae80bd25867a732981a35197d5586b6d8982
Szabo Zoltan <szabozoltan69@gmail.com>
2019-08-21 19:12:43 +0200

    Nicer frontend URL

--- main/frontend.py
+++ main/frontend.py
@@ -1,3 +1,6 @@
 import os
 
 frontend_url = os.environ.get('FRONTEND_URL')
+if frontend_url == 'prddsgoproxyapp.azurewebsites.net':
+    # We use a nicer frontend URL:
+    frontend_url = 'go.ifrc.org'
