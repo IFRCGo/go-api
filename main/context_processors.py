c4113e1b796ea1c41af4598c97f9c2517f8e5414
Szabo Zoltan <szabozoltan69@gmail.com>
2019-10-24 15:50:40 +0200

    Upgrading Pillow due to a vulnerability and some IS_STAGING code refactoring

--- main/context_processors.py
+++ main/context_processors.py
@@ -5,4 +5,5 @@ def ifrc_go(request):
     return {
         # Provide a variable to define current environment
         'PRODUCTION_URL': settings.PRODUCTION_URL,
+        'IS_STAGING': True if settings.PRODUCTION_URL == 'dsgocdnapi.azureedge.net' else False,
     }
