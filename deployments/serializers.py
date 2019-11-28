c5f459fb2828f1924503f5eecea0f30b38bde245
thenav56 <ayernavin@togglecorp.com>
2019-11-28 14:07:20 +0545

    3W Feedback Changes
    * Changes to Sectors and Tagging options.
    * Rename "Long term Operation" to "Programme"
    * Using Custom Widget for EnumFields

--- deployments/serializers.py
+++ deployments/serializers.py
@@ -25,54 +25,71 @@ from api.serializers import (
     MiniDistrictSerializer,
 )
 
+
 class ERUSetSerializer(serializers.ModelSerializer):
     deployed_to = MiniCountrySerializer()
+
     class Meta:
         model = ERU
         fields = ('type', 'units', 'equipment_units', 'deployed_to', 'event', 'eru_owner', 'available', 'id',)
 
+
 class ERUOwnerSerializer(serializers.ModelSerializer):
     eru_set = ERUSetSerializer(many=True)
     national_society_country = MiniCountrySerializer()
+
     class Meta:
         model = ERUOwner
         fields = ('created_at', 'updated_at', 'national_society_country', 'eru_set', 'id',)
+
 
 class ERUSerializer(serializers.ModelSerializer):
     deployed_to = MiniCountrySerializer()
     event = ListEventSerializer()
     eru_owner = ERUOwnerSerializer()
+
     class Meta:
         model = ERU
         fields = ('type', 'units', 'equipment_units', 'deployed_to', 'event', 'eru_owner', 'available', 'id',)
 
+
 class PersonnelDeploymentSerializer(serializers.ModelSerializer):
     country_deployed_to = MiniCountrySerializer()
     event_deployed_to = ListEventSerializer()
+
     class Meta:
         model = PersonnelDeployment
         fields = ('country_deployed_to', 'region_deployed_to', 'event_deployed_to', 'comments', 'id',)
 
+
 class PersonnelSerializer(serializers.ModelSerializer):
     country_from = MiniCountrySerializer()
     deployment = PersonnelDeploymentSerializer()
+
     class Meta:
         model = Personnel
         fields = ('start_date', 'end_date', 'name', 'role', 'type', 'country_from', 'deployment', 'id',)
 
+
 class PartnerDeploymentActivitySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PartnerSocietyActivities
         fields = ('activity', 'id',)
+
 
 class PartnerDeploymentSerializer(serializers.ModelSerializer):
     parent_society = MiniCountrySerializer()
     country_deployed_to = MiniCountrySerializer()
     district_deployed_to = MiniDistrictSerializer(many=True)
     activity = PartnerDeploymentActivitySerializer()
+
     class Meta:
         model = PartnerSocietyDeployment
-        fields = ('start_date', 'end_date', 'name', 'role', 'parent_society', 'country_deployed_to', 'district_deployed_to', 'activity', 'id',)
+        fields = (
+            'start_date', 'end_date', 'name', 'role', 'parent_society', 'country_deployed_to',
+            'district_deployed_to', 'activity', 'id',
+        )
 
 
 class RegionalProjectSerializer(serializers.ModelSerializer):
@@ -102,9 +119,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if data['status'] == Statuses.COMPLETED and data.get('reached_total') is None:
             raise serializers.ValidationError('Reached total should be provided if status is completed')
-        elif data['operation_type'] == OperationTypes.LONG_TERM_OPERATION and data.get('dtype') is None:
+        elif data['operation_type'] == OperationTypes.PROGRAMME and data.get('dtype') is None:
             raise serializers.ValidationError(
-                'Disaster Type should be provided if operation type is Long Term Operation'
+                'Disaster Type should be provided if operation type is Programme'
             )
         elif (
             data['operation_type'] == OperationTypes.EMERGENCY_OPERATION and
