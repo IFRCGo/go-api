04ca6e9c4b3d615e319a4f9f7708f3141097652b
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-02-05 16:32:42 +0100

    Removed delete action from AuthLog

--- api/admin.py
+++ api/admin.py
@@ -898,6 +898,12 @@ class AuthLogAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
 
 admin.site.register(models.DisasterType, DisasterTypeAdmin)
 admin.site.register(models.Event, EventAdmin)
