24c3486111ed9e470196e2bcd2139540a68eefdb
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-02-06 14:58:31 +0100

    Implement reversion into all the admin pages

--- notifications/admin.py
+++ notifications/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 import notifications.models as models
 from api.admin_classes import RegionRestrictedAdmin
 from django_admin_listfilter_dropdown.filters import ChoiceDropdownFilter
+from reversion.admin import VersionAdmin
+from reversion.models import Revision
+from reversion_compare.admin import CompareVersionAdmin
 
 
-class SurgeAlertAdmin(RegionRestrictedAdmin):
+class SurgeAlertAdmin(CompareVersionAdmin, RegionRestrictedAdmin):
     country_in = 'event__countries__in'
     region_in = 'event__regions__in'
     autocomplete_fields = ('event',)
--- registrations/admin.py
+++ registrations/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 import registrations.models as models
+from reversion.admin import VersionAdmin
+from reversion.models import Revision
+from reversion_compare.admin import CompareVersionAdmin
 
 
-class PendingAdmin(admin.ModelAdmin):
+class PendingAdmin(CompareVersionAdmin):
     search_fields = ('user__username', 'user__email', 'admin_contact_1', 'admin_contact_2')
     list_display = ('get_username_and_mail', 'created_at', 'admin_contact_1', 'admin_1_validated', 'admin_1_validated_date', 'admin_contact_2', 'admin_2_validated', 'admin_2_validated_date',)
 
