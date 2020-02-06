3338516349b373e43972cf534fc5587883182ee5
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-02-06 10:36:06 +0100

    Added validated_date 1-2 to Pending regs.

--- registrations/admin.py
+++ registrations/admin.py
@@ -4,7 +4,7 @@ import registrations.models as models
 
 class PendingAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'user__email', 'admin_contact_1', 'admin_contact_2')
-    list_display = ('get_username_and_mail', 'created_at', 'admin_contact_1', 'admin_1_validated', 'admin_contact_2', 'admin_2_validated',)
+    list_display = ('get_username_and_mail', 'created_at', 'admin_contact_1', 'admin_1_validated', 'admin_1_validated_date', 'admin_contact_2', 'admin_2_validated', 'admin_2_validated_date',)
 
     def get_username_and_mail(self, obj):
         return obj.user.username + ' - ' + obj.user.email
