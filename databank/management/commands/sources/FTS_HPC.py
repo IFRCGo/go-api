29d8d5eed90ddd3eb3cc2f23a8961fa3ba2e6880
thenav56 <ayernavin@togglecorp.com>
2020-03-17 15:44:07 +0545

    Fetch data from FTS Google sheet

--- databank/management/commands/sources/FTS_HPC.py
+++ databank/management/commands/sources/FTS_HPC.py
@@ -1,3 +1,5 @@
+import csv
+import io
 import datetime
 import requests
 
@@ -10,6 +12,7 @@ from api.models import CronJob, CronJobStatus
 
 FTS_URL = 'https://api.hpc.tools/v1/public/fts/flow?countryISO3={0}&groupby=year&report=3'
 EMERGENCY_URL = 'https://api.hpc.tools/v1/public/emergency/country/{0}'
+GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1MArQSVdbLXLaQ8ixUKo9jIjifTCVDDxTJYbGoRuw3Vw/gviz/tq?tqx=out:csv'
 
 HEADERS = {
     # TODO: USE Crendentils here
@@ -18,7 +21,40 @@ HEADERS = {
 
 
 @catch_error()
-def load(country, overview, _):
+def prefetch():
+    g_sheet_data = requests.get(GOOGLE_SHEET_URL, headers=HEADERS)
+
+    if g_sheet_data.status_code != 200:
+        CronJob.sync_cron({
+            "name": "FTS_HPC",
+            "message": f"Error querying HPC google sheet data feed at {GOOGLE_SHEET_URL}",
+            "status": CronJobStatus.ERRONEOUS,
+        })  # not every case is catched here, e.g. if the base URL is wrong...
+        return {}
+
+    g_sheet_data = list(csv.DictReader(io.StringIO(g_sheet_data.text)))
+
+    gho_data = {
+        f"{d['Country #country+code'].upper()}-{d['Year #date+year']}": {
+            'people_in_need': d['PIN #inneed'],
+            'people_targeted': d['PT #targeted'],
+            'funding_total_usd': d['Funding #value+funding+total+usd'],
+            'funding_required_usd': d['Requirements #value+funding+required+usd'],
+        }
+        for d in g_sheet_data
+    }
+
+    CronJob.sync_cron({
+        "name": "FDRS",
+        "message": f"Done querying FDRS NS HPC google sheet feed at {GOOGLE_SHEET_URL}",
+        "num_result": len(gho_data),
+        "status": CronJobStatus.SUCCESSFUL,
+    })
+    return gho_data
+
+
+@catch_error()
+def load(country, overview, gho_data):
     pcountry = get_country_by_iso2(country.iso)
     if pcountry is None:
         return
@@ -39,7 +75,7 @@ def load(country, overview, _):
     c_data = {}
 
     # fundingTotals, pledgeTotals
-    for fund_area in ['fundingTotals', 'pledgeTotals']:
+    for fund_area, fund_area_s in [('fundingTotals', 'funding_totals'), ('pledgeTotals', 'pledge_totals')]:
         fund_area_data = fts_data['data']['report3'][fund_area]['objects']
         if len(fund_area_data) > 0:
             for v in fund_area_data[0]['objectsBreakdown']:
@@ -49,9 +85,9 @@ def load(country, overview, _):
                 except ValueError:
                     continue
                 if year not in c_data:
-                    c_data[year] = {fund_area: totalFunding}
+                    c_data[year] = {fund_area_s: totalFunding}
                 else:
-                    c_data[year][fund_area] = totalFunding
+                    c_data[year][fund_area_s] = totalFunding
 
     # numActivations
     CronJobSum = 0
@@ -73,6 +109,7 @@ def load(country, overview, _):
         {
             'year': year,
             **values,
+            **gho_data.get(f"{pcountry.alpha_3.upper()}-{year}", {}),
         }
         for year, values in c_data.items()
     ]
