d06b770b07f888939d5874b1aaa0559d0532546e
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-04-06 11:11:56 +0200

    Filter mini_events too by parent_event

--- api/drf_views.py
+++ api/drf_views.py
@@ -243,7 +243,7 @@ class EventViewset(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         if self.action == 'mini_events':
-            return Event.objects.values('id', 'name')
+            return Event.objects.filter(parent_event__isnull=True).values('id', 'name')
         return Event.objects.filter(parent_event__isnull=True)
 
     def get_serializer_class(self):
