3d3bb66c348ac5520bb529b463c233c01b6368b6
thenav56 <ayernavin@togglecorp.com>
2020-04-14 21:19:21 +0545

    Clean up data for project import

--- deployments/forms.py
+++ deployments/forms.py
@@ -1,6 +1,6 @@
 import io
+import dateutil.parser
 import traceback
-import datetime
 import csv
 
 from django import forms
@@ -42,8 +42,19 @@ class ProjectImportForm(forms.Form):
     file = forms.FileField(widget=forms.FileInput(attrs={'accept': '.csv'}))
 
     def _handle_bulk_upload(self, user, file):
-        def strip_date(date):
-            return datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        def key_clean(string):
+            return string.lower().strip()
+
+        def parse_date(date):
+            return dateutil.parser.parse(date)
+
+        def parse_integer(integer):
+            try:
+                if isinstance(integer, str):
+                    return int(integer.replace(',', ''))
+                return integer
+            except ValueError:
+                return None
 
         file.seek(0)
         reader = csv.DictReader(io.StringIO(file.read().decode('utf-8', errors='ignore')), skipinitialspace=True)
@@ -58,38 +69,50 @@ class ProjectImportForm(forms.Form):
 
         for row in reader:
             # TODO: Try to do this in a single query
-            reporting_ns = Country.objects.filter(name__iexact=row['Supporting NS'].strip()).first()
-            project_district = District.objects.filter(
-                country__name__iexact=row['Country'].strip(),
-                name__iexact=row['Region'].strip(),
-            ).first()
+            district_name = row['Region'].strip()
+            reporting_ns_name = row['Supporting NS'].strip()
+            country_name = row['Country'].strip()
+
+            reporting_ns = Country.objects.filter(name__iexact=reporting_ns_name).first()
+            if district_name.lower() in ['countrywide', '']:
+                project_country = Country.objects.filter(name__iexact=country_name).first()
+                project_district = None
+            else:
+                project_district = District.objects.filter(
+                    country__name__iexact=country_name,
+                    name__iexact=district_name,
+                ).first()
+                project_country = project_district.country
 
             projects.append(Project(
                 user=user,
                 reporting_ns=reporting_ns,
                 project_district=project_district,
+                project_country=project_country,
 
                 # Enum fields
-                operation_type=operation_types[row['Operation type'].lower().strip()],
-                programme_type=programme_types[row['Programme Type'].lower().strip()],
-                primary_sector=sectors[row['Primary Sector'].lower().strip()],
-                secondary_sectors=[sector_tags[tag.lower().strip()] for tag in row['Tags'].split(',')],
-                status=statuses[row['Status'].lower().strip()],
+                operation_type=operation_types[key_clean(row['Operation type'])],
+                programme_type=programme_types[key_clean(row['Programme Type'])],
+                primary_sector=sectors[key_clean(row['Primary Sector'])],
+                secondary_sectors=[
+                    sector_tags[key_clean(tag)] for tag in row['Tags'].split(',') if key_clean(tag) in sector_tags
+                ],
+                status=statuses[key_clean(row['Status'])],
 
                 name=row['Project Name'],
-                start_date=strip_date(row['Start Date']),
-                end_date=strip_date(row['End Date']),
+                start_date=parse_date(row['Start Date']),
+                end_date=parse_date(row['End Date']),
                 budget_amount=row['Budget(CHF)'],
 
                 # Optional fields
-                target_male=row['Targeted Males'],
-                target_female=row['Targeted Females'],
-                target_other=row['Targeted Others'],
-                target_total=row['Targeted Total'],
-                reached_male=row['Reached Males'],
-                reached_female=row['Reached Females'],
-                reached_other=row['Reached Others'],
-                reached_total=row['Reached Total'],
+                target_male=parse_integer(row['Targeted Males']),
+                target_female=parse_integer(row['Targeted Females']),
+                target_other=parse_integer(row['Targeted Others']),
+                target_total=parse_integer(row['Targeted Total']),
+                reached_male=parse_integer(row['Reached Males']),
+                reached_female=parse_integer(row['Reached Females']),
+                reached_other=parse_integer(row['Reached Others']),
+                reached_total=parse_integer(row['Reached Total']),
             ))
         return Project.objects.bulk_create(projects)
 
