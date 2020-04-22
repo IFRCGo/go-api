24f25bd4d73c4d15fa7f6110bb7f0b205f93895f
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-04-22 19:11:50 +0200

    is_valid_domain use DB instead of static list

--- registrations/views.py
+++ registrations/views.py
@@ -1,6 +1,7 @@
 import json
 import pytz
 from django.db.models import Q
+from django.db.models.functions import Lower
 from datetime import timedelta, datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
@@ -15,178 +16,20 @@ from api.views import (
     PublicJsonRequestView,
 )
 from api.models import Country
-from .models import Pending
+from .models import Pending, DomainWhitelist
 from notifications.notification import send_notification
 from main.frontend import frontend_url
 
 
 def is_valid_domain(email):
     domain = email.lower().split('@')[1]
-    allowed_list = [
-        'ifrc.org',
-        'apdisasterresilience.org',
-        'arabrcrc.org',
-        'arcs.org.af',
-        'bdrcs.org',
-        'brcs.bt',
-        'cck-cr.cz',
-        'ckcg.me',
-        'climatecentre.org',
-        'creuroja.ad',
-        'cri.it',
-        'croix-rouge.be',
-        'croix-rouge.fr',
-        'croix-rouge.lu',
-        'croix-rouge.mc',
-        'croixrouge.ht',
-        'croixrouge-mali.org',
-        'croixrougesenegal.com',
-        'crucearosie.ro',
-        'cruzroja.cl',
-        'cruzroja.es',
-        'cruzroja.gt',
-        'cruzroja.or.cr',
-        'cruzroja.org',
-        'cruzroja.org.ar',
-        'cruzroja.org.hn',
-        'cruzroja.org.pa',
-        'cruzroja.org.pe',
-        'cruzroja.org.py',
-        'cruzrojaboliviana.org',
-        'cruzrojacolombiana.org',
-        'cruzrojamexicana.org.mx',
-        'cruzrojanicaraguense.org',
-        'cruzrojasal.org.sv',
-        'cruzrojauruguaya.org',
-        'cruzrojavenezolana.org',
-        'cruzvermelha.org.br',
-        'cruzvermelha.org.pt',
-        'drk.de',
-        'drk.dk',
-        'egyptianrc.org',
-        'fijiredcross.org',
-        'germanredcross.de',
-        'grsproadsafety.org',
-        'guatemala.cruzroja.org',
-        'hck.hr',
-        'honduras.cruzroja.org',
-        'icrc.org',
-        'indianredcross.org',
-        'jamaicaredcross.org',
-        'jnrcs.org',
-        'jrc.or.jp',
-        'kenyaredcross.org',
-        'kizilay.org.tr',
-        'krcs.org.kw',
-        'laoredcross.org.la',
-        'livelihoodscentre.org',
-        'mauritiusredcross.com',
-        'mdais.org',
-        'mda.org.il',
-        'nrcs.org',
-        'palestinercs.org',
-        'pck.org.pl',
-        'pck.pl',
-        'pmi.or.id',
-        'prcs.org.pk',
-        'pscentre.org',
-        'qrcs.org.qa',
-        'rcs.ir',
-        'rcsbahrain.org',
-        'rcuae.ae',
-        'redcrescent.az',
-        'redcrescent.kz',
-        'redcrescent.org.mv',
-        'redcrescent.org.my',
-        'redcrescent.tj',
-        'redcrescent.uz',
-        'redcross-eu.net',
-        'redcross.am',
-        'redcross.be',
-        'redcross.bg',
-        'redcross.by',
-        'redcross.ca',
-        'redcross.ch',
-        'redcross.ee',
-        'redcross.fi',
-        'redcross.ge',
-        'redcross.gr',
-        'redcross.ie',
-        'redcross.int',
-        'redcross.is',
-        'redcross.lk',
-        'redcross.lt',
-        'redcross.lv',
-        'redcross.md',
-        'redcross.mn',
-        'redcross.no',
-        'redcross.or.ke',
-        'redcross.or.kr',
-        'redcross.or.th',
-        'redcross.org',
-        'redcross.org.au',
-        'redcross.org.ck',
-        'redcross.org.cn',
-        'redcross.org.cy',
-        'redcross.org.hk',
-        'redcross.org.jm',
-        'redcross.org.kh',
-        'redcross.org.lb',
-        'redcross.org.mk',
-        'redcross.org.mm',
-        'redcross.org.mo',
-        'redcross.org.mt',
-        'redcross.org.mz',
-        'redcross.org.na',
-        'redcross.org.nz',
-        'redcross.org.ph',
-        'redcross.org.rs',
-        'redcross.org.sg',
-        'redcross.org.ua',
-        'redcross.org.uk',
-        'redcross.org.vn',
-        'redcross.org.za',
-        'redcross.org.zm',
-        'redcross.ru',
-        'redcross.se',
-        'redcross.sk',
-        'redcross.tl',
-        'redcrossghana.org',
-        'redcrosseth.org',
-        'redcrossmuseum.ch',
-        'redcrossug.org',
-        'redcrosszim.org.zw',
-        'rks.si',
-        'rmiredcross.org',
-        'rodekruis.be',
-        'roteskreuz.be',
-        'rodekors.dk',
-        'rodekruis.nl',
-        'redcross.nl',
-        'roteskreuz.at',
-        'st.roteskreuz.at', # Steiermark (Styria)
-        's.roteskreuz.at',  # Salzburg
-        'b.roteskreuz.at',  # Burgenland
-        't.roteskreuz.at',  # Tirol (Tyrol)
-        'w.roteskreuz.at',  # Wien (Vienna)
-        'v.roteskreuz.at',  # Vorarlberg
-        'k.roteskreuz.at',  # Kaernten (Carinthia)
-        'n.roteskreuz.at',  # Niederoesterreich (lower Austria)
-        'o.roteskreuz.at',  # Oberoesterreich (upper Austria)
-        'roteskreuz-innsbruck.at',
-        'roteskreuz.li',
-        'rwandaredcross.org',
-        'sarc.sy',
-        'sarc-sy.org',
-        'sierraleoneredcross.org',
-        'srcs.sd',
-        'standcom.ch',
-        'tongaredcross.to',
-        'trcs.or.tz',
-        'vanuaturedcross.org',
-        'redcrossvanuatu.com',
-        'voroskereszt.hu' # no comma
-    ]
+    allowed_list = (
+        DomainWhitelist.objects
+            .filter(is_active=True)
+            .values_list('domain_name', flat=True)
+            .annotate(domain_name_lower=Lower('domain_name'))
+    )
+    
     if domain in allowed_list:
         return True
     return False
