28e509011c824ca92f03400d3ea5579c8f870309
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-05-19 12:13:48 +0200

    email_verified to Pending Regs. list

--- registrations/admin.py
+++ registrations/admin.py
@@ -7,7 +7,9 @@ from reversion_compare.admin import CompareVersionAdmin
 
 class PendingAdmin(CompareVersionAdmin):
     search_fields = ('user__username', 'user__email', 'admin_contact_1', 'admin_contact_2')
-    list_display = ('get_username_and_mail', 'created_at', 'admin_contact_1', 'admin_1_validated', 'admin_1_validated_date', 'admin_contact_2', 'admin_2_validated', 'admin_2_validated_date',)
+    list_display = ('get_username_and_mail', 'created_at',
+                    'admin_contact_1', 'admin_1_validated', 'admin_1_validated_date',
+                    'admin_contact_2', 'admin_2_validated', 'admin_2_validated_date', 'email_verified',)
 
     def get_username_and_mail(self, obj):
         return obj.user.username + ' - ' + obj.user.email
