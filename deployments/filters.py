1eb8132894f19f30c8e7180a221b9aefe167e3ac
thenav56 <ayernavin@togglecorp.com>
2020-05-26 11:40:44 +0545

    Add Global National Society Activities API
    - Change filter for project
        - `country` supports multiple value seperated by comma
        - `region` supports multiple value seperated by comma

--- deployments/filters.py
+++ deployments/filters.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import django_filters as filters
 from django.db.models import Q
 
@@ -14,8 +15,9 @@ from .models import (
 
 class ProjectFilter(filters.FilterSet):
     budget_amount = filters.NumberFilter(field_name='budget_amount', lookup_expr='exact')
-    country = filters.CharFilter(label='Country ISO/ISO3', field_name='country', method='filter_country')
-    region = filters.ModelChoiceFilter(label='Region', queryset=Region.objects.all(), method='filter_region')
+    country = filters.CharFilter(label='Country ISO/ISO3', field_name='country', method='filter_countries')
+    region = filters.ModelMultipleChoiceFilter(
+        label='Region', queryset=Region.objects.all(), widget=filters.widgets.CSVWidget, method='filter_regions')
     operation_type = filters.MultipleChoiceFilter(choices=OperationTypes.choices(), widget=filters.widgets.CSVWidget)
     programme_type = filters.MultipleChoiceFilter(choices=ProgrammeTypes.choices(), widget=filters.widgets.CSVWidget)
     primary_sector = filters.MultipleChoiceFilter(choices=Sectors.choices(), widget=filters.widgets.CSVWidget)
@@ -33,27 +35,32 @@ class ProjectFilter(filters.FilterSet):
             return queryset.filter(secondary_sectors__overlap=value)
         return queryset
 
-    def filter_country(self, queryset, name, value):
-        if value:
+    def filter_countries(self, queryset, name, countries):
+        countries = countries.split(',')
+        if len(countries):
             return queryset.filter(
-                # ISO2
-                Q(project_country__iso__iexact=value) |
-                Q(project_districts__country__iso__iexact=value) |
-                # ISO3
-                Q(project_country__iso3__iexact=value) |
-                Q(project_districts__country__iso3__iexact=value)
+                reduce(
+                    lambda acc, item: acc | item,
+                    [
+                        (
+                            # ISO2
+                            Q(project_country__iso__iexact=country) |
+                            Q(project_districts__country__iso__iexact=country) |
+                            # ISO3
+                            Q(project_country__iso3__iexact=country) |
+                            Q(project_districts__country__iso3__iexact=country)
+                        )
+                        for country in countries
+                    ]
+                )
             ).distinct()
         return queryset
 
-    def filter_region(self, queryset, name, region):
-        if region:
+    def filter_regions(self, queryset, name, regions):
+        if len(regions):
             return queryset.filter(
-                # ISO2
-                Q(project_country__region=region) |
-                Q(project_districts__country__region=region) |
-                # ISO3
-                Q(project_country__region=region) |
-                Q(project_districts__country__region=region)
+                Q(project_country__region__in=regions) |
+                Q(project_districts__country__region__in=regions)
             ).distinct()
         return queryset
 
