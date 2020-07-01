e1acf06af4adcdf33b4190a4b3a1ef5d924b5d61
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-07-01 14:07:57 +0200

    Added error icon back, fixed query and css

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,18 +1,15 @@
 from django.conf import settings
-# from api.models import CronJob, CronJobStatus
+from api.models import CronJob, CronJobStatus
 
 
 def ifrc_go(request):
-    # having_ingest_issue = CronJob.objects.raw('SELECT * FROM api_cronjob WHERE status=' + str(CronJobStatus.ERRONEOUS.value))
-    # ingest_issue_id = having_ingest_issue[0].id if len(having_ingest_issue) > 0 else -1
+    cron_error = CronJob.objects.filter(status=CronJobStatus.ERRONEOUS).order_by('-id').first()
     return {
         # Provide a variable to define current environment
         'PRODUCTION_URL': settings.PRODUCTION_URL,
         'HIDE_LANGUAGE_UI': settings.HIDE_LANGUAGE_UI,
         'IS_STAGING': True if settings.PRODUCTION_URL == 'dsgocdnapi.azureedge.net' else False,
-        # to be used later in api/templates/admin/base_site.html # True if len(having_ingest_issue) > 0 else False,
-        'HAVING_INGEST_ISSUE': False,
-        # 'INGEST_ISSUE_ID': ingest_issue_id,
+        # For header /_!_\ error symbol in base_site.html
+        'HAVING_INGEST_ISSUE': True if cron_error else False,
+        'INGEST_ISSUE_ID': cron_error.id if cron_error else None,
     }
-
-#  These can be uncommented if the IM Team decides to have the ! signal in case of ingestion issue on the backend top banner
