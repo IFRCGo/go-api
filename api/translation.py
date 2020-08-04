8c0e6b680c4d2481ca8b5e806cb38c1ac54fe061
thenav56 <ayernavin@togglecorp.com>
2020-08-04 11:56:01 +0545

    Add model translation to api/ models.

--- api/translation.py
+++ api/translation.py
@@ -1,9 +1,82 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import FieldReport
+from .models import (
+    Action,
+    ActionsTaken,
+    Appeal,
+    AppealDocument,
+    Country,
+    CountrySnippet,
+    DisasterType,
+    Event,
+    FieldReport,
+    GDACSEvent,
+    RegionSnippet,
+    SituationReport,
+    Snippet,
+)
 
 
 # Field Report Translation Options
 @register(FieldReport)
 class FieldReportTO(TranslationOptions):
-    pass
-    # fields = ('summary', 'description')
+    fields = ('summary', 'description', 'actions_others', 'other_sources')
+
+
+@register(Action)
+class ActionTO(TranslationOptions):
+    fields = ('name',)
+
+
+@register(ActionsTaken)
+class ActionsTakenTO(TranslationOptions):
+    fields = ('summary',)
+
+
+@register(Appeal)
+class AppealTO(TranslationOptions):
+    fields = ('name',)
+
+
+@register(AppealDocument)
+class AppealDocumentTO(TranslationOptions):
+    fields = ('name',)
+
+
+@register(Country)
+class CountryTO(TranslationOptions):
+    fields = ('name', 'overview',)
+
+
+@register(CountrySnippet)
+class CountrySnippetTO(TranslationOptions):
+    fields = ('snippet',)
+
+
+@register(DisasterType)
+class DisasterTypeTO(TranslationOptions):
+    fields = ('name', 'summary')
+
+
+@register(Event)
+class EventTO(TranslationOptions):
+    fields = ('name', 'slug')
+
+
+@register(GDACSEvent)
+class GDACSEventTO(TranslationOptions):
+    fields = ('title', 'description', 'country_text')
+
+
+@register(RegionSnippet)
+class RegionSnippetTO(TranslationOptions):
+    fields = ('snippet',)
+
+
+@register(SituationReport)
+class SituationReportTO(TranslationOptions):
+    fields = ('name',)
+
+
+@register(Snippet)
+class SnippetTO(TranslationOptions):
+    fields = ('snippet',)
