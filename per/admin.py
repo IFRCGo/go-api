dc005ec7e455c420284442be8577d39c696455f0
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-09-04 16:28:43 +0200

    NSPhaseAdmin queryset opt.

--- per/admin.py
+++ per/admin.py
@@ -30,6 +30,9 @@ class FormDataAdmin(CompareVersionAdmin, RegionRestrictedAdmin):
 class NSPhaseAdmin(CompareVersionAdmin, RegionRestrictedAdmin):
     search_fields = ('phase',)
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('country')
+
 
 class WorkPlanAdmin(CompareVersionAdmin, RegionRestrictedAdmin):
     search_fields = ('prioritization',)
