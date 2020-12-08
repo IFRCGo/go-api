d1e5132c0fcde3d3fef9d86321d089227e650605
Sajjad Anwar <sajjadkm@gmail.com>
2020-12-08 18:46:21 +0530

    regex validator for uppercase iso columns. data migration to make all isos uppercase

---
+++ api/migrations/0101_uppercase_iso.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+def uppercase_iso(apps, schema_editor):
+  Country = apps.get_model('api', 'Country')
+  for country in Country.objects.all():
+    if country.iso:
+      country.iso = country.iso.upper()
+
+    if country.iso2:
+      country.iso2 = country.iso2.upper()
+  
+  District = apps.get_model('api', 'District')
+  for district in District.objects.all():
+    if district.country_iso:
+      district.country_iso = district.country_iso.upper()
+
+class Migration(migrations.Migration):
+  dependencies = [
+    ('api', '0100_auto_20201130_0954'),
+  ]
+
+  operations = [
+    migrations.RunPython(uppercase_iso),
+  ]
