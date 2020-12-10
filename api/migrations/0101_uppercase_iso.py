8f570227e254bee5053ee910ef1a96131bc30db4
Sajjad Anwar <sajjadkm@gmail.com>
2020-12-10 14:40:01 +0530

    missed to save the object to db

--- api/migrations/0101_uppercase_iso.py
+++ api/migrations/0101_uppercase_iso.py
@@ -5,14 +5,17 @@ def uppercase_iso(apps, schema_editor):
   for country in Country.objects.all():
     if country.iso:
       country.iso = country.iso.upper()
+      country.save()
 
     if country.iso3:
       country.iso3 = country.iso3.upper()
+      country.save()
   
   District = apps.get_model('api', 'District')
   for district in District.objects.all():
     if district.country_iso:
       district.country_iso = district.country_iso.upper()
+      district.save()
 
 class Migration(migrations.Migration):
   dependencies = [
