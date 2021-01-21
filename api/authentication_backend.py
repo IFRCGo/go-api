5be30653e79602bd878857ccbd427844e704df96
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2021-01-21 15:05:15 +0100

    Add comments to custom Auth Backend

--- api/authentication_backend.py
+++ api/authentication_backend.py
@@ -9,7 +9,13 @@ class EmailBackend(ModelBackend):
         To allow to authenticate with email too without needing
         to implement a custom User model at this point
     """
+
     def authenticate(self, request, username=None, password=None, **kwargs):
+        '''
+            Overriding: https://github.com/django/django/blob/master/django/contrib/auth/backends.py#L36
+            most of it is working the same, only added a few more checks needed for the email auth
+        '''
+
         # To make 'finally' work becase we have 'MultipleObjectsReturned' exception
         # which could still result in a valid login
         user = None
@@ -19,6 +25,8 @@ class EmailBackend(ModelBackend):
             user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
         except UserModel.DoesNotExist:
             shouldcheck = False
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
             UserModel().set_password(password)
         except MultipleObjectsReturned:
             user = User.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
