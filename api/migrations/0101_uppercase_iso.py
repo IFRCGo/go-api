d4c9f4624b76ed94c6288c94d96618492f7f8e2c
thenav56 <ayernavin@togglecorp.com>
2021-04-12 12:09:23 +0545

    Fix country uppercase migration issue

--- api/migrations/0101_uppercase_iso.py
+++ api/migrations/0101_uppercase_iso.py
@@ -1,27 +1,20 @@
+from django.db.models.functions import Upper
 from django.db import migrations, models
 
-def uppercase_iso(apps, schema_editor):
-  Country = apps.get_model('api', 'Country')
-  for country in Country.objects.all():
-    if country.iso:
-      country.iso = country.iso.upper()
-      country.save()
 
-    if country.iso3:
-      country.iso3 = country.iso3.upper()
-      country.save()
-  
-  District = apps.get_model('api', 'District')
-  for district in District.objects.all():
-    if district.country_iso:
-      district.country_iso = district.country_iso.upper()
-      district.save()
+def uppercase_iso(apps, schema_editor):
+    Country = apps.get_model('api', 'Country')
+    District = apps.get_model('api', 'District')
+
+    Country.objects.update(iso3=Upper('iso3'), iso=Upper('iso'))
+    District.objects.update(country_iso=Upper('country_iso'))
+
 
 class Migration(migrations.Migration):
-  dependencies = [
-    ('api', '0100_auto_20201130_0954'),
-  ]
+    dependencies = [
+        ('api', '0100_auto_20201130_0954'),
+    ]
 
-  operations = [
-    migrations.RunPython(uppercase_iso),
-  ]
+    operations = [
+        migrations.RunPython(uppercase_iso),
+    ]
