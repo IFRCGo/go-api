b3bc7fc0eb77c45e54f8679fb66e03b470c6a721
Szabo, Zoltan <szabozoltan69@gmail.com>
2021-05-06 15:12:12 +0200

    Adding back ERP interface

--- api/receivers.py
+++ api/receivers.py
@@ -175,7 +175,6 @@ def handle_fr_for_erp(sender, instance, using, **kwargs):
     update ERP with the data, calling a middleware microservice
     '''
 
-    return # Temporary disabling on prod
     if isinstance(instance, FieldReport):
         # TODO: maybe add a check, and only send request if anything has changed
         if instance.ns_request_assistance:
