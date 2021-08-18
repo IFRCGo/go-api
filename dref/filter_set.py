9d5fc1c2c6470847759f1babb4cc074b2958b82d
k9845 <khatririshi2430@gmail.com>
2021-08-18 14:41:37 +0545

    Add filter for dref-overview page

--- dref/filter_set.py
+++ dref/filter_set.py
@@ -1,12 +1,16 @@
 import django_filters as filters
 
 from dref.models import Dref
+from api.models import Country
 
 
 class DrefFilter(filters.FilterSet):
     status = filters.MultipleChoiceFilter(
         choices=Dref.Status.choices(),
         widget=filters.widgets.CSVWidget,
+    )
+    national_society = filters.ModelMultipleChoiceFilter(
+        queryset=Country.objects.all()
     )
 
     class Meta:
