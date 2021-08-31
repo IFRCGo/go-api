e2935c0a829027c945706241630ff39a78a85875
k9845 <khatririshi2430@gmail.com>
2021-08-31 10:05:04 +0545

    Add summary filter in field_report

--- api/drf_views.py
+++ api/drf_views.py
@@ -591,6 +591,7 @@ class FieldReportFilter(filters.FilterSet):
     regions__in = ListFilter(field_name='regions__id')
     id = filters.NumberFilter(field_name='id', lookup_expr='exact')
     is_covid_report = filters.BooleanFilter(field_name='is_covid_report')
+    summary = filters.CharFilter(field_name='summary', lookup_expr='icontains')
 
     class Meta:
         model = FieldReport
