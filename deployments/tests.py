40652941ca4e88d24d2eb731dd37d86003b8e942
Szabo, Zoltan <szabozoltan69@gmail.com>
2021-09-27 20:29:37 +0200

    Also anon user can use personnel/format=csv, but without names

--- deployments/tests.py
+++ deployments/tests.py
@@ -145,8 +145,9 @@ class TestProjectAPI(SnapshotTestCase):
         [PersonnelFactory() for i in range(10)]
 
         url = '/api/v2/personnel/?format=csv'
-        resp = self.client.get(url)
-        self.assert_401(resp)
+        # Also unaunthenticated user can use this, but without names:
+        # resp = self.client.get(url)
+        # self.assert_401(resp)
 
         self.authenticate()
         resp = self.client.get(url)
