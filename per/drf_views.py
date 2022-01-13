2a8f861e62a8502610d0174c52c0ed2523811bc4
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-01-13 11:58:08 +0100

    Meta.fields must not contain non-model field name: area_id / 2

--- per/drf_views.py
+++ per/drf_views.py
@@ -514,7 +514,7 @@ class FormQuestionFilter(filters.FilterSet):
     class Meta:
         model = FormQuestion
         fields = {
-            'area_id': ('exact',)
+            'area': ('exact',)
         }
 
 
