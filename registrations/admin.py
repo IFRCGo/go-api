07dccda1debdbda3f2bdae57609d5eb8be48502b
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-01-27 17:09:25 +0100

    notif fix

--- registrations/admin.py
+++ registrations/admin.py
@@ -6,15 +6,17 @@ from reversion_compare.admin import CompareVersionAdmin
 from notifications.notification import send_notification
 from django.template.loader import render_to_string
 from main.frontend import frontend_url
-
+from django.http import HttpResponse, HttpResponseRedirect
 
 class PendingAdmin(CompareVersionAdmin):
-    readonly_fields = ('created_at','user','justification')
+    readonly_fields = ('get_username_and_mail','get_region','get_country','get_org','get_city','get_department','get_position','get_phone','justification','created_at','user_is_active')
     search_fields = ('user__username', 'user__email', 'admin_contact_1', 'admin_contact_2')
-    list_display = ('get_username_and_mail', 'created_at', 'email_verified', 'user_is_active')
+    list_display = ('get_username_and_mail', 'get_region','get_country','created_at', 'email_verified', 'user_is_active')
     actions = ('activate_users',)
     list_filter = ['email_verified']
     
+    change_form_template = "admin/pending_changeform.html"
+
     # Get the 'user' objects with a JOIN query
     def get_queryset(self, request):
         if request.user.is_superuser:
@@ -30,6 +32,54 @@ class PendingAdmin(CompareVersionAdmin):
          
     def get_username_and_mail(self, obj):
         return obj.user.username + ' - ' + obj.user.email
+    
+    get_username_and_mail.short_description = 'Username - Email'
+    get_username_and_mail.admin_order_field = 'user__username'
+    
+    def get_region(self, obj):
+        if obj.user.profile.country:
+            return obj.user.profile.country.region
+        else:  
+            return obj.user.profile.country
+    
+    get_region.short_description = 'Region'
+    get_region.admin_order_field = 'user__profile__country__region'
+
+    def get_country(self, obj):
+        return obj.user.profile.country
+    
+    get_country.short_description = 'Country'
+    get_country.admin_order_field = 'user__profile__country'
+
+    def get_org(self, obj):
+        return obj.user.profile.organization
+    
+    get_org.short_description = 'Organization'
+    get_org.admin_order_field = 'user__profile__organization'
+
+    def get_city(self, obj):
+        return obj.user.profile.city
+    
+    get_city.short_description = 'City'
+    get_city.admin_order_field = 'user__profile__city'
+
+    def get_department(self, obj):
+        return obj.user.profile.department
+    
+    get_department.short_description = 'Department'
+    get_department.admin_order_field = 'user__profile__department'
+
+    def get_position(self, obj):
+        return obj.user.profile.position
+    
+    get_position.short_description = 'Position'
+    get_position.admin_order_field = 'user__profile__position'
+
+    def get_phone(self, obj):
+        return obj.user.profile.phone
+    
+    get_phone.short_description = 'Phone'
+    get_phone.admin_order_field = 'user__profile__phone'
 
     def user_is_active(self, obj):
         return 'Yes' if obj.user.is_active else ''
@@ -55,6 +105,33 @@ class PendingAdmin(CompareVersionAdmin):
                     logger.info(f'User {usr.username} was already active')
             else:
                 logger.info(f'There is no User record with the ID: {pu.user_id}')
+
+
+    def response_change(self, request, obj):
+        if "_activate-user" in request.POST:
+            usr = User.objects.get(id=obj.user.id)
+
+            if usr:
+                if usr.is_active is False:
+                    
+                    email_context = {
+                        'frontend_url': frontend_url
+                    }
+
+                    send_notification('Your account has been approved',
+                              [usr.email],
+                              render_to_string('email/registration/outside-email-success.html', email_context),
+                              'Approved account successfully - ' + usr.username)
+
+                    usr.is_active = True
+                    usr.save()
+                else:
+                    logger.info(f'User {usr.username} was already active')
+            else:
+                logger.info(f'There is no User record with the ID: {obj.user.id}')
+
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
 
     def get_actions(self, request):
         actions = super(PendingAdmin, self).get_actions(request)
