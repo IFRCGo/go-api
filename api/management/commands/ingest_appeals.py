c1628af8a74bfb012139d13847bf48a014b5dfbb
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-02-02 14:58:35 +0100

    Use the latest date-interval at appeal ingest

--- api/management/commands/ingest_appeals.py
+++ api/management/commands/ingest_appeals.py
@@ -219,23 +219,20 @@ class Command(BaseCommand):
             else:
                 region = Region.objects.get(name=regions[region_name])
 
-        # get the most recent appeal detail, using the appeal start date
+        # ordering appeals by start date
         # if there is more than one detail, the start date should be the *earliest
-        if len(r['Details']) == 1:
-            detail = r['Details'][0]
-            start_date = self.parse_date(detail['APD_startDate'])
-        else:
-            details = sorted(r['Details'], reverse=True, key=lambda x: self.parse_date(x['APD_startDate']))
-            detail = details[0]
-            start_date = self.parse_date(details[-1]['APD_startDate'])
-
+        # end date should be the *latest
+        details = sorted(r['Details'], key=lambda x: self.parse_date(x['APD_startDate']))
+        detail0 = details[0]  # first
+        detail1 = details[-1]  # last
+        start_date = self.parse_date(detail0['APD_startDate'])
+        end_date = self.parse_date(detail1['APD_endDate'])
         atypes = {66: AppealType.DREF, 64: AppealType.APPEAL, 1537: AppealType.INTL}
-        atype = atypes[detail['APD_TYP_Id']]
+        atype = atypes[detail0['APD_TYP_Id']]
 
         amount_funded = triggering_amount = 0
-        # detail variable is used for other purpose
-        for detl in r['Details']:
-            if self.parse_date(detl['APD_endDate']) < tz.now():
+        for detl in details:
+            if self.parse_date(detl['APD_endDate']) < end_date:
                 continue
             triggering_amount += detl['TriggeringAmount'] if detl['TriggeringAmount'] else 0
             if atype == AppealType.DREF:
@@ -244,7 +241,6 @@ class Command(BaseCommand):
             else:
                 amount_funded += detl['ContributionAmount'] if detl['ContributionAmount'] else 0
 
-        end_date = self.parse_date(detail['APD_endDate'])
         # for new, open appeals, if we have a country, try to guess what emergency it belongs to.
         # only consider emergencies within the past 90 days
         event = None
@@ -258,8 +254,8 @@ class Command(BaseCommand):
                              .first()
             )
 
-        if detail['APD_modifyTime'] > r['APP_modifyTime']:
-            modify_time = self.parse_date(detail['APD_modifyTime'])
+        if detail1['APD_modifyTime'] > r['APP_modifyTime']:
+            modify_time = self.parse_date(detail1['APD_modifyTime'])
         else:
             modify_time = self.parse_date(r['APP_modifyTime'])
 
@@ -275,8 +271,8 @@ class Command(BaseCommand):
             'status': {'Active': 0, 'Closed': 1, 'Frozen': 2, 'Archived': 3}[r['APP_status']],
             'start_date': start_date,
             'end_date': end_date,
-            'num_beneficiaries': detail['APD_noBeneficiaries'],
-            'amount_requested': detail['APD_amountCHF'],
+            'num_beneficiaries': detail1['APD_noBeneficiaries'],
+            'amount_requested': detail1['APD_amountCHF'],
             'amount_funded': amount_funded,
             'real_data_update': modify_time,
             'triggering_amount': triggering_amount,
