f84ef02e227012b1e051a46c5a4dfb2ae3d9e270
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-02-21 15:09:42 +0100

    A small typo fix

--- main/entrypoint.sh
+++ main/entrypoint.sh
@@ -3,6 +3,6 @@ cmd="$@"
 
 export DOCKER_HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
 
-wait-for-it ${DJANGO_DB_HOST:-db}:${DJANGO_DB_PORT-5432}
+wait-for-it ${DJANGO_DB_HOST:-db}:${DJANGO_DB_PORT:-5432}
 >&2 echo "Postgres is up - continuing..."
 exec $cmd
