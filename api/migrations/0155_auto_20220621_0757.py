9aff3a8a0c6058895a69de52842615873e598288
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-06-21 10:31:11 +0200

    Migration order resetting

--- api/migrations/0155_auto_20220621_0757.py
+++ api/migrations/0155_auto_20220621_0757.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0154_merge_20220513_0934'),
+        ('api', '0155_auto_20220523_0824'),
     ]
 
     operations = [
