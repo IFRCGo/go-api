9eeb9f3fea68e208c4d696ba6fdc9d19458bb4fe
Sajjad Anwar <sajjadkm@gmail.com>
2022-11-22 00:31:09 +0530

    [deploy] provide all vars

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "environment" {
   type = string
 }
-variable "region" {
+
+variable "subscriptionId" {
   type = string
-default = "West Europe"
 }
 
 variable "domain" {
-  type = string
+    type = string
 }
 
 variable "DJANGO_SECRET_KEY" {
@@ -171,41 +171,4 @@ variable "FRONTEND_URL" {
 variable "DEBUG_EMAIL" {
   type = string
   default = "sanjay@developmentseed.org"
-}
-
-
-# -----------------
-# Attach ACR
-# Defaults to common resources
-
-variable "ifrcgo_test_resources_acr" {
-  type    = string
-  default = "ifrcgotest"
-}
-
-variable "ifrcgo_test_resources_rg" {
-  type = string
-  default = "ifrcgo_test"
-}
-
-variable "ifrcgo_test_resources_db_server" {
-  type = string
-  default = "ifrcgotest"
-}
-
-variable "ifrcgo_test_resources_db" {
-  type = string
-  default = "postgres"
-}
-
-# -----------------
-# Local variables
-
-locals {
-  stack_id              = "ifrcgo"
-  location              = lower(replace(var.region, " ", ""))
-  prefix                = "${local.stack_id}-${var.environment}"
-  prefixnodashes        = "${local.stack_id}${var.environment}"
-  storage               = (var.environment == "production" ? "${local.stack_id}tf${var.environment}" : "${local.stack_id}${var.environment}")
-  deploy_secrets_prefix = "${local.stack_id}-${var.environment}"
 }
