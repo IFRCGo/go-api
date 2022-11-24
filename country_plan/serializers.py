066c434fb6f2cdafe73061f549f5c06525c76502
thenav56 <ayernavin@gmail.com>
2022-11-24 11:11:48 +0545

    Refactor/Clean-up

--- country_plan/serializers.py
+++ country_plan/serializers.py
@@ -8,25 +8,23 @@ from .models import (
 
 
 class StrategicPrioritySerializer(serializers.ModelSerializer):
-    sp_name_display = serializers.CharField(source='get_sp_name_display', read_only=True)
+    type_display = serializers.CharField(source='get_type_display', read_only=True)
 
     class Meta:
         model = StrategicPriority
         fields = (
             'id',
             'country_plan',
-            'sp_name',
+            'type',
+            'type_display',
             'funding_requirement',
             'people_targeted',
-            # Manual Defined
-            'sp_name_display',
         )
 
 
 # NOTE: Defined to be used with CountryPlanViewset
 class MembershipCoordinationSerializer(serializers.ModelSerializer):
-    # TODO: national_society_display
-    strategic_priority_display = serializers.CharField(source='get_strategic_priority_display', read_only=True)
+    sector_display = serializers.CharField(source='get_sector_display', read_only=True)
     national_society_name = serializers.CharField(read_only=True)  # Generated in CountryPlanViewset.queryset Prefetch
 
     class Meta:
@@ -36,29 +34,27 @@ class MembershipCoordinationSerializer(serializers.ModelSerializer):
             'country_plan',
             'national_society',
             'national_society_name',
-            'strategic_priority',
+            'sector',
+            'sector_display',
             'has_coordination',
-            # Manual Defined
-            'strategic_priority_display',
         )
 
 
 # NOTE: Defined to be used with CountryPlanViewset
 class CountryPlanSerializer(serializers.ModelSerializer):
     strategic_priorities = StrategicPrioritySerializer(source='country_plan_sp', many=True, read_only=True)
-    membership_coordinationes = MembershipCoordinationSerializer(source='country_plan_mc', many=True, read_only=True)
+    membership_coordinations = MembershipCoordinationSerializer(source='full_country_plan_mc', many=True, read_only=True)
 
     class Meta:
         model = CountryPlan
         fields = (
-            'id',
+            'country',
             'internal_plan_file',  # TODO: This is internal. When to show?
             'public_plan_file',
-            'country',
             'requested_amount',
             'people_targeted',
             'is_publish',
             # Manual Defined
             'strategic_priorities',
-            'membership_coordinationes',
+            'membership_coordinations',
         )
--- country_plan/tasks.py
+++ country_plan/tasks.py
@@ -1,11 +1,13 @@
+import re
 import logging
 import pandas as pd
-from celery import shared_task
-import re
-from django.db.models import Q
 import operator
 from functools import reduce
 
+from celery import shared_task
+from django.db import transaction, models
+
+from api.models import CountryType
 from .models import (
     DataImport,
     CountryPlan,
@@ -17,9 +19,8 @@ from api.models import Country
 logger = logging.getLogger(__name__)
 
 
-@shared_task
-def process_data_import(pk):
-    csv_column = [
+class CountryPlanImporter():
+    CSV_COLUMN = [
         'ISO3',
         'Country',
         'SP1 - Climate and environmental crisis',
@@ -39,49 +40,124 @@ def process_data_import(pk):
         'TOTAL\nFUNDING REQUIREMENTS',
     ]
 
-    data_import = DataImport.objects.get(pk=pk)
-    data = pd.read_excel(data_import.file, skiprows=1, na_filter=False, usecols=csv_column)
-    data = data[:-1]
+    SP_COLUMN = [
+        'SP1',
+        'SP2',
+        'SP3',
+        'SP4',
+        'SP5',
+        'EA1',
+        'EA2',
+        'EA3',
+    ]
 
-    for dl in data.itertuples():
-        country = Country.objects.get(iso3=dl.ISO3)
-        country_plan, created = CountryPlan.objects.get_or_create(
-            country=country
-        )
-        requested_amount = dl._17
-        if isinstance(requested_amount, str):
-            requested_amount = re.findall(r'\d+(?:\.\d+)?', requested_amount)
-            country_plan.requested_amount = float(requested_amount[0]) * 1000000 if not requested_amount == [] else 0
-        else:
-            requested_amount = requested_amount
-            country_plan.requested_amount = requested_amount
+    @staticmethod
+    def _process_number(number):
+        if number in ('', None):
+            return None
+        if type(number) in [float, int]:
+            return number
+        # Some sepecific case using regex
+        value_search = re.search(r'(?P<value>(\d+(?:\.\d+)?))?\s?(?P<expression>(M|K|B){1})', number)
+        if value_search is None or (
+            value_search.group('value') is None or
+            value_search.group('expression') is None
+        ):
+            return pd.to_numeric(number)
+        value = pd.to_numeric(value_search.group('value'))
+        expression = value_search.group('expression')
+        EXPRESSION_MULTIPLER = {
+            'K': 1000,
+            'M': 1000000,
+            'B': 1000000000,
+        }
+        return value * EXPRESSION_MULTIPLER[expression]
 
-        country_plan.people_targeted = dl._8 if not dl._8 == '' else None
+    @classmethod
+    def _save_country_plan(cls, row):
+        country = Country.objects.filter(record_type=CountryType.COUNTRY).get(iso3=row.ISO3)
+        # -- Country Plan
+        country_plan, _ = CountryPlan.objects.get_or_create(country=country)
+        country_plan.requested_amount = cls._process_number(row._17)
+        country_plan.people_targeted = cls._process_number(row._8)
         country_plan.save()
 
-        target_values_list = [dl._3, dl._4, dl._5, dl._6, dl._7]
+        # -- StrategicPriority
+        target_values_list = [row._3, row._4, row._5, row._6, row._7]
         sp_target_value_map = dict(zip(StrategicPriority.Type, target_values_list))
         for sp, t_people in sp_target_value_map.items():
-            strategic_priority, created = StrategicPriority.objects.get_or_create(
+            strategic_priority, _ = StrategicPriority.objects.get_or_create(
                 country_plan=country_plan,
-                sp_name=sp
+                type=sp,
             )
-            strategic_priority.people_targeted = t_people if not t_people == '' else None
+            strategic_priority.people_targeted = cls._process_number(t_people)
+            # strategic_priority.funding_requirement?
             strategic_priority.save()
-        n_society_list = [dl.SP1, dl.SP2, dl.SP3, dl.SP4, dl.SP5, dl.EA1, dl.EA2, dl.EA3]
-        sp_column = ['SP1', 'SP2', 'SP3', 'SP4', 'SP5', 'EA1', 'EA2', 'EA3']
-        sp_column_n_society_map = dict(zip(sp_column, n_society_list))
-        sp_column_ns_type_map = dict(zip(sp_column, MembershipCoordination.Type))
-        for sp in sp_column:
-            ns = sp_column_n_society_map[sp] if not sp_column_n_society_map[sp] == '' else None
-            if ns:
-                ns_list = str(ns).split(',')
-                filters = reduce(operator.or_, (Q(society_name__icontains=item.strip()) for item in ns_list))
-                country_list = Country.objects.filter(filters)
-                for country in country_list:
-                    membership_coordination, created = MembershipCoordination.objects.get_or_create(
-                        country_plan=country_plan,
-                        national_society=country,
-                        strategic_priority=sp_column_ns_type_map[sp]
-                    )
 
+        # -- MembershipCoordination
+        n_society_list = [row.SP1, row.SP2, row.SP3, row.SP4, row.SP5, row.EA1, row.EA2, row.EA3]
+        sp_column_n_society_map = dict(zip(cls.SP_COLUMN, n_society_list))
+        sp_column_ns_type_map = dict(zip(cls.SP_COLUMN, MembershipCoordination.Sector))
+        membership_coordination_ids = []
+        for sp in cls.SP_COLUMN:
+            national_societies_str = set([
+                name.strip()
+                for name in str(sp_column_n_society_map[sp] or '').split(',')
+                if name.strip()
+            ])
+            if not national_societies_str:
+                continue
+            countries_qs = Country.objects.filter(
+                reduce(
+                    operator.or_, (
+                        models.Q(society_name__icontains=name)
+                        for name in national_societies_str
+                    )
+                ),
+                record_type=CountryType.COUNTRY,
+                in_search=True,
+            )
+            # TODO: Better error handling
+            missing_ns_in_db = set(national_societies_str) - set(countries_qs.values_list('society_name', flat=True))
+            if missing_ns_in_db:
+                raise Exception(f"Missing NS in the database: {missing_ns_in_db}")
+            for country in countries_qs.all():
+                membership_coordination, _ = MembershipCoordination.objects.get_or_create(
+                    country_plan=country_plan,
+                    national_society=country,
+                    sector=sp_column_ns_type_map[sp],
+                )
+                membership_coordination.has_coordination = True
+                membership_coordination.save()
+                membership_coordination_ids.append(membership_coordination.id)
+        # Update dangling membership coordination
+        MembershipCoordination.objects\
+            .filter(country_plan=country_plan)\
+            .exclude(id__in=membership_coordination_ids)\
+            .update(has_coordination=False)
+
+    @classmethod
+    def process(cls, file):
+        data = pd.read_excel(file, skiprows=1, na_filter=False, usecols=cls.CSV_COLUMN)
+        data = data[:-1]
+
+        errors = []
+        for row in data.itertuples():
+            try:
+                with transaction.atomic():
+                    cls._save_country_plan(row)
+            except Exception as e:
+                logger.error(f'Failed to import Country-Plan: iso3:{row.ISO3}', exc_info=True)
+                errors.append(dict(
+                    iso3=row.ISO3,
+                    error=str(e),
+                ))
+        return errors
+
+
+@shared_task
+def process_data_import(pk):
+    data_import = DataImport.objects.get(pk=pk)
+    errors = CountryPlanImporter.process(data_import.file)
+    data_import.errors = errors
+    data_import.save()
