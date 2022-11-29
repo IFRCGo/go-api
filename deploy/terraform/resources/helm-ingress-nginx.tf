cbb8e56316161a246eb91f2a34bea7a152a06fe4
Sanjay Bhangar <sanjay@developmentseed.org>
2022-11-29 14:32:14 +0530

    [deploy] more config options on ingress controller

--- deploy/terraform/resources/helm-ingress-nginx.tf
+++ deploy/terraform/resources/helm-ingress-nginx.tf
@@ -19,10 +19,20 @@ resource "helm_release" "ifrcgo-ingress-nginx" {
   }
 
   set {
+    name = "controller.config.use-proxy-protocol"
+    value = "true"
+  }
+
+  set {
+    name = "controller.config.use-forwarded-headers"
+    value = "true"
+  }
+  
+  set {
     name = "controller.replicaCount"
     value = 1
   }
-  
+
   set {
     name = "controller.service.loadBalancerIP"
     value = azurerm_public_ip.ifrcgo.ip_address
