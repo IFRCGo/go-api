5ed86141e8f857e7b3f4ec3b88d63b0ef280152a
Szabo, Zoltan <szabozoltan69@gmail.com>
2023-03-01 11:52:04 +0100

    Avoid double https prefix in project update notifications

--- main/frontend.py
+++ main/frontend.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 
 def get_project_url(id):
-    return f'https://{settings.FRONTEND_URL}/three-w/{id}/'
+    return f'{settings.FRONTEND_URL}/three-w/{id}/'
 
 
 def get_flash_update_url(id):
