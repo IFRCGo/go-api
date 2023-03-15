e413dd9b2a556f3c2b15476c136d8af8bad49525
k9845 <khatririshi2430@gmail.com>
2023-03-15 14:14:09 +0545

    Run black in lang model

--- lang/models.py
+++ lang/models.py
@@ -4,25 +4,20 @@ from django.db import models
 
 
 class String(models.Model):
-    language = models.CharField(max_length=8, verbose_name=_('language'), choices=settings.LANGUAGES)
-    key = models.CharField(max_length=255, verbose_name=_('key'))
+    language = models.CharField(max_length=8, verbose_name=_("language"), choices=settings.LANGUAGES)
+    key = models.CharField(max_length=255, verbose_name=_("key"))
     # Used by go-frontend translation management dashboard to detect changes.
-    hash = models.CharField(max_length=32, null=True, blank=True, verbose_name=_('hash'))
-    value = models.TextField(verbose_name=_('value'))
-    page_name = models.CharField(
-        max_length=255,
-        verbose_name=_('Page name'),
-        null=True,
-        blank=True
-    )
+    hash = models.CharField(max_length=32, null=True, blank=True, verbose_name=_("hash"))
+    value = models.TextField(verbose_name=_("value"))
+    page_name = models.CharField(max_length=255, verbose_name=_("Page name"), null=True, blank=True)
 
     class Meta:
-        unique_together = ('language', 'key')
-        verbose_name = _('String')
-        verbose_name_plural = _('Strings')
+        unique_together = ("language", "key")
+        verbose_name = _("String")
+        verbose_name_plural = _("Strings")
 
     def __str__(self):
-        return '{} ({})'.format(self.value, self.language)
+        return "{} ({})".format(self.value, self.language)
 
     @classmethod
     def _get_permission_per_language_codename(cls, lang_code):
--- lang/views.py
+++ lang/views.py
@@ -24,25 +24,24 @@ class LanguageViewSet(viewsets.ViewSet):
     # TODO: Cache retrive response to file
     authentication_classes = (TokenAuthentication,)
     permission_classes = (LangStringPermission,)
-    lookup_url_kwarg = 'pk'
+    lookup_url_kwarg = "pk"
 
     def list(self, request, version=None):
-        languages = [
-            {'code': code, 'title': title}
-            for code, title in settings.LANGUAGES
-        ]
-        return response.Response({
-            'count': len(languages),
-            'results': languages,
-        })
+        languages = [{"code": code, "title": title} for code, title in settings.LANGUAGES]
+        return response.Response(
+            {
+                "count": len(languages),
+                "results": languages,
+            }
+        )
 
     def retrieve(self, request, pk=None, version=None):
-        page_name = self.request.query_params.getlist('page_name')
+        page_name = self.request.query_params.getlist("page_name")
         page_name_list = []
         for xs in page_name:
-            for x in xs.split(','):
+            for x in xs.split(","):
                 page_name_list.append(x)
-        if pk == 'all':
+        if pk == "all":
             obj = StringSerializer(String.objects.all(), many=True).data
             if page_name:
                 for page in page_name_list:
@@ -55,54 +54,52 @@ class LanguageViewSet(viewsets.ViewSet):
                 query = functools.reduce(operator.or_, pages)
                 queryset = String.objects.filter(query)
                 obj = {
-                    'code': code,
-                    'title': title,
-                    'strings': StringSerializer(queryset, many=True).data,
+                    "code": code,
+                    "title": title,
+                    "strings": StringSerializer(queryset, many=True).data,
                 }
             else:
                 obj = {
-                    'code': code,
-                    'title': title,
-                    'strings': StringSerializer(String.objects.filter(language=code), many=True).data,
+                    "code": code,
+                    "title": title,
+                    "strings": StringSerializer(String.objects.filter(language=code), many=True).data,
                 }
 
         return response.Response(obj)
 
     @transaction.atomic
     @djaction(
-        detail=False,
-        url_path='bulk-action',
-        methods=('post',),
+        detail=True,
+        url_path="bulk-action",
+        methods=("post",),
     )
     def bulk_action(self, request, pk=None, *args, **kwargs):
         actions = LanguageBulkActionsSerializer(data=request.data)
         actions.is_valid(raise_exception=True)
 
+        lang = pk
+        lang_strings_qs = String.objects.filter(language=lang)
+        existing_string_keys = set(lang_strings_qs.values_list("key", flat=True))
         new_strings = []
         changed_strings = {}
         deleted_string_keys = []
         # Extract Actions
-        for meta in actions.validated_data['actions']:
-            action = meta['action']
-            key = meta['key']
-            value = meta.get('value')
-            value_hash = meta.get('hash')
-            page_name = meta.get('page_name')
-            lang = meta.get('language')
-            lang_strings_qs = String.objects.filter(language=lang)
-            existing_string_keys = set(lang_strings_qs.values_list('key', flat=True))
+        for meta in actions.validated_data["actions"]:
+            action = meta["action"]
+            key = meta["key"]
+            value = meta.get("value")
+            value_hash = meta.get("hash")
+            page_name = meta.get("page_name")
             value_meta = {
-                'value': value,
-                'hash': value_hash,
-                'page_name': page_name,
+                "value": value,
+                "hash": value_hash,
+                "page_name": page_name,
             }
             if action == LanguageBulkActionSerializer.SET:
                 if key in existing_string_keys:
                     changed_strings[key] = value_meta
                 else:
-                    new_strings.append(
-                        String(language=lang, key=key, **value_meta)
-                    )
+                    new_strings.append(String(language=lang, key=key, **value_meta))
             elif action == LanguageBulkActionSerializer.DELETE and key in existing_string_keys:
                 deleted_string_keys.append(key)
 
@@ -112,16 +109,18 @@ class LanguageViewSet(viewsets.ViewSet):
         if len(changed_strings):
             to_update_strings = list(lang_strings_qs.filter(key__in=changed_strings.keys()).all())
             for string in to_update_strings:
-                string.value = changed_strings[string.key]['value']
-                string.hash = changed_strings[string.key]['hash']
-                string.page_name = changed_strings[string.key]['page_name']
-            String.objects.bulk_update(to_update_strings, ['value', 'hash', 'page_name'])
+                string.value = changed_strings[string.key]["value"]
+                string.hash = changed_strings[string.key]["hash"]
+                string.page_name = changed_strings[string.key]["page_name"]
+            String.objects.bulk_update(to_update_strings, ["value", "hash", "page_name"])
             changed_strings = to_update_strings
         if len(deleted_string_keys):
             String.objects.filter(key__in=deleted_string_keys).delete()
 
-        return response.Response({
-            'new_strings': StringSerializer(new_strings, many=True).data,
-            'updated_strings': StringSerializer(changed_strings, many=True).data,
-            'deleted_strings_keys': deleted_string_keys,
-        })
+        return response.Response(
+            {
+                "new_strings": StringSerializer(new_strings, many=True).data,
+                "updated_strings": StringSerializer(changed_strings, many=True).data,
+                "deleted_strings_keys": deleted_string_keys,
+            }
+        )
