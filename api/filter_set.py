5376d0157b4131a36bb4a7d1a4f90aee1e120619
k9845 <khatririshi2430@gmail.com>
2023-03-30 15:46:00 +0545

    Add separate endpoint for users list

---
+++ api/filter_set.py
@@ -0,0 +1,11 @@
+import django_filters as filters
+
+from django.contrib.auth.models import User
+
+
+class UserFilterSet(filters.FilterSet):
+    name = filters.CharFilter(field_name='username', lookup_expr='icontains')
+
+    class Meta:
+        model = User
+        fields = ()
