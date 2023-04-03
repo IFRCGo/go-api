a0b5f838123aed446cfc86f4bd287b91d57a4bab
Sajjad Anwar <sajjadkm@gmail.com>
2023-04-03 16:48:40 +0530

    create a new disk

--- deploy/terraform/resources/disk.tf
+++ deploy/terraform/resources/disk.tf
@@ -1,5 +1,5 @@
 resource "azurerm_managed_disk" "ifrcgo" {
-  name                 = "${local.prefix}-disk"
+  name                 = "${local.prefix}-disk001"
   resource_group_name      = azurerm_kubernetes_cluster.ifrcgo.node_resource_group
   location                 = data.azurerm_resource_group.ifrcgo.location
   storage_account_type = "StandardSSD_LRS"
