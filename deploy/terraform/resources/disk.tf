d69aeb131ae46d67699623f4f5a4188d69216f72
Sajjad Anwar <sajjadkm@gmail.com>
2023-04-03 16:32:06 +0530

    try to put the disk in the aks node resource group

--- deploy/terraform/resources/disk.tf
+++ deploy/terraform/resources/disk.tf
@@ -1,6 +1,6 @@
 resource "azurerm_managed_disk" "ifrcgo" {
   name                 = "${local.prefix}-disk"
-  resource_group_name      = data.azurerm_resource_group.ifrcgo.name
+  resource_group_name      = azurerm_kubernetes_cluster.ifrcgo.node_resource_group
   location                 = data.azurerm_resource_group.ifrcgo.location
   storage_account_type = "StandardSSD_LRS"
   create_option        = "Empty"
