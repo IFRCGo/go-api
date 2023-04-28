3aef2aeadee3a415b828f17c9d11b311b72b3373
Sajjad Anwar <sajjadkm@gmail.com>
2023-04-28 18:39:42 +0530

    remove unnecessary comment

--- deploy/terraform/resources/disk.tf
+++ deploy/terraform/resources/disk.tf
@@ -9,7 +9,6 @@ resource "azurerm_managed_disk" "ifrcgo" {
 
 resource "azurerm_role_assignment" "disk" {
   scope                = azurerm_managed_disk.ifrcgo.id
-  # scope = "/subscriptions/39308fb0-9929-4b29-aafa-b3c78a8b0658/resourceGroups/${azurerm_kubernetes_cluster.ifrcgo.node_resource_group}"
   role_definition_name = "Contributor"
   principal_id         = azurerm_kubernetes_cluster.ifrcgo.identity[0].principal_id
 }
