d3844683a58c399db7fe59936b4ad008e31fbf31
k9845 <khatririshi2430@gmail.com>
2023-05-10 11:42:41 +0545

    Add custom_component in per workplan

---
+++ per/validators.py
@@ -0,0 +1,39 @@
+from typing import Any
+from jsonschema import validate, ValidationError as JSONValidationError
+from django.core.exceptions import ValidationError
+
+from per.models import WorkPlanStatus
+
+
+custom_component_schema = {
+    "title": "Custom Component",
+    "properties": {
+        "actions": {
+            "type": "string"
+        },
+        "due_date" : {
+            "type": "string",
+            "format": "date"
+        },
+        "status": {
+            "type": "number",
+            "enum": [x.value for x in WorkPlanStatus]
+        }
+    },
+    "required": ["actions", "due_date"],
+}
+
+
+def validate_custom_component(custom_component: dict):
+    return validate_with_schema(custom_component_schema)(custom_component)
+
+
+def validate_with_schema(schema: Any):
+    def validator(data):
+        try:
+            validate(data, schema)
+        except JSONValidationError as e:
+            msg = e.message[:200] + "..." if len(e.message) > 200 else e.message
+            raise ValidationError(f"Invalid data: {msg}")
+
+    return validator
