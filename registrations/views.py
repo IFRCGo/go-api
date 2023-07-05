135b6e0e615dbde30cd97feab1691f5dada10ab4
k9845 <khatririshi2430@gmail.com>
2023-07-05 15:53:33 +0545

    Add serializer for registration view

--- registrations/views.py
+++ registrations/views.py
@@ -1,170 +1,22 @@
 from datetime import timedelta
 
 from django.db.models import Q
-from django.db.models.functions import Lower
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
-from django.utils.crypto import get_random_string
-from django.contrib.auth.models import User
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse
 from django.template.loader import render_to_string
 from rest_framework.views import APIView
 
 from api.views import (
-    bad_request,
     bad_http_request,
 )
-from api.models import Country, Profile, UserRegion
 from notifications.notification import send_notification
-from .models import Pending, DomainWhitelist
-
-
-def is_valid_domain(email):
-    if email is None:
-        return False
-    domain = email.lower().split('@')[1]
-    is_allowed = (
-        DomainWhitelist.objects
-                       .filter(is_active=True)
-                       .annotate(domain_name_lower=Lower('domain_name'))
-                       .filter(domain_name_lower=domain)
-                       .exists()
-    )
-
-    if is_allowed or domain in ['ifrc.org', 'arcs.org.af', 'voroskereszt.hu']:
-        return True
-    return False
-
-
-def get_valid_admins(contacts):
-    if not type(contacts) is list:
-        return False
-    # Get the emails into an array, don't include None
-    emails = [admin.get('email', None) for admin in contacts if admin.get('email', None)]
-    # Currently we enforce 2 admins to validate
-    if len(emails) < 2:
-        return False
-    is_admin1 = User.objects.filter(email__iexact=emails[0]).exists()
-    is_admin2 = User.objects.filter(email__iexact=emails[1]).exists()
-    if is_admin1 is False or is_admin2 is False:
-        return False
-    return emails
-
-
-def create_inactive_user(username, firstname, lastname, email, password):
-    user = User.objects.create_user(username=username,
-                                    first_name=firstname,
-                                    last_name=lastname,
-                                    email=email,
-                                    password=password,
-                                    is_active=False)
-    return user
-
-
-def set_user_profile(user, country, organization_type, organization, city, department, position, phone_number):
-    user.profile.country = Country.objects.get(pk=country)
-    user.profile.org_type = organization_type
-    user.profile.org = organization
-    user.profile.city = city
-    user.profile.department = department
-    user.profile.position = position
-    user.profile.phone_number = phone_number
-    user.save()
-    return user
-
-
-def getRegionalAdmins(userId):
-    countryId = Profile.objects.get(user_id=userId).country_id
-    regionId = Country.objects.get(id=countryId).region_id
-
-    admins = UserRegion.objects.filter(region_id=regionId).values_list('user__email', flat=True)
-    return admins
-
-
-class NewRegistration(APIView):
-    permission_classes = []
-
-    def post(self, request):
-        required_fields = (
-            'email',
-            # 'username',
-            'password',
-            'country',
-            'organizationType',
-            'organization',
-            'firstname',
-            'lastname',
-        )
-
-        missing_fields = [field for field in required_fields if field not in request.data]
-        if missing_fields:
-            return bad_request('Could not complete request. Please submit %s' % ', '.join(missing_fields))
-
-        email = request.data.get('email', None)
-        # Since username is a required field we still need to fill it in
-        # but now only email is being used for new registrations
-        username = request.data.get('email', None)
-        password = request.data.get('password', None)
-        firstname = request.data.get('firstname', None)
-        lastname = request.data.get('lastname', None)
-        country = request.data.get('country', None)
-        city = request.data.get('city', None)
-        organization_type = request.data.get('organizationType', None)
-        organization = request.data.get('organization', None)
-        department = request.data.get('department', None)
-        position = request.data.get('position', None)
-        phone_number = request.data.get('phoneNumber', None)
-        justification = request.data.get('justification', None)
-
-        is_staff = is_valid_domain(email)
-
-        if User.objects.filter(email__iexact=email).exists():
-            return bad_request('A user with that email address already exists.')
-        if User.objects.filter(username__iexact=username).exists():
-            return bad_request('That username is taken, please choose a different one.')
-        if ' ' in username:
-            return bad_request('Username can not contain spaces, please choose a different one.')
-
-        # Create the User object
-        try:
-            user = create_inactive_user(username, firstname, lastname, email, password)
-        except Exception:
-            return bad_request('Could not create user.')
-
-        # Set the User Profile properties
-        try:
-            set_user_profile(user, country, organization_type, organization, city, department, position, phone_number)
-        except Exception:
-            User.objects.filter(username=username).delete()
-            return bad_request('Could not create user profile.')
-
-        pending = Pending.objects.create(user=user, justification=justification, token=get_random_string(length=32))
-        if not is_staff:
-            pending.admin_token_1 = get_random_string(length=32)
-
-        pending.save()
-
-        email_context = {
-            'confirmation_link': 'https://%s/verify_email/?token=%s&user=%s' % (
-                settings.BASE_URL,  # on PROD it should point to goadmin...
-                pending.token,
-                username,
-            )
-        }
-
-        # if validated email accounts get a different message
-        if is_staff:
-            template = 'email/registration/verify-staff-email.html'
-        else:
-            template = 'email/registration/verify-outside-email.html'
-
-        send_notification('Validate your account',
-                          [email],
-                          render_to_string(template, email_context),
-                          'Validate account - ' + username)
-
-        return JsonResponse({'status': 'ok'})
+from .models import Pending
+from .utils import (
+    is_valid_domain,
+    getRegionalAdmins
+)
 
 
 class VerifyEmail(APIView):
