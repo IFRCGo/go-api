f3e5ed627d8dccf95f8cb81fa8331fdab542308b
k9845 <khatririshi2430@gmail.com>
2023-07-28 11:47:56 +0545

    Add notes in formcompnent responses

--- per/models.py
+++ per/models.py
@@ -152,6 +152,10 @@ class FormComponentResponse(models.Model):
         verbose_name=_("Climate Environmental Considerations"),
         null=True, blank=True
     )
+    notes = models.TextField(
+        verbose_name=_("Notes"),
+        null=True, blank=True
+    )
 
 
 @reversion.register()
