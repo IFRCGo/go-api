172c5c48fd5c0a56ccaa05a86de44d03eb6070da
k9845 <khatririshi2430@gmail.com>
2023-08-04 11:30:45 +0545

    Refactor compoent from char to int choices

--- per/enums.py
+++ per/enums.py
@@ -3,5 +3,6 @@ from . import models
 enum_register = {
     'workplanstatus': models.PerWorkPlanStaus,
     'perphases': models.Overview.Phase,
-    'overviewassessmentmethods': models.Overview.AssessmentMethod
+    'overviewassessmentmethods': models.Overview.AssessmentMethod,
+    'component_status': models.FormComponent.FormComponentStatus
 }
