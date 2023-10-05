71bfd87812bad66ac8718566f2ab9601e19c51fa
k9845 <khatririshi2430@gmail.com>
2023-10-05 10:13:24 +0545

    Exclude component performance

--- per/migrations/0086_migrate_old_form.py
+++ per/migrations/0086_migrate_old_form.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
         FormComponentResponse = apps.get_model('per', 'FormComponentResponse')
         AreaResponse = apps.get_model('per', 'AreaResponse')
         PerAssessment = apps.get_model('per', 'PerAssessment')
+        PerComponentRating = apps.get_model('per', 'PerComponentRating')
 
         per_assessments_by_overview = {}
 
@@ -33,6 +34,8 @@ class Migration(migrations.Migration):
             area_response = AreaResponse.objects.create(area=form.area)
             per_assessment.area_responses.add(area_response)
 
+            rating_map = {3: 1, 4: 2, 5: 3, 6: 4, 7: 5, 8: 6}
+
             for component_response in area.formcomponent_set.all():
                 form_component_response = FormComponentResponse.objects.create(
                     component=component_response,
@@ -45,12 +48,27 @@ class Migration(migrations.Migration):
                 )
 
                 for form_data_entry in form_data_entries:
-                    question_and_answer = FormComponentQuestionAndAnswer.objects.create(
-                        question=form_data_entry.question,
-                        answer=form_data_entry.selected_answer,
-                        notes=form_data_entry.notes
-                    )
-                    form_component_response.question_responses.add(question_and_answer)
+                    question = form_data_entry.question.question
+                    answer = form_data_entry.selected_answer
+                    notes = form_data_entry.notes_en
+                    if answer:
+                        if question != "Component performance" and answer.id in [1, 2]:
+                            question_and_answer = FormComponentQuestionAndAnswer.objects.create(
+                                question=form_data_entry.question,
+                                answer=form_data_entry.selected_answer,
+                                notes=form_data_entry.notes_en
+                            )
+                            form_component_response.question_responses.add(question_and_answer)
+                    question = form_data_entry.question.question
+                    if question == "Component performance":
+                        answer = form_data_entry.selected_answer
+                        if answer:
+                            rating = rating_map.get(answer.id)
+                            if rating is not None:
+                                form_component_response.rating = PerComponentRating.objects.get(id=rating)
+                                form_component_response.save(update_fields=['rating'])
+                            else:
+                                    print(f"No rating found for answer {answer}")
 
     dependencies = [
         ('per', '0085_alter_overview_assessment_method'),
