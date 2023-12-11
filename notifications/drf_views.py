2eb6b6c4213d70024b5dd9cda7cb56486fb6b696
Szabo, Zoltan <szabozoltan69@gmail.com>
2023-12-11 17:11:59 +0100

    New filters on SurgeAlerts

--- notifications/drf_views.py
+++ notifications/drf_views.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta, timezone
 from django.db.models import Q
 from django_filters import rest_framework as filters
+from django_filters.widgets import CSVWidget
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import SurgeAlert, Subscription
+from deployments.models import MolnixTag
 from .serializers import (
     SurgeAlertSerializer,
 #   UnauthenticatedSurgeAlertSerializer,
@@ -12,18 +14,54 @@ from .serializers import (
 )
 
 
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
 class SurgeAlertFilter(filters.FilterSet):
     atype = filters.NumberFilter(field_name='atype', lookup_expr='exact')
     category = filters.NumberFilter(field_name='category', lookup_expr='exact')
     event = filters.NumberFilter(field_name='event', lookup_expr='exact')
+    molnix_tags = filters.ModelMultipleChoiceFilter(
+        label='tag-ids',
+        method='molnix_tags_filter',
+        help_text='Molnix_tag GO identifiers, comma separated',
+        widget=CSVWidget,
+        queryset=MolnixTag.objects.all(),
+    )
+    molnix_tag_names = CharInFilter(
+        label='tag-names',
+        method='molnix_tag_names_filter',
+        help_text='Molnix_tag names, comma separated',
+        widget=CSVWidget,
+        # How can it work without queryset??? And if that can work, why that one ^ not?
+    )
+
+    def molnix_tags_filter(self, qs, name, value):
+        if value:
+            return qs.filter(molnix_tags__in=value).order_by('id').distinct()
+        return qs
+
+    def molnix_tag_names_filter(self, qs, name, value):
+        if value:
+            return qs.filter(molnix_tags__name__in=value).order_by('id').distinct()
+        return qs
 
     class Meta:
         model = SurgeAlert
         fields = {
             'created_at': ('exact', 'gt', 'gte', 'lt', 'lte'),
+            'start': ('exact', 'gt', 'gte', 'lt', 'lte'),
             'end': ('exact', 'gt', 'gte', 'lt', 'lte'),
-            'is_stood_down': ('exact',),
-            'is_active': ('exact',)
+            'is_stood_down': ('exact', 'in'),
+            'is_active': ('exact',),
+            'molnix_id': ('exact', 'in'),
+            'molnix_status': ('exact', 'in'),
+            'message': ('exact', 'in'),
+            'country': ('exact', 'in'),
+            'country__name': ('exact', 'in'),
+            'country__iso': ('exact', 'in'),
+            'country__iso3': ('exact', 'in'),
         }
 
 
--- notifications/tests.py
+++ notifications/tests.py
@@ -7,6 +7,8 @@ from lang.serializers import TranslatedModelSerializerMixin
 from main.test_case import APITestCase
 
 from .models import SurgeAlert, SurgeAlertType
+from api.models import Region, Country, District, Admin2
+from deployments.models import MolnixTag
 
 
 class NotificationTestCase(APITestCase):
@@ -56,3 +58,75 @@ class NotificationTestCase(APITestCase):
                         getattr(surge_alert, build_localized_fieldname(field, lang)),
                         self.aws_translator._fake_translation(original_value, lang, 'en') if lang != 'en' else original_value,
                     )
+
+class SurgeAlertFilteringTest(APITestCase):
+
+    def test_filtering(self):
+        region_1 = Region.objects.create(name=1)
+        region_2 = Region.objects.create(name=2)
+        country_1 = Country.objects.create(name='Atlantis', iso3='ATL', region=region_1)
+        country_2 = Country.objects.create(name='Neptunus', iso3='NPT', region=region_2)
+        molnix_tag_1 = MolnixTag.objects.create(
+            molnix_id=3,
+            name='OP-6700',
+            description='Middle East Crisis | MENA',
+            tag_type='regular',
+            tag_category='molnix_operation')
+        molnix_tag_2 = MolnixTag.objects.create(
+            molnix_id=5,
+            name='L-FRA',
+            description='French',
+            tag_type='language',
+            tag_category='molnix_language')
+        molnix_tag_3 = MolnixTag.objects.create(
+            molnix_id=17,
+            name='AMER',
+            description='Americas Region',
+            tag_type='regular',
+            tag_category='molnix_region')
+        surge_alert_1 = SurgeAlert.objects.create(
+            message='CEA Coordinator, Floods, Atlantis', atype=6, category=2, molnix_status='active', country=country_1)
+        surge_alert_1.molnix_tags.set([molnix_tag_1, molnix_tag_2])
+        surge_alert_2 = SurgeAlert.objects.create(
+            message='WASH Coordinator, Earthquake, Neptunus', atype=5, category=1, molnix_status='active', country=country_2)
+        surge_alert_1.molnix_tags.set([molnix_tag_1, molnix_tag_2])
+        surge_alert_2.molnix_tags.set([molnix_tag_1, molnix_tag_3])
+
+        # we have 2 SurgeAlerts with 2 different Countries: A, B
+        # Filtering for X, B gives 1; filtering for A, B gives both results.
+
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=AAA,NPT').json()
+        self.assertEqual(response['count'], 1)
+        self.assertEqual(response['results'][0]['molnix_tags'][0]['name'], 'OP-6700')
+
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=ATL,NPT').json()
+        self.assertEqual(response['count'], 2)
+        self.assertEqual(
+            {response['results'][0]['molnix_tags'][0]['name'],
+             response['results'][0]['molnix_tags'][1]['name'],
+             response['results'][1]['molnix_tags'][0]['name'],
+             response['results'][1]['molnix_tags'][1]['name']},
+            {'OP-6700', 'L-FRA', 'AMER'})
+
+        # we have 2 SurgeAlerts with MolnixTags A, B and A, C.
+        # Filtering for A, B, C gives both. Filtering for only B or C gives 1.
+
+        # filtering by molnix_tags
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=ATL,NPT&molnix_tags=' + str(molnix_tag_2.id)
+            ).json()
+        self.assertEqual(response['count'], 1)
+
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=ATL,NPT&molnix_tags=' \
+            + str(molnix_tag_1.id) + ',' + str(molnix_tag_2.id) + ',' + str(molnix_tag_3.id)
+            ).json()
+        self.assertEqual(response['count'], 2)
+
+        # filtering by molnix_tag_names
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=ATL,NPT&molnix_tag_names=' + 'AMER'
+            ).json()
+        self.assertEqual(response['count'], 1)
+
+        response = self.client.get('/api/v2/surge_alert/?country__iso3__in=ATL,NPT&molnix_tag_names=' \
+            + 'OP-6700,L-FRA,AMER'
+            ).json()
+        self.assertEqual(response['count'], 2)
