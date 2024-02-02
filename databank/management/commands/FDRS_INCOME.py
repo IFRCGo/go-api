132cd86fefd87035f5ec43b19484781bcb64aad5
k9845 <khatririshi2430@gmail.com>
2024-02-02 15:06:43 +0545

    Add annual income for historical data

--- databank/management/commands/FDRS_INCOME.py
+++ databank/management/commands/FDRS_INCOME.py
@@ -16,46 +16,25 @@ class Command(BaseCommand):
 
         FDRS_INDICATORS = [e.value for e in FDRSIncome.FDRSIndicator]
 
-        FDRS_NS_API_ENDPOINT = f'https://data-api.ifrc.org/api/entities/ns?apiKey={settings.FDRS_APIKEY}'
-
-        # To fetch NS Data using NS ID
-        FDRS_DATA_API_ENDPOINT = f'https://data-api.ifrc.org/api/data?apiKey={settings.FDRS_APIKEY}&indicator=' + ','.join(FDRS_INDICATORS)
-
-
-        #@catch_error('Error occured while fetching from FDRS API.')
-        fdrs_entities = requests.get(FDRS_NS_API_ENDPOINT)
-        if fdrs_entities.status_code != 200:
-            return
-        fdrs_entities.raise_for_status()
-        fdrs_entities = fdrs_entities.json()
-
-        ns_iso_map = {
-            # ISO3 are missing for some in FDRS & IFRC-GO only have ISO2 for countries
-            ns['KPI_DON_code']: ns['iso_2']
-            for ns in fdrs_entities
-        }
-
-        data =  {
-            # KEY <ISO2>-<Indicator_ID>: {year: '', value: ''}
-            f"{ns_iso_map[ns_data['id']].upper()}-{indicator_data['id']}": (
-                ns_data['data'][-1] if (
-                    ns_data['data'] and len(ns_data['data']) > 0
-                ) else None
-            )
-            for indicator_data in requests.get(FDRS_DATA_API_ENDPOINT).json()['data']
-            for ns_data in indicator_data['data']
-        }
-        for key, value in data.items():
-            country_indicator = key.split('-')
-            country_iso = country_indicator[0]
-            indicator = country_indicator[1]
-            if value and len(value) > 0:
-                year = str(value['year']) + '-01-01'
-                value = value['value']
-                data = {
-                    'overview' : CountryOverview.objects.filter(country__iso=country_iso).first(),
-                    'date' : year,
-                    'indicator' : indicator,
-                    'value': value,
-                }
-                FDRSIncome.objects.create(**data)
+        for overview in CountryOverview.objects.all():
+            country_fdrs_code = overview.country.fdrs
+            FDRS_DATA_API_ENDPOINT = f'https://data-api.ifrc.org/api/data?apiKey={settings.FDRS_APIKEY}&KPI_Don_Code={country_fdrs_code}&indicator=' + ','.join(FDRS_INDICATORS)
+            #@catch_error('Error occured while fetching from FDRS API.')
+            fdrs_entities = requests.get(FDRS_DATA_API_ENDPOINT)
+            if fdrs_entities.status_code != 200:
+                return
+            fdrs_entities.raise_for_status()
+            fdrs_entities = fdrs_entities.json()
+            for d in fdrs_entities['data']:
+                indicator = next(iter(d.values()))
+                income_list = d['data'][0]['data']
+                if len(income_list):
+                    for income in income_list:
+                        data = {
+                            'date': str(income['year']) + '-01-01',
+                            'value': income['value'],
+                            'indicator': indicator,
+                            'overview': overview
+                        }
+                        print("*****")
+                        FDRSIncome.objects.create(**data)
