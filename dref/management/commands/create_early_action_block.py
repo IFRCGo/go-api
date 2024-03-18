59505e2b950a9637e3d5c6e446aa2d9a34e32db0
Sushil Tiwari <susiltiwari750@gmail.com>
2024-03-18 16:39:19 +0545

    Add script to map old list of activities to early action block

---
+++ dref/management/commands/create_early_action_block.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from dref.models import ActivityTimeFrame, PlannedIntervention
+
+from api.logger import logger
+
+class Command(BaseCommand):
+    help = 'Convert Old PlannedIntervention description to early action block'
+
+    def handle(self, *args, **options):
+        logger.info("Creating early action blocks from planned interventions description")
+        interventions_to_create = []
+        for pi in PlannedIntervention.objects.filter(description__isnull=False):
+            if pi.description:
+                descriptions = pi.description.split('•')
+                for desc in descriptions[1:]:
+                    activity_frame_title = desc.replace('\n', '').strip()
+                    interventions_to_create.append(ActivityTimeFrame(title=activity_frame_title))
+            if interventions_to_create:
+                ActivityTimeFrame.objects.bulk_create(interventions_to_create)
+                pi.early_action_block.add(*interventions_to_create)
+                pi.description = None
+                pi.save()
+                logger.info(f"Created {len(interventions_to_create)} early action blocks for planned intervention")
+        if not interventions_to_create:
+            logger.info(f"No early action blocks created for planned intervention")
