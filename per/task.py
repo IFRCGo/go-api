dbd38c24cd22b33d604cb40bd1e9d8247863690d
Sushil Tiwari <susiltiwari750@gmail.com>
2024-08-02 22:49:39 +0545

    Caching implementation and add changes on parsing

--- per/task.py
+++ per/task.py
@@ -1,17 +1,30 @@
 from celery import shared_task
 
+from api.logger import logger
 from per.models import OpsLearningCacheResponse
 from per.ops_learning_summary import OpsLearningSummaryTask
 
 
 @shared_task
 def generate_summary(ops_learning_summary_instance: OpsLearningCacheResponse, filter_data: dict):
-    regional_list, global_list, country_list = OpsLearningSummaryTask.generate_priotization_list()
-    prioritized_learnings = OpsLearningSummaryTask.prioritize_components(filter_data, regional_list, global_list, country_list)
-    primary_learning_df, secondary_learning_df = OpsLearningSummaryTask.prioritize_excerpts(prioritized_learnings)
-    primary_learning_prompt, secondary_learning_prompt = OpsLearningSummaryTask.format_prompt(
-        primary_learning_df, secondary_learning_df, filter_data
-    )
-    OpsLearningSummaryTask.get_or_create_summary(
-        ops_learning_summary_instance, primary_learning_prompt, secondary_learning_prompt
-    )
+    try:
+        OpsLearningSummaryTask.change_ops_learning_status(
+            instance=ops_learning_summary_instance, status=OpsLearningCacheResponse.Status.STARTED
+        )
+        regional_list, global_list, country_list = OpsLearningSummaryTask.generate_priotization_list()
+        prioritized_learnings = OpsLearningSummaryTask.prioritize_components(
+            filter_data, regional_list, global_list, country_list
+        )
+        primary_learning_df, secondary_learning_df = OpsLearningSummaryTask.prioritize_excerpts(prioritized_learnings)
+        primary_learning_prompt, secondary_learning_prompt = OpsLearningSummaryTask.format_prompt(
+            primary_learning_df, secondary_learning_df, filter_data
+        )
+        OpsLearningSummaryTask.get_or_create_summary(
+            ops_learning_summary_instance, primary_learning_prompt, secondary_learning_prompt
+        )
+    except Exception as e:
+        OpsLearningSummaryTask.change_ops_learning_status(
+            instance=ops_learning_summary_instance, status=OpsLearningCacheResponse.Status.FAILED
+        )
+        logger.error(e)
+        raise e
