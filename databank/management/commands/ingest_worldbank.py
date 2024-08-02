445aefed2c600deecd5e9fdb30234912bf7f6836
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2024-08-02 17:26:20 +0545

    Get latest data from world bank data for country overview.

--- databank/management/commands/ingest_worldbank.py
+++ databank/management/commands/ingest_worldbank.py
@@ -15,6 +15,22 @@ from .sources.utils import get_country_by_iso3
 logger = logging.getLogger(__name__)
 
 
+def get_indicator_value(indicator):
+    try:
+        result = indicator[0]
+    except (IndexError, TypeError):
+        result = None
+    return result
+
+
+def get_indicator_year(indicator):
+    try:
+        result = indicator[1]
+    except (IndexError, TypeError):
+        result = None
+    return result
+
+
 class Command(BaseCommand):
     help = "Add Acaps seasonal calendar data"
 
@@ -38,7 +54,7 @@ class Command(BaseCommand):
         country_dict = {}
 
         now = datetime.datetime.now()
-        daterange = f"{now.year - 3}:{now.year - 2}"  # Data of 2022
+        daterange = f"{now.year - 10}:{now.year}"  # Fetch data up to 10 years.
 
         for country in Country.objects.filter(
             iso3__isnull=False, record_type=CountryType.COUNTRY, region__isnull=False, independent=True
@@ -58,20 +74,25 @@ class Command(BaseCommand):
                                 "page": page,
                             },
                         )
+                        response.raise_for_status()
                     except requests.exceptions.HTTPError:
                         continue
+
                     try:
                         data_list = response.json()[1]
                         if data_list is not None and len(data_list) > 0:
                             data = data_list
                     except (IndexError, KeyError):
                         continue
+
                     if data:
-                        # Check if data_list is not None and has elements
-                        for pop_data in data:
-                            geo_code = pop_data["countryiso3code"]
-                            pop = pop_data["value"]
-                            year = pop_data["date"]
+                        country_data = None
+                        geo_id = None
+                        existing_data = None
+                        for datum in data:
+                            geo_code = datum["countryiso3code"]
+                            value = datum["value"]
+                            year = datum["date"]
 
                             if len(geo_code) == 3:
                                 pcountry = get_country_by_iso3(geo_code)
@@ -80,10 +101,14 @@ class Command(BaseCommand):
 
                                     if geo_id not in country_dict:
                                         country_dict[geo_id] = []
-                                    existing_data = next((data for data in country_dict[geo_id] if data[2] == indicator), None)
-                                    if existing_data is None or existing_data[1] < year:
-                                        country_dict[geo_id].append((pop, year, indicator))
-                                        logger.info(json.dumps(country_dict))
+
+                                    if value is not None:
+                                        if existing_data is None or year > existing_data["date"]:
+                                            existing_data = datum
+                                            country_data = (value, year, indicator)
+                        country_dict[geo_id].append(country_data)
+                        logger.info(json.dumps(country_dict))
+
                         if "pages" in response.json()[0]:
                             if page >= response.json()[0]["pages"]:
                                 break
@@ -92,29 +117,61 @@ class Command(BaseCommand):
         for country_code, indicators in country_dict.items():
             overview = CO.objects.filter(country__iso=country_code).first()
             if overview:
-                overview.world_bank_population = indicators[0][0]
-                overview.world_bank_population_above_age_65 = indicators[1][0]
-                overview.world_bank_population_age_14 = indicators[2][0]
-                overview.world_bank_urban_population_percentage = indicators[3][0]
-                overview.world_bank_gdp = indicators[4][0]
-                overview.world_bank_gni = indicators[5][0]
-                overview.world_bank_gender_inequality_index = indicators[6][0]
-                overview.world_bank_life_expectancy = indicators[7][0]
-                overview.world_bank_literacy_rate = indicators[8][0]
-                overview.world_bank_poverty_rate = indicators[9][0]
-                overview.world_bank_gni_capita = indicators[10][0]
+                overview.world_bank_population = get_indicator_value(indicators[0])
+                overview.calculated_world_bank_population_year = get_indicator_year(indicators[0])
+
+                overview.world_bank_population_above_age_65 = get_indicator_value(indicators[1])
+                overview.calculated_world_bank_population_above_age_65_year = get_indicator_year(indicators[1])
+
+                overview.world_bank_population_age_14 = get_indicator_value(indicators[2])
+                overview.calculated_world_bank_population_age_14_year = get_indicator_year(indicators[2])
+
+                overview.world_bank_urban_population_percentage = get_indicator_value(indicators[3])
+                overview.calculated_world_bank_urban_population_percentage_year = get_indicator_year(indicators[3])
+
+                overview.world_bank_gdp = get_indicator_value(indicators[4])
+                overview.calculated_world_bank_gdp_year = get_indicator_year(indicators[4])
+
+                overview.world_bank_gni = get_indicator_value(indicators[5])
+                overview.calculated_world_bank_gni_year = get_indicator_year(indicators[5])
+
+                overview.world_bank_gender_inequality_index = get_indicator_value(indicators[6])
+                overview.calculated_world_bank_gender_inequality_index_year = get_indicator_year(indicators[6])
+
+                overview.world_bank_life_expectancy = get_indicator_value(indicators[7])
+                overview.calculated_world_bank_life_expectancy_year = get_indicator_year(indicators[7])
+
+                overview.world_bank_literacy_rate = get_indicator_value(indicators[8])
+                overview.calculated_world_bank_literacy_rate_year = get_indicator_year(indicators[8])
+
+                overview.world_bank_poverty_rate = get_indicator_value(indicators[9])
+                overview.calculated_world_bank_poverty_rate_year = get_indicator_year(indicators[9])
+
+                overview.world_bank_gni_capita = get_indicator_value(indicators[10])
+                overview.calculated_world_bank_gni_capita_year = get_indicator_value(indicators[10])
                 overview.save(
                     update_fields=[
                         "world_bank_population",
+                        "calculated_world_bank_population_year",
                         "world_bank_population_above_age_65",
+                        "calculated_world_bank_population_above_age_65_year",
                         "world_bank_population_age_14",
+                        "calculated_world_bank_population_age_14_year",
                         "world_bank_urban_population_percentage",
+                        "calculated_world_bank_urban_population_percentage_year",
                         "world_bank_gdp",
+                        "calculated_world_bank_gdp_year",
                         "world_bank_gni",
+                        "calculated_world_bank_gni_year",
                         "world_bank_gender_inequality_index",
+                        "calculated_world_bank_gender_inequality_index_year",
                         "world_bank_life_expectancy",
+                        "calculated_world_bank_life_expectancy_year",
                         "world_bank_literacy_rate",
+                        "calculated_world_bank_literacy_rate_year",
                         "world_bank_poverty_rate",
+                        "calculated_world_bank_poverty_rate_year",
                         "world_bank_gni_capita",
+                        "calculated_world_bank_gni_capita_year",
                     ]
                 )
