f0a7dcab910cd10e75cd89d53b74b6ad3111afea
Sushil Tiwari <susiltiwari750@gmail.com>
2024-10-09 11:41:45 +0545

    Simplify monty token and add logger

--- api/management/commands/load_monty_users.py
+++ api/management/commands/load_monty_users.py
@@ -6,12 +6,13 @@ from django.conf import settings
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from django.utils import timezone
 
 from registrations.models import UserExternalToken
 from registrations.utils import jwt_encode_handler
 
-logging = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -19,36 +20,46 @@ class Command(BaseCommand):
     missing_args_message = "Filename is missing. Filename / path to TAB separated CSV file required."
 
     def add_arguments(self, parser):
-        parser.add_argument("filename", nargs="+", type=str)
+        parser.add_argument("filename", type=str, help="Path to the TAB separated CSV file")
 
     def handle(self, *args, **options):
-        filename = options["filename"][0]
-        with open(filename, newline="", encoding="utf-8") as csvfile:
-            user_list = list(csv.DictReader(csvfile))
+        filename = options["filename"]
+        try:
+            with open(filename, newline="", encoding="utf-8") as csvfile:
+                user_list = list(csv.DictReader(csvfile))
 
-            for user_data in user_list:
-                # Creating User using email
-                user, _ = User.objects.get_or_create(username=user_data["e-mail address"], email=user_data["e-mail address"])
-                user.is_superuser = False
-                user.is_staff = False
-                user.password = make_password(user_data["e-mail address"])
-                user.save()
+                for user_data in user_list:
+                    self.create_user_with_token(user_data)
+        except FileNotFoundError:
+            logger.error(f"File {filename} not found.")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
 
-                # Save user profile
-                profile = user.profile
-                profile.limit_access_to_guest = True
-                profile.save(update_fields=["limit_access_to_guest"])
-                logging.info(f"User {user.username} created successfully")
+    def create_user_with_token(self, user_data):
+        email = user_data["e-mail address"]
+        first_name, last_name = self.split_name(user_data["Name"])
 
-                # Creating External Monty Token
-                user_external_token = UserExternalToken(
-                    title="CSV users Monty token",
-                    user=user,
-                    expire_timestamp=timezone.now() + timedelta(days=settings.JWT_EXPIRE_TIMESTAMP_DAYS),
-                )
-                token = jwt_encode_handler(user_external_token.get_payload())
-                user_external_token.token = token
-                user_external_token.save(update_fields=["token"])
-                print(f"External Token for {user.username} is {token}")
+        with transaction.atomic():
+            user, _ = User.objects.get_or_create(
+                username=email,
+                defaults={"first_name": first_name, "last_name": last_name, "email": email, "password": make_password(email)},
+            )
 
-        logging.info("All users created successfully")
+            user.profile.limit_access_to_guest = True
+            user.profile.save(update_fields=["limit_access_to_guest"])
+
+            user_external_token = UserExternalToken(
+                title="External user monty token",
+                user=user,
+                created_at=timezone.now(),
+                expire_timestamp=timezone.now() + timedelta(days=settings.JWT_EXPIRE_TIMESTAMP_DAYS),
+            )
+            token = jwt_encode_handler(user_external_token.get_payload())
+            self.stdout.write(self.style.SUCCESS(f"User: {user.username}, Monty Token: {token}"))
+
+    @staticmethod
+    def split_name(name):
+        parts = name.split(" ", 1)
+        first_name = parts[0].strip()
+        last_name = parts[1].strip() if len(parts) > 1 else ""
+        return first_name, last_name
