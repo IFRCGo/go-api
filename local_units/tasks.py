82db3fe1e796facaf5154e07af2e9ad2d988b608
Sushil Tiwari <susiltiwari750@gmail.com>
2024-12-18 14:47:50 +0545

    Add email backend

--- local_units/tasks.py
+++ local_units/tasks.py
@@ -14,24 +14,33 @@ def send_local_unit_email(local_unit_id, users_emails, new_or_updated=""):
 
     instance = LocalUnit.objects.get(id=local_unit_id)
     email_context = get_email_context(instance)
+    email_context["new_local_unit"] = True
     email_subject = f"Action Required: {new_or_updated} Local Unit Pending Validation"
     email_body = render_to_string("email/local_units/local_unit.html", email_context)
     email_type = f"{new_or_updated} Local Unit"
 
-    send_notification(email_subject, users_emails, email_body, email_type)
+    for email in users_emails:
+        # NOTE: Adding the validator email to the context
+        email_context["validator_email"] = email
+        send_notification(email_subject, email, email_body, email_type)
     return email_context
 
 
 @shared_task
-def edit_local_unit_email(local_unit_id, users_emails, new_or_updated=""):
+def update_local_unit_email(local_unit_id, users_emails, new_or_updated=""):
     if not local_unit_id or not users_emails:
         return None
 
     instance = LocalUnit.objects.get(id=local_unit_id)
     email_context = get_email_context(instance)
+    email_context["update_local_unit"] = True
     email_subject = "Action Required: Local Unit Pending Validation"
     email_body = render_to_string("email/local_units/edit_local_unit.html", email_context)
     email_type = f"{new_or_updated} Local Unit"
 
-    send_notification(email_subject, users_emails, email_body, email_type)
+    for email in users_emails:
+        # NOTE: Adding the validator email to the context
+        email_context["validator_email"] = email
+        send_notification(email_subject, email, email_body, email_type)
+
     return email_context
