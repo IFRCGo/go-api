efd9d800953a2fa946f788b6a0e255bc798792d2
sudip-khanal <sudipkhanal0777@gmail.com>
2024-12-31 16:22:31 +0545

    Add bulk update

--- api/management/commands/migrate_field_report_number.py
+++ api/management/commands/migrate_field_report_number.py
@@ -1,10 +1,15 @@
+import logging
+import os
 import re
 
+import psutil
 from django.core.management.base import BaseCommand
-from django.db import transaction
+from django.db.models import Count, Q
 
-from api.logger import logger
 from api.models import FieldReport
+from main.managers import BulkUpdateManager
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -13,52 +18,64 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         suffix_pattern = re.compile(r"#(\d+)")
 
-        reports = FieldReport.objects.filter(summary__icontains="#", event__isnull=False, countries__isnull=False)
+        event_country_filter_qs = (
+            FieldReport.objects.order_by()
+            .values("event", "countries")
+            .annotate(count=Count("id", filter=Q(fr_num__isnull=True)))
+            .exclude(count__gte=1)
+        )
 
+        reports = FieldReport.objects.filter(
+            event__in=event_country_filter_qs.values("event"),
+            countries__in=event_country_filter_qs.values("countries"),
+        ).distinct()
+
+        # reports = FieldReport.objects.filter(fr_num__isnull=True).select_related('event').distinct()
         logger.info(f"Found {reports.count()} FieldReports to process.")
 
         if not reports:
-            logger.warning("No FieldReports found exiting migration.")
+            logger.warning("No FieldReports found.")
             return
 
         event_country_data = {}
 
-        for report in reports:
+        for report in reports.iterator():
             country = report.countries.first()
 
             summary_match = suffix_pattern.search(report.summary)
             derived_fr_num = int(summary_match.group(1)) if summary_match else None
             key = (report.event.id, country.id)
 
-            # Initialize data for the event-country group if not present
-            if key not in event_country_data:
-                event_country_data[key] = {
+            # Initialize or retrieve group data
+            group_data = event_country_data[key] = event_country_data.get(
+                key,
+                {
                     "highest_fr_num": 0,
                     "report_highest_fr": None,
-                }
+                },
+            )
 
-            group_data = event_country_data[key]
             max_fr_num = max(derived_fr_num or 0, report.fr_num or 0)
 
-            # Update the group data if this report has the highest fr number
             if max_fr_num > group_data["highest_fr_num"]:
-
                 group_data["highest_fr_num"] = max_fr_num
                 group_data["report_highest_fr"] = report
 
-        with transaction.atomic():
-            for (event_id, country_id), data in event_country_data.items():
-                highest_report = data["report_highest_fr"]
-                highest_fr_num = data["highest_fr_num"]
+        bulk_manager = BulkUpdateManager(update_fields=["fr_num"])
 
-                if highest_report:
+        for (event_id, country_id), data in event_country_data.items():
+            highest_report = data["report_highest_fr"]
+            highest_fr_num = data["highest_fr_num"]
 
-                    highest_report.fr_num = highest_fr_num
-                    highest_report.save(update_fields=["fr_num"])
+            if highest_report:
+                logger.debug(f"Preparing to update FieldReport ID {highest_report.id} with fr_num {highest_fr_num}.")
+                highest_report.fr_num = highest_fr_num
+                bulk_manager._process_obj(highest_report)
+        bulk_manager._commit
 
-                # Set fr_num to null for all other reports in the group
-                FieldReport.objects.filter(event_id=event_id, countries=country_id).exclude(id=highest_report.id).update(
-                    fr_num=None
-                )
+        process = psutil.Process(os.getpid())
+
+        memory_usage = process.memory_info().rss / (1024 * 1024)
+        logger.info(f"Memory usage:{memory_usage} MB")
 
         logger.info("FieldReport migration completed successfully.")
