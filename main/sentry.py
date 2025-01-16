74ae6997236c81858e8ed083d19fe2124cd1d311
thenav56 <ayernavin@gmail.com>
2025-01-16 14:38:06 +0545

    Add cleartokens cronjob to clear expired tokens

--- main/sentry.py
+++ main/sentry.py
@@ -128,6 +128,7 @@ class SentryMonitor(models.TextChoices):
     INGEST_NS_DOCUMENT = "ingest_ns_document", "0 0 * * 0"
     INGEST_NS_INITIATIVES = "ingest_ns_initiatives", "0 0 * * 0"
     INGEST_ICRC = "ingest_icrc", "0 3 * * 0"
+    OAUTH_CLEARTOKENS = "oauth_cleartokens", "0 1 * * *"
 
     @staticmethod
     def load_cron_data() -> typing.List[typing.Tuple[str, str]]:
