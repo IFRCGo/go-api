6471646fe99afb25651737939b5aa3266a2d03fe
Sushil Tiwari <susiltiwari750@gmail.com>
2025-01-16 17:22:29 +0545

    Add is_global_validator on user me endpoint

--- api/drf_views.py
+++ api/drf_views.py
@@ -1337,7 +1337,13 @@ class UsersViewset(viewsets.ReadOnlyModelViewSet):
                         name__iexact="IFRC Admins",
                         user=OuterRef("pk"),
                     )
-                )
+                ),
+                is_local_unit_global_validator=models.Exists(
+                    Group.objects.filter(
+                        name__iexact="Local Unit Global Validators",
+                        user=OuterRef("pk"),
+                    )
+                ),
             )
             .filter(is_active=True)
         )
--- api/serializers.py
+++ api/serializers.py
@@ -1647,7 +1647,9 @@ class MiniSubscriptionSerializer(ModelSerializer):
 class UserSerializer(ModelSerializer):
     profile = ProfileSerializer()
     subscription = MiniSubscriptionSerializer(many=True)
+    # NOTE: This field is annotated in the viewset
     is_ifrc_admin = serializers.BooleanField(read_only=True)
+    is_local_unit_global_validator = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = User
@@ -1661,6 +1663,7 @@ class UserSerializer(ModelSerializer):
             "subscription",
             "is_superuser",
             "is_ifrc_admin",
+            "is_local_unit_global_validator",
         )
 
     def create(self, _):
