f82fceef5314ee1d598b14eba1900ec1a546ffda
sudip-khanal <sudipkhanal0777@gmail.com>
2025-07-10 11:45:00 +0545

    fixup! feat(command): add script to generate permissions by local unit type

--- local_units/management/commands/make_local_unit_validator_permissions.py
+++ local_units/management/commands/make_local_unit_validator_permissions.py
@@ -1,76 +1,77 @@
-import logging
-
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
-from api.models import Region
-from local_units.models import Country, LocalUnit, LocalUnitType
-
-logger = logging.getLogger(__name__)
+from api.models import Country, CountryType, Region
+from local_units.models import LocalUnit, LocalUnitType
 
 
 class Command(BaseCommand):
     help = "Create local unit validator permissions and groups for each local unit type"
 
+    @transaction.atomic
     def handle(self, *args, **options):
         content_type = ContentType.objects.get_for_model(LocalUnit)
         local_unit_types = LocalUnitType.objects.all()
         regions = Region.objects.all()
-        countries = Country.objects.filter(is_deprecated=False, independent=True, iso3__isnull=False)
-
+        countries = Country.objects.filter(
+            is_deprecated=False, independent=True, iso3__isnull=False, record_type=CountryType.COUNTRY
+        )
         # Global level Permissions and Groups
-        logger.info("Creating global validators permissions and groups...")
-        for local_unit_type in local_unit_types:
+        self.stdout.write(self.style.NOTICE("Creating global validators permissions and groups"))
+        for local_unit_type in local_unit_types.iterator():
             type_name = local_unit_type.name.replace(" ", "_").lower()
             codename = f"local_unit_global_validator_{type_name}"
-            permission, created = Permission.objects.get_or_create(
+            permission, _ = Permission.objects.get_or_create(
                 codename=codename, name=f"Local Unit Global Validator For {local_unit_type.name}", content_type=content_type
             )
             group_name = f"local_unit_global_validator_for_{type_name}"
-            group, group_created = Group.objects.get_or_create(name=group_name)
+            group, _ = Group.objects.get_or_create(name=group_name)
             group.permissions.add(permission)
-            logger.info(
-                f"Local unit global validator for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Local unit global validator for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+                )
             )
-
         # Region level Permissions and Groups
-        logger.info("Creating region-level permissions and groups...")
-        for region in regions:
+        self.stdout.write(self.style.NOTICE("Creating region-level permissions and groups"))
+        for region in regions.iterator():
             region_label = region.label.replace(" ", "_").lower()
-
-            for local_unit_type in local_unit_types:
+            for local_unit_type in local_unit_types.iterator():
                 type_name = local_unit_type.name.replace(" ", "_").lower()
                 codename = f"local_unit_validator_{region_label}_{type_name}"
-
-                permission, created = Permission.objects.get_or_create(
+                permission, _ = Permission.objects.get_or_create(
                     codename=codename,
                     name=f"Local Unit Validator For {local_unit_type.name} in {region.label}",
                     content_type=content_type,
                 )
                 group_name = f"local_unit_validator_for_{type_name}_{region_label}"
-                group, group_created = Group.objects.get_or_create(name=group_name)
+                group, _ = Group.objects.get_or_create(name=group_name)
                 group.permissions.add(permission)
-                logger.info(
-                    f"Local unit validator in {region.label} for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Local unit validator in {region.label} for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+                    )
                 )
-
         # Country level Permissions and Groups
-        logger.info("Creating country-level permissions and groups...")
-        for country in countries:
+        self.stdout.write(self.style.NOTICE("Creating country-level permissions and groups"))
+        for country in countries.iterator():
             country_name = country.name.replace(" ", "_").lower()
-            for local_unit_type in local_unit_types:
+            for local_unit_type in local_unit_types.iterator():
                 type_name = local_unit_type.name.replace(" ", "_").lower()
                 codename = f"local_unit_validator_{country_name}_{type_name}"
 
-                permission, created = Permission.objects.get_or_create(
+                permission, _ = Permission.objects.get_or_create(
                     codename=codename,
                     name=f"Local Unit Validator For {local_unit_type.name} in {country.name}",
                     content_type=content_type,
                 )
                 group_name = f"local_unit_validator_for_{type_name}_{country_name}"
-                group, group_created = Group.objects.get_or_create(name=group_name)
+                group, _ = Group.objects.get_or_create(name=group_name)
                 group.permissions.add(permission)
-                logger.info(
-                    f"Local unit validator in {country.name} for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Local unit validator in {country.name} for {local_unit_type.name} permission and group created successfully"  # noqa: E501
+                    )
                 )
