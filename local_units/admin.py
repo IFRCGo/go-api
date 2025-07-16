34b9d21a99f3380b4509aa465849d37bf498a432
sudip-khanal <sudipkhanal0777@gmail.com>
2025-07-16 21:07:40 +0545

    feat(models): add model to tract local unit bulk upload history

--- local_units/admin.py
+++ local_units/admin.py
@@ -16,6 +16,7 @@ from .models import (
     HealthData,
     HospitalType,
     LocalUnit,
+    LocalUnitBulkUpload,
     LocalUnitChangeRequest,
     LocalUnitLevel,
     LocalUnitType,
@@ -51,6 +52,7 @@ class LocalUnitAdmin(CompareVersionAdmin, admin.OSMGeoAdmin):
         "type",
         "level",
         "health",
+        "bulk_upload",
     )
     readonly_fields = (
         "validated",
@@ -69,6 +71,27 @@ class LocalUnitAdmin(CompareVersionAdmin, admin.OSMGeoAdmin):
         if obj.type.code == 1 and obj.health:
             raise ValidationError({"Can't have health data for type %s" % obj.type.code})
         super().save_model(request, obj, form, change)
+
+
+@admin.register(LocalUnitBulkUpload)
+class LocalUnitBulkUploadAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    search_fields = ("country__name", "local_unit_type__id")
+    list_select_related = True
+    autocomplete_fields = (
+        "country",
+        "local_unit_type",
+        "triggered_by",
+    )
+    list_filter = ("status",)
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "triggered_by",
+            )
+        )
 
 
 @admin.register(LocalUnitChangeRequest)
