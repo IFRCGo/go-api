12c1a0093656f3c8bd9609775311de76d2f19ccf
Sudip Khanal <101724348+sudip-khanal@users.noreply.github.com>
2025-07-18 14:29:15 +0545

    feat(local-unit): add model for externally managed local unit (#2515)
    * feat(local-unit): add model for externally managed local unit
    * feat(api): add api for externally managed local unit
    * feat(test): add test case for externally managed local unit api
    * refactor(api): refactor externally managed api
    * feat(api): add filter on externally managed api
    * chore(api): make update local unit reagion required false
    * fix(models): add unique constraint to externally managed local unit model

--- local_units/test_views.py
+++ local_units/test_views.py
@@ -6,7 +6,8 @@ from django.contrib.gis.geos import Point
 from django.core import management
 from factory import fuzzy
 
-from api.models import Country, Region
+from api.factories.country import CountryFactory
+from api.models import Country, CountryType, Region
 from deployments.factories.user import UserFactory
 from main.test_case import APITestCase
 
@@ -14,6 +15,7 @@ from .models import (
     Affiliation,
     DelegationOffice,
     DelegationOfficeType,
+    ExternallyManagedLocalUnit,
     FacilityType,
     Functionality,
     HealthData,
@@ -38,6 +40,11 @@ class LocalUnitFactory(factory.django.DjangoModelFactory):
 class HealthDataFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = HealthData
+
+
+class ExternallyManagedLocalUnitFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ExternallyManagedLocalUnit
 
 
 class TestLocalUnitsListView(APITestCase):
@@ -418,6 +425,7 @@ class TestLocalUnitCreate(APITestCase):
             "subtype": "",
             "date_of_data": "2024-05-13",
             "level": level.id,
+            "update_reason_overview": "Needed update for testing",
             "location_json": {
                 "lat": 42.066667,
                 "lng": 19.983333,
@@ -525,6 +533,7 @@ class TestLocalUnitCreate(APITestCase):
             "subtype": "",
             "date_of_data": "2024-05-13",
             "level": level.id,
+            "update_reason_overview": "Needed update for testing",
             "location_json": {
                 "lat": 42.066667,
                 "lng": 19.983333,
@@ -613,6 +622,7 @@ class TestLocalUnitCreate(APITestCase):
             "date_of_data": "2024-05-13",
             "level": level.id,
             "address_loc": "Silele Clinic is is in Hosea Inkhundla under the Shiselweni, Sigombeni is in Nkom'iyahlaba Inkhundla under the Manzini region and Mahwalala is in the Mbabane West Inkhundla under the Hhohho region.",  # noqa: E501
+            "update_reason_overview": "Needed update for testing",
             "location_json": {
                 "lat": 42.066667,
                 "lng": 19.983333,
@@ -664,6 +674,7 @@ class TestLocalUnitCreate(APITestCase):
                 "lat": 42.066667,
                 "lng": 19.983333,
             },
+            "update_reason_overview": "Needed update for testing",
         }
         self.authenticate()
         response = self.client.post("/api/v2/local-units/", data=data, format="json")
@@ -712,3 +723,117 @@ class TestLocalUnitCreate(APITestCase):
             local_unit=local_unit_id, status=LocalUnitChangeRequest.Status.APPROVED
         ).last()
         self.assertEqual(local_unit_request.current_validator, Validator.GLOBAL)
+
+
+class TestExternallyManagedLocalUnit(APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = UserFactory.create()
+        self.root_user = UserFactory.create(is_superuser=True)
+        self.region = Region.objects.create(name=2)
+        self.country1 = CountryFactory.create(
+            name="Nepal",
+            iso3="NLP",
+            iso="NP",
+            region=self.region,
+            is_deprecated=False,
+            independent=True,
+            record_type=CountryType.COUNTRY,
+        )
+        self.country2 = CountryFactory.create(
+            name="India",
+            iso3="IND",
+            region=self.region,
+            is_deprecated=False,
+            independent=True,
+            record_type=CountryType.COUNTRY,
+        )
+        self.local_unit_type = LocalUnitType.objects.create(code=1, name="Code 0")
+
+        self.externally_managed_local_unit = ExternallyManagedLocalUnitFactory.create(
+            country=self.country1,
+            local_unit_type=self.local_unit_type,
+            created_by=self.user,
+            updated_by=self.user,
+        )
+
+    def test_filter_by_country_name(self):
+        self.client.force_authenticate(user=self.root_user)
+        url = "/api/v2/externally-managed-local-unit/?country__name=Nepal"
+        response = self.client.get(url)
+        self.assert_200(response)
+        self.assertEqual(response.data["count"], 1)
+
+    def test_filter_by_country_iso3(self):
+        self.client.force_authenticate(user=self.root_user)
+        url = "/api/v2/externally-managed-local-unit/?country__iso3=IND"
+        response = self.client.get(url)
+        self.assert_200(response)
+        self.assertEqual(response.data["count"], 0)
+
+    def test_filter_by_country_iso_invalid(self):
+        self.client.force_authenticate(user=self.root_user)
+        url = "/api/v2/externally-managed-local-unit/?country__iso=PH"
+        response = self.client.get(url)
+        self.assert_200(response)
+        self.assertEqual(response.data["count"], 0)
+
+    def test_create_externally_managed_local_unit(self):
+        url = "/api/v2/externally-managed-local-unit/"
+        data = {"country": self.country2.id, "local_unit_type": self.local_unit_type.id, "enabled": True}
+        # Without authentication
+        response = self.client.patch(url, data=data, format="json")
+        self.assert_401(response)
+
+        # Normal user
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data=data)
+        self.assert_403(response)
+
+        # Superuser
+        self.client.force_authenticate(user=self.root_user)
+        response = self.client.post(url, data=data)
+        self.assert_201(response)
+
+        # Try to create the duplicate
+        self.client.force_authenticate(user=self.root_user)
+        response = self.client.post(url, data=data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_externally_managed_local_unit(self):
+        url = f"/api/v2/externally-managed-local-unit/{self.externally_managed_local_unit.id}/"
+        data = {
+            "enabled": True,
+        }
+        # Without authentication
+        response = self.client.patch(url, data=data, format="json")
+        self.assert_401(response)
+
+        # Normal user
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(url, data=data, format="json")
+        self.assert_403(response)
+
+        # Superuser
+        self.client.force_authenticate(user=self.root_user)
+        response = self.client.patch(url, data=data, format="json")
+        self.assert_200(response)
+
+        self.externally_managed_local_unit.refresh_from_db()
+        self.assertTrue(self.externally_managed_local_unit.enabled)
+
+    def test_get_externally_managed_local_unit(self):
+        url = "/api/v2/externally-managed-local-unit/"
+        # Without authentication
+        response = self.client.get(url)
+        self.assert_401(response)
+
+        # Normal user
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assert_403(response)
+
+        # Superuser
+        self.client.force_authenticate(user=self.root_user)
+        response = self.client.get(url)
+        self.assert_200(response)
