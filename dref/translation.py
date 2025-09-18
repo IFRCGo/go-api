82ab62d6caa175df07e1aa30a963aae68fa3d911
Sushil Tiwari <susiltiwari750@gmail.com>
2025-09-18 22:52:07 +0545

    feat(dref): Skips fields on dref translation

--- dref/translation.py
+++ dref/translation.py
@@ -1,11 +1,16 @@
 from modeltranslation.translator import TranslationOptions, register
 
-from dref.models import Dref, DrefFinalReport, DrefOperationalUpdate
-
-"""
-NOTE:Commenting out for now as, we are currently
-working on translation feature on dref
-"""
+from dref.models import (
+    Dref,
+    DrefFile,
+    DrefFinalReport,
+    DrefOperationalUpdate,
+    IdentifiedNeed,
+    NationalSocietyAction,
+    PlannedIntervention,
+    PlannedInterventionIndicators,
+    RiskSecurity,
+)
 
 
 @register(Dref)
@@ -14,7 +19,34 @@ class DrefTO(TranslationOptions):
         "title",
         "title_prefix",
         "event_text",
+        "ns_request_text",
+        "dref_recurrent_text",
+        "lessons_learned",
+        "child_safeguarding_risk_level",
+        "event_description",
+        "anticipatory_actions",
+        "event_scope",
+        "national_authorities",
+        "ifrc",
+        "icrc",
+        "partner_national_society",
+        "un_or_other_actor",
+        "major_coordination_mechanism",
+        "identified_gaps",
+        "people_assisted",
+        "selection_criteria",
+        "operation_objective",
+        "response_strategy",
+        "human_resource",
+        "is_volunteer_team_diverse",
+        "surge_personnel_deployed",
+        "logistic_capacity_of_ns",
+        "pmer",
+        "communication",
+        "risk_security_concern",
     )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
     skip_fields = (
         "ns_request_text",
         "dref_recurrent_text",
@@ -44,9 +76,12 @@ class DrefTO(TranslationOptions):
     )
 
 
-# @register(DrefFile)
-# class DrefFileTO(TranslationOptions):
-#     fields = ("caption",)
+@register(DrefFile)
+class DrefFileTO(TranslationOptions):
+    fields = ("caption",)
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = ("caption",)
 
 
 @register(DrefFinalReport)
@@ -54,7 +89,25 @@ class DrefFinalReportTO(TranslationOptions):
     fields = (
         "title",
         "title_prefix",
+        "event_description",
+        "anticipatory_actions",
+        "event_scope",
+        "ifrc",
+        "icrc",
+        "partner_national_society",
+        "national_authorities",
+        "un_or_other_actor",
+        "selection_criteria",
+        "operation_objective",
+        "response_strategy",
+        "major_coordination_mechanism",
+        "risk_security_concern",
+        "event_text",
+        "national_society_conducted_description",
+        "financial_report_description",
     )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
     skip_fields = (
         "event_description",
         "anticipatory_actions",
@@ -82,7 +135,28 @@ class DrefOperationalUpdateTO(TranslationOptions):
         "title_prefix",
         "event_description",
         "anticipatory_actions",
+        "event_scope",
+        "ifrc",
+        "icrc",
+        "partner_national_society",
+        "national_authorities",
+        "un_or_other_actor",
+        "major_coordination_mechanism",
+        "people_assisted",
+        "selection_criteria",
+        "operation_objective",
+        "response_strategy",
+        "risk_security_concern",
+        "anticipatory_to_response",
+        "human_resource",
+        "is_volunteer_team_diverse",
+        "surge_personnel_deployed",
+        "logistic_capacity_of_ns",
+        "pmer",
+        "communication",
     )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
     skip_fields = (
         "event_scope",
         "ifrc",
@@ -106,38 +180,67 @@ class DrefOperationalUpdateTO(TranslationOptions):
     )
 
 
-# @register(IdentifiedNeed)
-# class IdentifiedNeedTO(TranslationOptions):
-#     fields = (
-#         "title",
-#         "description",
-#     )
+@register(IdentifiedNeed)
+class IdentifiedNeedTO(TranslationOptions):
+    fields = (
+        "title",
+        "description",
+    )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = (
+        "title",
+        "description",
+    )
 
 
-# @register(NationalSocietyAction)
-# class NationalSocietyActionTO(TranslationOptions):
-#     fields = ("description",)
+@register(NationalSocietyAction)
+class NationalSocietyActionTO(TranslationOptions):
+    fields = ("description",)
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = ("description",)
 
 
-# @register(PlannedIntervention)
-# class PlannedInterventionTO(TranslationOptions):
-#     fields = (
-#         "title",
-#         "description",
-#         "progress_towards_outcome",
-#         "narrative_description_of_achievements",
-#         "challenges",
-#         "lessons_learnt",
-#     )
+@register(PlannedIntervention)
+class PlannedInterventionTO(TranslationOptions):
+    fields = (
+        "title",
+        "description",
+        "progress_towards_outcome",
+        "narrative_description_of_achievements",
+        "challenges",
+        "lessons_learnt",
+    )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = (
+        "title",
+        "description",
+        "progress_towards_outcome",
+        "narrative_description_of_achievements",
+        "challenges",
+        "lessons_learnt",
+    )
 
-# @register(PlannedInterventionIndicators)
-# class PlannedInterventionIndicatorsTO(TranslationOptions):
-#     fields = ("title",)
+
+@register(PlannedInterventionIndicators)
+class PlannedInterventionIndicatorsTO(TranslationOptions):
+    fields = ("title",)
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = ("title",)
 
 
-# @register(RiskSecurity)
-# class RiskSecurityTO(TranslationOptions):
-#     fields = (
-#         "risk",
-#         "mitigation",
-#     )
+@register(RiskSecurity)
+class RiskSecurityTO(TranslationOptions):
+    fields = (
+        "risk",
+        "mitigation",
+    )
+    # NOTE: Adding skip_fields temporarily to avoid removing previously translated data
+    # TODO(sudip): Remove skip_fields after dref translation feature is implemented
+    skip_fields = (
+        "risk",
+        "mitigation",
+    )
