d50009cfe2fa3116e5953c5a06c3ae756604d851
Shreeyash Shrestha <shreeyash.shrestha@togglecorp.com>
2025-10-28 15:46:40 +0545

    fix(local-units): remove other profiles field from and add mixin

--- local_units/test_views.py
+++ local_units/test_views.py
@@ -34,7 +34,6 @@ from .models import (
     LocalUnitChangeRequest,
     LocalUnitLevel,
     LocalUnitType,
-    OtherProfile,
     PrimaryHCC,
     ProfessionalTrainingFacility,
     SpecializedMedicalService,
@@ -525,7 +524,6 @@ class TestLocalUnitCreate(APITestCase):
         functionality = Functionality.objects.create(code=1, name="Code 1")
         health_facility_type = FacilityType.objects.create(code=1, name="Code 1")
         primary_health_care_center = PrimaryHCC.objects.create(code=1, name="Code 1")
-        other_profiles = OtherProfile.objects.create(number=1, position="test")
         data = {
             "local_branch_name": "Silele Red Cross Clinic, Sigombeni Red Cross Clinic & Mahwalala Red Cross Clinic",
             "english_branch_name": None,
@@ -578,7 +576,10 @@ class TestLocalUnitCreate(APITestCase):
                 "nursing_aid": 0,
                 "midwife": 9,
                 "other_medical_heal": True,
-                "other_profiles": [other_profiles.id],
+                "other_profiles": [
+                    {"number": 2, "position": "Nurse"},
+                    {"number": 3, "position": "Doctor"},
+                ],
                 "feedback": "first question of initial question did not provide for the option to write the name of the NS. It is written LRC yet it should allow Baphalali Eswatini Red Cross Society (BERCS) to be inscribed in the box.",  # noqa: E501
                 "affiliation": affiliation.id,
                 "functionality": functionality.id,
