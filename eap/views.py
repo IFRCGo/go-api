6d06b8819e9c6d27ce0ffb329e9abfeda6a66204
Sushil Tiwari <susiltiwari750@gmail.com>
2025-11-04 14:27:14 +0545

    feat(eap): Add DevelopmentRegistrationEAP Endpoint
    - Add Serializer, filterset
    - Admin setup, router for eap development-registration

--- eap/views.py
+++ eap/views.py
@@ -1,1 +1,29 @@
 # Create your views here.
+from rest_framework import permissions, viewsets
+
+from eap.filter_set import DevelopmentRegistrationEAPFilterSet
+from eap.models import DevelopmentRegistrationEAP
+from eap.serializers import DevelopmentRegistrationEAPSerializer
+from main.permissions import DenyGuestUserMutationPermission
+
+
+class DevelopmentRegistrationEAPViewset(viewsets.ModelViewSet):
+    queryset = DevelopmentRegistrationEAP.objects.all()
+    serializer_class = DevelopmentRegistrationEAPSerializer
+    permission_classes = [permissions.IsAuthenticated, DenyGuestUserMutationPermission]
+    filterset_class = DevelopmentRegistrationEAPFilterSet
+
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .select_related(
+                "created_by",
+                "modified_by",
+                "national_society",
+                "disaster_type",
+            )
+            .prefetch_related(
+                "partners",
+            )
+        )
