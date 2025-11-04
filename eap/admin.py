8ec9f7ac1f7cacf7a68470d6b29dc1fcb67162c2
Sushil Tiwari <susiltiwari750@gmail.com>
2025-11-04 14:27:14 +0545

    feat(eap): Add DevelopmentRegistrationEAP Endpoint
    - Add Serializer, filterset
    - Admin setup, router for eap development-registration

--- eap/admin.py
+++ eap/admin.py
@@ -1,1 +1,43 @@
-# Register your models here.
+from django.contrib import admin
+
+from eap.models import DevelopmentRegistrationEAP
+
+
+@admin.register(DevelopmentRegistrationEAP)
+class DevelopmentRegistrationEAPAdmin(admin.ModelAdmin):
+    list_select_related = True
+    search_fields = (
+        "national_society__name",
+        "country__name",
+        "disaster_type__name",
+    )
+    list_filter = ("eap_type", "disaster_type", "national_society")
+    list_display = (
+        "national_society",
+        "country",
+        "eap_type",
+        "disaster_type",
+    )
+    autocomplete_fields = (
+        "national_society",
+        "disaster_type",
+        "partners",
+        "created_by",
+        "modified_by",
+    )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "national_society",
+                "country",
+                "disaster_type",
+                "created_by",
+                "modified_by",
+            )
+            .prefetch_related(
+                "partners",
+            )
+        )
