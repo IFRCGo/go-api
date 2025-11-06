dd93de11d5deba60baf690a4abd9bcbac564ba07
Sushil Tiwari <susiltiwari750@gmail.com>
2025-11-06 16:02:29 +0545

    feat(eap): Add simplified model, operational, actions

--- eap/enums.py
+++ eap/enums.py
@@ -3,4 +3,6 @@ from . import models
 enum_register = {
     "eap_status": models.EAPStatus,
     "eap_type": models.EAPType,
+    "sector": models.PlannedOperations.Sector,
+    "timeframe": models.OperationActivity.TimeFrame,
 }
