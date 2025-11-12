ae1de6f5fa22d59b8955eed003b4738337351868
sandeshit <thapasandesh56@gmail.com>
2025-11-12 11:36:39 +0545

    chore(sentry): add cron monitor for tracking

--- main/sentry.py
+++ main/sentry.py
@@ -128,6 +128,9 @@ class SentryMonitor(models.TextChoices):
     INGEST_NS_DOCUMENT = "ingest_ns_document", "0 0 * * 0"
     INGEST_NS_INITIATIVES = "ingest_ns_initiatives", "0 0 * * 0"
     INGEST_ICRC = "ingest_icrc", "0 3 * * 0"
+    POLL_USGS_EQ = "poll_usgs_eq", "0 0 * * 0"
+    POLL_GDACS_FL = "poll_gdacs_fl", "0 0 * * 0"
+    POLL_GDACS_CY = "poll_gdacs_cy", "0 0 * * 0"
     # NOTIFY_VALIDATORS = "notify_validators", "0 0 * * *" # NOTE: Disable local unit email notification for now
     OAUTH_CLEARTOKENS = "oauth_cleartokens", "0 1 * * *"
 
