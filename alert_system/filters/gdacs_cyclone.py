bfad679b568849016c07be0afb9c865c74bb5540
sandeshit <thapasandesh56@gmail.com>
2025-11-23 23:20:37 +0545

    feat(filter): add filtration classes and cronjob

---
+++ alert_system/filters/gdacs_cyclone.py
@@ -0,0 +1,15 @@
+from .base_filter import BaseFilter
+
+
+class GdacsCycloneFilter(BaseFilter):
+
+    impact_threshold = {
+        ("people", "death"): 1,
+        ("people", "injured"): 10,
+        ("people", "missing"): 1,
+    }
+
+    hazard_threshold = {
+        "severity_value": 5,
+        "severity_label": "Green",
+    }
