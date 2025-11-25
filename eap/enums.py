f61a1f588ca052cc3151d0856cbe5a31358c0bcb
Sushil Tiwari <susiltiwari750@gmail.com>
2025-11-25 11:41:06 +0545

    feat(eap): Add validation on operation timeframe and time_value
    - Update test cases
    - Add timeframe values on enums

--- eap/enums.py
+++ eap/enums.py
@@ -5,5 +5,9 @@ enum_register = {
     "eap_type": models.EAPType,
     "sector": models.PlannedOperation.Sector,
     "timeframe": models.OperationActivity.TimeFrame,
+    "years_timeframe_value": models.OperationActivity.YearsTimeFrameChoices,
+    "months_timeframe_value": models.OperationActivity.MonthsTimeFrameChoices,
+    "days_timeframe_value": models.OperationActivity.DaysTimeFrameChoices,
+    "hours_timeframe_value": models.OperationActivity.HoursTimeFrameChoices,
     "approach": models.EnableApproach.Approach,
 }
