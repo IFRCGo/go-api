77560f31722d17f7f00da61bdec6ffe1b950ed28
sandeshit <thapasandesh56@gmail.com>
2025-11-28 14:02:21 +0545

    feat(etl): refactor existing extraction into ETL.
    This commit splits the data extraction, transform and load part.
    The filtration part is also included in the load part.

--- alert_system/mappings.py
+++ alert_system/mappings.py
@@ -1,22 +1,15 @@
 from dataclasses import dataclass
 
-from alert_system.extraction.gdacs_cyclone import GdacsCycloneExtraction
-from alert_system.extraction.gdacs_flood import GdacsFloodExtraction
-from alert_system.extraction.usgs_earthquake import USGSEarthquakeExtraction
-from alert_system.filters.gdacs_cyclone import GdacsCycloneFilter
-from alert_system.filters.gdacs_flood import GdacsFloodFilter
-from alert_system.filters.usgs_earthquake import USGSEarthquakeFilter
+from alert_system.etl.Gdacs_flood.extraction import GdacsFloodExtraction
 from alert_system.models import Connector
 
 
+# NOTE: Store all the mappings here.
 @dataclass
 class ConnectorClasses:
     extractor: type
-    filter: type
 
 
 CONNECTOR_REGISTRY = {
-    Connector.ConnectorType.GDACS_FLOOD: ConnectorClasses(extractor=GdacsFloodExtraction, filter=GdacsFloodFilter),
-    Connector.ConnectorType.GDACS_CYCLONE: ConnectorClasses(extractor=GdacsCycloneExtraction, filter=GdacsCycloneFilter),
-    Connector.ConnectorType.USGS_EARTHQUAKE: ConnectorClasses(extractor=USGSEarthquakeExtraction, filter=USGSEarthquakeFilter),
+    Connector.ConnectorType.GDACS_FLOOD: ConnectorClasses(extractor=GdacsFloodExtraction),
 }
