3d2f260edb5262ac3a29d937de60920c82fc414a
sandeshit <thapasandesh56@gmail.com>
2025-11-28 14:02:21 +0545

    feat(etl): refactor existing extraction into ETL.
    This commit splits the data extraction, transform and load part.
    The filtration part is also included in the load part.

--- alert_system/extraction/usgs_earthquake.py
+++
@@ -1,7 +0,0 @@
-from alert_system.extraction.base_extraction import BaseExtractionClass
-
-
-class USGSEarthquakeExtraction(BaseExtractionClass):
-    event_endpoint = "/usgs-events/items"
-    hazard_endpoint = "/usgs-hazards/items"
-    impact_endpoint = "/usgs-impacts/items"
