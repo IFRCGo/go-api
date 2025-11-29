d71e1d0c71d55d805b41a36e32073464e840a8ea
sudip-khanal <sudipkhanal0777@gmail.com>
2025-11-29 19:49:12 +0545

    feat(notifications): add alert subscription api

--- notifications/enums.py
+++ notifications/enums.py
@@ -2,4 +2,7 @@ from . import models
 
 enum_register = {
     "surge_alert_status": models.SurgeAlertStatus,
+    "alert_source": models.AlertSubscription.AlertSource,
+    "hazard_type": models.HazardType.Type,
+    "alert_per_day": models.AlertSubscription.AlertPerDay,
 }
