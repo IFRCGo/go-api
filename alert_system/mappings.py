c06b27c0ff6a8f1c285876106674e9f31c08a9f5
sandeshit <thapasandesh56@gmail.com>
2025-12-07 23:28:54 +0545

    chore(alert-system): clean up naming and cronjobs

--- alert_system/mappings.py
+++ alert_system/mappings.py
@@ -1,15 +1,24 @@
 from dataclasses import dataclass
 
+from alert_system.etl.base.extraction import BaseExtractionClass
+from alert_system.etl.base.loader import BaseLoaderClass
+from alert_system.etl.base.transform import BaseTransformerClass
 from alert_system.etl.Gdacs_flood.extraction import GdacsFloodExtraction
+from alert_system.etl.Gdacs_flood.loader import GdacsLoader
+from alert_system.etl.Gdacs_flood.transform import GdacsTransformer
 from alert_system.models import Connector
 
 
 # NOTE: Store all the mappings here.
 @dataclass
 class ConnectorClasses:
-    extractor: type
+    extractor: type[BaseExtractionClass]
+    transfomer: type[BaseTransformerClass]
+    loader: type[BaseLoaderClass]
 
 
 CONNECTOR_REGISTRY = {
-    Connector.ConnectorType.GDACS_FLOOD: ConnectorClasses(extractor=GdacsFloodExtraction),
+    Connector.ConnectorType.GDACS_FLOOD: ConnectorClasses(
+        extractor=GdacsFloodExtraction, transfomer=GdacsTransformer, loader=GdacsLoader
+    ),
 }
