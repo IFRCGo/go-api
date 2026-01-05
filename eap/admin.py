05ff56e2524dacbb7518310657b3d0b4d34e9473
Sushil Tiwari <susiltiwari750@gmail.com>
2026-01-05 23:21:50 +0545

    refactor(export): Decoupling pdf export of playwright
    - Fix appeal failing test cases(use mocking timezone)

--- eap/admin.py
+++ eap/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.db import transaction
 
-from eap.models import EAPFile, EAPRegistration, EAPType, FullEAP, KeyActor, SimplifiedEAP
+from eap.models import (
+    EAPFile,
+    EAPRegistration,
+    EAPType,
+    FullEAP,
+    KeyActor,
+    SimplifiedEAP,
+)
 
 
 @admin.register(EAPFile)
