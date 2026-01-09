a7ecd26aee3f2d36de74ae68732a7ca196661fda
Sudip Khanal <101724348+sudip-khanal@users.noreply.github.com>
2026-01-09 16:15:11 +0545

    EAP: email notification setup (#2624)

--- eap/tasks.py
+++ eap/tasks.py
@@ -1,14 +1,23 @@
 from datetime import datetime
 
 from celery import shared_task
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.utils import timezone
 from rest_framework.authtoken.models import Token
 
 from api.logger import logger
 from api.playwright import render_pdf_from_url
 from api.utils import generate_eap_export_url
 from eap.models import EAPRegistration, EAPType, FullEAP, SimplifiedEAP
+from eap.utils import (
+    get_coordinator_emails_by_region,
+    get_eap_email_context,
+    get_eap_registration_email_context,
+)
 from main.utils import logger_context
+from notifications.notification import send_notification
 
 
 def build_filename(eap_registration: EAPRegistration) -> str:
@@ -108,7 +117,6 @@ def generate_export_eap_pdf(eap_registration_id, version):
     eap_registration = EAPRegistration.objects.get(id=eap_registration_id)
     user = User.objects.get(id=eap_registration.created_by_id)
     token = Token.objects.filter(user=user).last()
-
     url = generate_eap_export_url(
         registration_id=eap_registration_id,
         version=version,
@@ -152,3 +160,456 @@ def generate_export_eap_pdf(eap_registration_id, version):
                 dict(eap_registration_id=eap_registration.pk),
             ),
         )
+
+
+@shared_task
+def send_new_eap_registration_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipients = [
+        settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+        instance.ifrc_contact_email,
+    ]
+    cc_recipients = list(
+        set(
+            [
+                instance.national_society_contact_email,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+            ]
+        )
+    )
+    email_context = get_eap_registration_email_context(instance)
+    email_subject = (
+        f"[{instance.get_eap_type_display() if instance.get_eap_type_display() else 'EAP'} IN DEVELOPMENT] "
+        f"{instance.country} {instance.disaster_type}"
+    )
+    email_body = render_to_string("email/eap/registration.html", email_context)
+    email_type = "New EAP Registration"
+
+    send_notification(
+        subject=email_subject,
+        recipients=recipients,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+
+    return email_context
+
+
+@shared_task
+def send_new_eap_submission_email(eap_registration_id: int):
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP:
+        latest_eap = instance.latest_simplified_eap
+    else:
+        latest_eap = instance.latest_full_eap
+
+    if not latest_eap.export_file:
+        generate_export_eap_pdf(
+            eap_registration_id=instance.id,
+            version=latest_eap.version,
+        )
+    partner_contacts = latest_eap.partner_contacts
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipients = [
+        settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+        instance.ifrc_contact_email,
+    ]
+    cc_recipients = list(
+        set(
+            [
+                instance.national_society_contact_email,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = (
+        f"[DREF {instance.get_eap_type_display()} FOR REVIEW] " f"{instance.country} {instance.disaster_type} TO THE IFRC-DREF"
+    )
+    email_body = render_to_string("email/eap/submission.html", email_context)
+    email_type = "EAP Submission"
+    send_notification(
+        subject=email_subject,
+        recipients=recipients,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+
+    return email_context
+
+
+@shared_task
+def send_feedback_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP:
+        latest_eap = instance.latest_simplified_eap
+    else:
+        latest_eap = instance.latest_full_eap
+
+    ifrc_delegation_focal_point_email = latest_eap.ifrc_delegation_focal_point_email
+
+    partner_contacts = latest_eap.partner_contacts
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipient = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                ifrc_delegation_focal_point_email,
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = (
+        f"[DREF {instance.get_eap_type_display()} FEEDBACK] "
+        f"{instance.country} {instance.disaster_type} TO THE {instance.national_society}"
+    )
+    email_body = render_to_string("email/eap/feedback_to_national_society.html", email_context)
+    email_type = "Feedback to the National Society"
+    send_notification(
+        subject=email_subject,
+        recipients=recipient,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+
+    return email_context
+
+
+@shared_task
+def send_eap_resubmission_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+    if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP:
+        latest_eap = instance.latest_simplified_eap
+    else:
+        latest_eap = instance.latest_full_eap
+
+    latest_version = latest_eap.version
+
+    if not latest_eap.diff_file:
+        generate_export_diff_pdf(
+            eap_registration_id=instance.id,
+            version=latest_eap.version,
+        )
+
+    partner_contacts = latest_eap.partner_contacts
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipients = [
+        settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+        instance.ifrc_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                instance.national_society_contact_email,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = (
+        f"[DREF {instance.get_eap_type_display()} FOR REVIEW] "
+        f"{instance.country} {instance.disaster_type} version {latest_version} TO THE IFRC-DREF"
+    )
+    email_body = render_to_string("email/eap/re-submission.html", email_context)
+    email_type = "Feedback to the National Society"
+    send_notification(
+        subject=email_subject,
+        recipients=recipients,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+
+    return email_context
+
+
+@shared_task
+def send_feedback_email_for_resubmitted_eap(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP:
+        latest_eap = instance.latest_simplified_eap
+        eap_model = SimplifiedEAP
+    else:
+        latest_eap = instance.latest_full_eap
+        eap_model = FullEAP
+
+    latest_version = latest_eap.version
+
+    partner_contacts = latest_eap.partner_contacts
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    previous_eap = (
+        eap_model.objects.filter(
+            eap_registration=instance,
+            version__lt=latest_version,
+        )
+        .order_by("-version")
+        .first()
+    )
+
+    previous_version = previous_eap.version if previous_eap else None
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipients = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = (
+        f"[DREF {instance.get_eap_type_display()} FEEDBACK] "
+        f"{instance.country} {instance.disaster_type} version {previous_version} TO {instance.national_society}"
+    )
+    email_body = render_to_string("email/eap/feedback_to_revised_eap.html", email_context)
+    email_type = "Feedback to the National Society"
+    send_notification(
+        subject=email_subject,
+        recipients=recipients,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+
+    return email_context
+
+
+@shared_task
+def send_technical_validation_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    partner_contacts = (
+        instance.latest_simplified_eap.partner_contacts
+        if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP
+        else instance.latest_full_eap.partner_contacts
+    )
+
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipient = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = f"[DREF {instance.get_eap_type_display()} TECHNICALLY VALIDATED] {instance.country} {instance.disaster_type}"
+    email_body = render_to_string("email/eap/technically_validated_eap.html", email_context)
+    email_type = "Technically Validated EAP"
+    send_notification(
+        subject=email_subject,
+        recipients=recipient,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+    return email_context
+
+
+@shared_task
+def send_pending_pfa_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP:
+        latest_eap = instance.latest_simplified_eap
+    else:
+        latest_eap = instance.latest_full_eap
+
+        if not latest_eap.summary_file:
+            generate_eap_summary_pdf(
+                eap_registration_id=instance.id,
+            )
+
+    partner_contacts = latest_eap.partner_contacts
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipient = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = f"[DREF {instance.get_eap_type_display()} APPROVED PENDING PFA] {instance.country} {instance.disaster_type}"
+    email_body = render_to_string("email/eap/pending_pfa.html", email_context)
+    email_type = "Approved Pending PFA EAP"
+    send_notification(
+        subject=email_subject,
+        recipients=recipient,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+    return email_context
+
+
+@shared_task
+def send_approved_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    partner_contacts = (
+        instance.latest_simplified_eap.partner_contacts
+        if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP
+        else instance.latest_full_eap.partner_contacts
+    )
+
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipient = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = f"[DREF {instance.get_eap_type_display()} APPROVED] {instance.country} {instance.disaster_type}"
+    email_body = render_to_string("email/eap/approved.html", email_context)
+    email_type = "Approved EAP"
+    send_notification(
+        subject=email_subject,
+        recipients=recipient,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+    return email_context
+
+
+@shared_task
+def send_deadline_reminder_email(eap_registration_id: int):
+
+    instance = EAPRegistration.objects.filter(id=eap_registration_id).first()
+    if not instance:
+        return None
+
+    partner_contacts = (
+        instance.latest_simplified_eap.partner_contacts
+        if instance.get_eap_type_enum == EAPType.SIMPLIFIED_EAP
+        else instance.latest_full_eap.partner_contacts
+    )
+
+    partner_ns_emails = list(partner_contacts.values_list("email", flat=True))
+
+    regional_coordinator_emails: list[str] = get_coordinator_emails_by_region(instance.country.region)
+
+    recipient = [
+        instance.national_society_contact_email,
+    ]
+
+    cc_recipients = list(
+        set(
+            [
+                settings.EMAIL_EAP_DREF_ANTICIPATORY_PILLAR,
+                *settings.EMAIL_EAP_DREF_AA_GLOBAL_TEAM,
+                *regional_coordinator_emails,
+                *partner_ns_emails,
+            ]
+        )
+    )
+    email_context = get_eap_email_context(instance)
+    email_subject = f"[DREF {instance.get_eap_type_display()} SUBMISSION REMINDER] {instance.country} {instance.disaster_type}"
+    email_body = render_to_string("email/eap/reminder.html", email_context)
+    email_type = "Approved EAP"
+    send_notification(
+        subject=email_subject,
+        recipients=recipient,
+        html=email_body,
+        mailtype=email_type,
+        cc_recipients=cc_recipients,
+    )
+    instance.deadline_remainder_sent_at = timezone.now()
+    instance.save(update_fields=["deadline_remainder_sent_at"])
+
+    return email_context
