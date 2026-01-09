ff304bdd265d6229be71611902cef482f125a17e
sandeshit <thapasandesh56@gmail.com>
2026-01-09 12:00:09 +0545

    feat(etl): change transform logic.
    - Added category and type enum.
    - Alter people and building exposed fallback to None.
    - Add polling start date and lookback weeks field in Connector.
    - Squash Migrations.

--- alert_system/etl/gdacs_flood/loader.py
+++ alert_system/etl/gdacs_flood/loader.py
@@ -7,6 +7,7 @@ class GdacsLoader(BaseLoaderClass):
 
     # NOTE: Add additional changes to the filter here. This is example only.
     def filter_eligible_items(self, load_obj):
-        if load_obj.get("people_exposed") > gdacs_flood_config["people_exposed_threshold"]:
-            return True
-        return False
+        people_exposed = load_obj.get("people_exposed")
+        if people_exposed is None:
+            return False
+        return people_exposed > gdacs_flood_config["people_exposed_threshold"]
