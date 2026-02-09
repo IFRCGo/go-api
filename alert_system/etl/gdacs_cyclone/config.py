74c4d883f820211f182db1a23c1b87acb7f4a497
sandeshit <thapasandesh56@gmail.com>
2026-02-09 14:09:49 +0545

    feat(gdacs-cyclone): add source gdacs cyclone

---
+++ alert_system/etl/gdacs_cyclone/config.py
@@ -0,0 +1,13 @@
+# NOTE: Store Config files here. Might need to refactor if source supports filtering with hazards.
+from alert_system.etl.base.config import ExtractionConfig
+
+gdacs_cyclone_config: ExtractionConfig = {
+    "event_collection_type": "gdacs-events",
+    "hazard_collection_type": "gdacs-hazards",
+    "impact_collection_type": "gdacs-impacts",
+    "filter_event": {"hazard_codes": ["MH0309", "TC", "nat-met-sto-tro"]},
+    "filter_hazard": None,
+    "filter_impact": None,
+    "people_exposed_threshold": 500,
+    "forecasted_data": False,
+}
