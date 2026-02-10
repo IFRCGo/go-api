660d478d92908164b099f06cf817d6c97dbc35c8
Huang-Hao-Gao <huanggao73@outlook.com>
2026-02-10 16:25:11 +0000

    add serializer for cleaned framework agreements

--- api/drf_views.py
+++ api/drf_views.py
@@ -202,6 +202,7 @@ from .serializers import (  # AppealSerializer,; Tableau Serializers; AppealTabl
     EventSeverityLevelHistorySerializer,
     ExportSerializer,
     ExternalPartnerSerializer,
+    FabricCleanedFrameworkAgreementSerializer,    
     FabricDimAgreementLineSerializer,
     FabricDimAppealSerializer,
     FabricDimBuyerGroupSerializer,
