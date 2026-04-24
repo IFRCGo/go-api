c470f898a1e1e0f7c784934814b3f03b7e929888
sudip-khanal <sudipkhanal0777@gmail.com>
2026-04-24 15:41:03 +0545

    feat(api): add new api endpoint for emergency
    - Add new emergency api for.
    - Attach latest field report.
    - Add test case for emergency endpoint.

--- api/drf_views.py
+++ api/drf_views.py
@@ -89,6 +89,7 @@ from .models import (
     Export,
     ExternalPartner,
     FieldReport,
+    KeyFigure,
     MainContact,
     Profile,
     Region,
@@ -123,6 +124,7 @@ from .serializers import (  # AppealSerializer,; Tableau Serializers; AppealTabl
     CountrySupportingPartnerSerializer,
     CountryTableauSerializer,
     DeploymentsByEventSerializer,
+    DetailEmergencySerializer,
     DetailEventSerializer,
     DisasterTypeSerializer,
     DistrictSerializer,
@@ -1560,11 +1562,12 @@ class CountrySupportingPartnerViewSet(viewsets.ModelViewSet):
         return CountrySupportingPartner.objects.select_related("country")
 
 
-class EmergencyViewset(viewsets.ReadOnlyModelViewSet):
+class EmergencyViewset(ReadOnlyVisibilityViewset):
     queryset = Event.objects.all()
     lookup_field = "id"
     serializer_class = DetailEmergencySerializer
     filterset_class = EventFilter
+    visibility_model_class = Event
 
     def get_queryset(self):
         return (
@@ -1582,9 +1585,21 @@ class EmergencyViewset(viewsets.ReadOnlyModelViewSet):
                 Prefetch("contacts", queryset=EventContact.objects.all()),
             )
             .annotate(
-                latest_field_report_id=Subquery(
-                    FieldReport.objects.filter(event=OuterRef("pk")).order_by("-created_at").values("id")[:1]
+                first_field_report_id=Subquery(
+                    FieldReport.objects.filter(event=OuterRef("pk"))
+                    .order_by(
+                        "fr_num",
+                        "updated_at",
+                    )
+                    .values("id")[:1]
                 ),
-                appeal_id=Subquery(Appeal.objects.filter(event=OuterRef("pk")).order_by("-created_at").values("id")[:1]),
+                latest_field_report_id=Subquery(
+                    FieldReport.objects.filter(event=OuterRef("pk"))
+                    .order_by(
+                        "-fr_num",
+                        "-updated_at",
+                    )
+                    .values("id")[:1]
+                ),
             )
         )
