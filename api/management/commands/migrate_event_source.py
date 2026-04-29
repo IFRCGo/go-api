540a75c29d51a762c7b3cd3da2c88b25bba29e2f
sudip-khanal <sudipkhanal0777@gmail.com>
2026-04-29 16:59:21 +0545

    feat(api): add script to update event source

---
+++ api/management/commands/migrate_event_source.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from api.models import Event
+
+
+class Command(BaseCommand):
+    help = "Update event sources based on name prefix and auto-generated status"
+
+    def handle(self, *args, **options):
+        queryset = Event.objects.filter(auto_generated=True, source=0)
+        self.stdout.write(
+            self.style.NOTICE(
+                f"{queryset.count()} events will be assigned a source (GDACS or Manual Input) based on name prefix."
+            )
+        )
+        to_update = []
+        for event in queryset.iterator():
+            if event.name.startswith("GDACS"):
+                event.source = Event.EventSource.GDACS
+                self.stdout.write(self.style.NOTICE(f"Updating {event.name} source to {Event.EventSource.GDACS.label}"))
+            else:
+                event.source = Event.EventSource.Manual_Input
+                self.stdout.write(self.style.NOTICE(f"Updating {event.name} source to {Event.EventSource.Manual_Input.label}"))
+                event.auto_generated = False
+            to_update.append(event)
+
+        updated_event = Event.objects.bulk_update(to_update, ["source", "auto_generated"])
+        self.stdout.write(self.style.SUCCESS(f"Total {updated_event} Events source have been updated successfully."))
