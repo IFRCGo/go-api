a815e673cf43704fa8167d82dac1d5cfaa132acb
Szabó, Zoltán <szabozoltan69@gmail.com>
2026-05-26 13:29:52 +0200

    Revert "Project:Alert system workflow"

--- notifications/enums.py
+++ notifications/enums.py
@@ -2,6 +2,4 @@ from . import models
 
 enum_register = {
     "surge_alert_status": models.SurgeAlertStatus,
-    "alert_source": models.AlertSubscription.AlertSource,
-    "alert_per_day": models.AlertSubscription.AlertPerDay,
 }
