6ea155a517f31767c928388ec0288abc4247d4a9
sudip-khanal <sudipkhanal0777@gmail.com>
2026-06-10 13:42:50 +0545

    feat(api): include external source fields in auto generated event from field report
    - Added auto_generated_external_source field to Event
    - Added auto_generated_external_source_id field to Event
    - Set both fields during automatic event creation from a FieldReport

--- api/admin.py
+++ api/admin.py
@@ -417,6 +417,8 @@ class EventAdmin(CompareVersionAdmin, RegionRestrictedAdmin, TranslationAdmin):
                 "created_at",
                 "updated_at",
                 "who_guid",
+                "auto_generated_external_source",
+                "auto_generated_external_source_id",
             )
 
         # Set severity level from GET parameter
@@ -659,7 +661,15 @@ class FieldReportAdmin(CompareVersionAdmin, RegionRestrictedAdmin, TranslationAd
         "districts",
     )
 
-    readonly_fields = ("report_date", "created_at", "updated_at", "summary", "fr_num")
+    readonly_fields = (
+        "report_date",
+        "created_at",
+        "updated_at",
+        "summary",
+        "fr_num",
+        "external_source",
+        "external_source_id",
+    )
     list_filter = [MembershipFilter, FieldReportStatusFilter, "ns_request_assistance"]
     actions = [
         "create_events",
