ec973e2a7e8ce67f43b2c4f18902af222f0e6b6c
Szabo Zoltan <szabozoltan69@gmail.com>
2026-07-02 09:59:25 +0200

    Change App_startDate in ingest_appeals

--- api/management/commands/ingest_appeals.py
+++ api/management/commands/ingest_appeals.py
@@ -131,7 +131,7 @@ class Command(BaseCommand):
             # get latest APPEALS
             logger.info("Querying appeals API for new appeals data")
             url = "https://go-api.ifrc.org/api/appeals"  # DEBUG: can append filter &app_code=MDRDJ003
-            params = {"App_startDate": "2020-01-01|2100-01-01"}
+            params = {"App_startDate": "2023-07-01|2100-01-01"}
             # try 3 times to reach the API
             try:
                 response = sess.get(url, auth=auth, headers=headers, params=params)
