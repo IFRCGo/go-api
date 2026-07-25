68529f15ce92e8152e54af4055e7d7ef23376c57
Szabo Zoltan <szabozoltan69@gmail.com>
2026-07-25 13:14:13 +0200

    String search for AppealFilter

--- api/management/commands/ingest_appeals.py
+++ api/management/commands/ingest_appeals.py
@@ -1,6 +1,5 @@
 import json
 import os
-import re
 from datetime import datetime, timedelta
 from datetime import timezone as datetime_timezone
 
@@ -40,7 +39,7 @@ class Command(BaseCommand):
 
     def get_codes_skip(self):
         value = AppealFilter.objects.filter(name="ingestAppealFilter").values_list("value", flat=True).first()
-        return re.findall(r"[^\s,]+", value or "")
+        return (value or "").casefold()
 
     def parse_date(self, date_string):
         timeformat = "%Y-%m-%dT%H:%M:%S"
@@ -75,7 +74,7 @@ class Command(BaseCommand):
 
                 for r in records:
                     # Temporary filtering, the manual version should be kept:
-                    if r["APP_code"] in codes_skip:  # ['MDR65002', 'MDR00001', 'MDR00004']:
+                    if r["APP_code"].casefold() in codes_skip:  # 'MDR65002, MDR00001, MDR00004'
                         continue
                     if r["APP_code"] not in codes:
                         new.append(r)
@@ -161,7 +160,7 @@ class Command(BaseCommand):
 
             for r in records:
                 # Temporary filtering, the manual version should be kept:
-                if r["APP_code"] in codes_skip:  # ['MDR65002', 'MDR00001', 'MDR00004']:
+                if r["APP_code"].casefold() in codes_skip:  # 'MDR65002, MDR00001, MDR00004'
                     continue
                 # if r['APP_code'] != 'DEBUG_this': continue
                 if r["APP_code"] not in codes:
